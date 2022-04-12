6c722b4b7653cf3f666f2ab44d71dcc98ae7e899
tomholford <tomholford@users.noreply.github.com>
2022-04-12 09:09:03 -0700

    devex: add snapshot testing lib and example

--- ui/src/components/AppTile.test.tsx
+++ ui/src/components/AppTile.test.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { describe, expect, it } from 'vitest'
+import renderer from 'react-test-renderer';
 import { AppTile } from './AppTile'
 import { render, screen } from '../../test/utils'
 
 describe('AppTile', () => {
   describe('when image is available', () => {
     it('it renders an image', () => {
-      render(<AppTile color='#fefefe' image="test.jpg" />)
-      expect(screen.getByAltText('urbit app tile')).toBeInTheDocument()
+      render(<AppTile color='#fefefe' image="test.jpg" title="pals" />)
+      expect(screen.getByAltText('app tile for pals')).toBeInTheDocument()
     })
   });
 
   describe('when image is not available', () => {
     it('it does not render an image', () => {
-      render(<AppTile color='#fefefe' />)
-      expect(screen.queryByAltText('urbit app tile')).toBeNull()
+      render(<AppTile color='#fefefe' title="pals" />)
+      expect(screen.queryByAltText('app tile for pals')).toBeNull()
     })
+  });
+
+  it('renders as expected', () => {
+    const tree = renderer
+      .create(<AppTile color='#fefefe' image="test.jpg" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 })
