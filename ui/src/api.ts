153b9c59345464a894c1b1624128353ae4b4814c
Patrick O'Sullivan <patrick@osullivan.io>
2022-04-26 10:15:00 -0500

    Use VITE_SHIP_URL for mocking requests

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -3,9 +3,10 @@ import UrbitMock from '@tloncorp/mock-http-api';
 import mockHandlers from './state/mockHandlers';
 
 const IS_MOCK = import.meta.env.MODE === 'mock';
+const URL = import.meta.env.VITE_SHIP_URL as string;
 
 const api = IS_MOCK
-  ? new UrbitMock('', '', mockHandlers)
+  ? new UrbitMock(URL, '', mockHandlers)
   : new Urbit('', '', window.desk);
 api.ship = window.ship;
 
