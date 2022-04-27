2c07c5cd2781672d7a98065159bc0e67630dc937
Hunter Miller <hunter@hmiller.dev>
2022-04-27 13:19:53 -0500

    button: adding story

---
+++ ui/src/components/button/Button.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './Button';
+
+export default {
+  component: Button,
+  title: 'Button',
+};
+
+export function Primary() {
+  return <Button>Hello!</Button>;
+}
