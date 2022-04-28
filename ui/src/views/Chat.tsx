01f170e34152e4ebbe64ad0434c5c984126c0c8e
Patrick O'Sullivan <patrick@osullivan.io>
2022-04-28 15:00:50 -0500

    chat: use new Chat component

--- ui/src/views/Chat.tsx
+++ ui/src/views/Chat.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import ChatMessage from '../components/ChatMessage';
+import ChatMessage from '../components/chat-message/ChatMessage';
 import ChatInput from '../components/ChatInput';
 import api from '../api';
 import { useChatState, useMessagesForChat } from '../state/chat';
@@ -7,7 +7,6 @@ import Layout from '../components/layout/Layout';
 import { version } from '../../package.json';
 
 const DEF_FLAG = '~zod/test';
-
 
 export default function Chat() {
   const messages = useMessagesForChat(DEF_FLAG);
@@ -23,29 +22,39 @@ export default function Chat() {
   return (
     <Layout
       header={
-        <div className='flex justify-center items-center h-full'>
+        <div className="flex items-center justify-center h-full">
           <h1 className="text-3xl font-bold text-blue">Welcome to homestead</h1>
         </div>
       }
       footer={
-        <div className='flex justify-center items-center h-full'>
-          <span className='text-gray-300'>
-            homestead {version}
-          </span>
+        <div className="flex items-center justify-center h-full">
+          <span className="text-gray-300">homestead {version}</span>
         </div>
       }
       main={
-        <div className="flex justify-center items-center min-h-screen">
-          <div className="py-20 space-y-6 max-w-md">
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="max-w-md py-20 space-y-6">
             <div className="space-y-4">
-              {messages &&
-                messages
-                  .keys()
-                  .reverse()
-                  .map((key) => {
-                    const writ = messages.get(key);
-                    return <ChatMessage key={writ.seal.time} writ={writ} />;
-                  })}
+              {messages
+                .keys()
+                .reverse()
+                .map((key, index) => {
+                  const writ = messages.get(key);
+                  const lastWrit =
+                    index > 0
+                      ? messages.get(messages.keys().reverse()[index - 1])
+                      : undefined;
+                  const newAuthor = lastWrit
+                    ? writ.memo.author !== lastWrit.memo.author
+                    : true;
+                  return (
+                    <ChatMessage
+                      key={writ.seal.time}
+                      writ={writ}
+                      newAuthor={newAuthor}
+                    />
+                  );
+                })}
             </div>
             <ChatInput flag={DEF_FLAG} />
           </div>
