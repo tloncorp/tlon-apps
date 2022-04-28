1e3e103c094a2a5bd5359b586743ee9006890512
Patrick O'Sullivan <patrick@osullivan.io>
2022-04-28 14:14:00 -0500

    chat: add comments to ChatContent

--- ui/src/components/chat-content/ChatContent.tsx
+++ ui/src/components/chat-content/ChatContent.tsx
@@ -7,11 +7,13 @@ type ChatContentProps = {
 
 export default function ChatContent({ content }: ChatContentProps) {
   if (Array.isArray(content)) {
+    // This will handle the various content types that will be received as arrays.
     return (
       <div>
         {content.map((contentItem) => {
           switch (contentItem.kind) {
             case 'image':
+              // This is just an example. This will be its own component soon.
               return (
                 <img
                   src={contentItem.source}
@@ -28,6 +30,7 @@ export default function ChatContent({ content }: ChatContentProps) {
     );
   }
   switch (content.kind) {
+      // We'll build out components for each type and return them here.
     case 'text':
       return <div>{content.contentText}</div>;
     default:
