592854a02003234a55aa332a4460ecb998956b7c
Patrick O'Sullivan <patrick@osullivan.io>
2022-04-29 10:35:28 -0500

    chat: shunt block content to the top

--- ui/src/components/ChatContent/ChatContent.tsx
+++ ui/src/components/ChatContent/ChatContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {memo} from 'react';
 import { ChatMessage, isBold, isItalics, isLink } from '../../types/chat';
 
 interface ChatContentProps {
@@ -7,68 +7,70 @@ interface ChatContentProps {
 
 export default function ChatContent({ content }: ChatContentProps) {
   const inlineLength = content.inline.length;
+  const blockLength = content.block.length;
 
-  if (inlineLength) {
-    return (
-      <div>
-        {content.inline.map((contentItem, index) => {
-          const key = `${contentItem.toString}-${index}`;
-          const space = inlineLength - 1 === index ? null : <span>&nbsp;</span>;
+  const InlineContent = memo(() => (
+    <div>
+      {content.inline.map((contentItem, index) => {
+        const key = `${contentItem.toString}-${index}`;
+        const space = inlineLength - 1 === index ? null : <span>&nbsp;</span>;
 
-          if (typeof contentItem === 'string') {
-            return (
-              <span key={key}>
-                {contentItem} {space}
-              </span>
-            );
-          }
-
-          if (isBold(contentItem)) {
-            return (
-              <b key={key}>
-                {contentItem.bold} {space}
-              </b>
-            );
-          }
-
-          if (isItalics(contentItem)) {
-            return (
-              <i key={key}>
-                {contentItem.italics} {space}
-              </i>
-            );
-          }
-
-          if (isLink(contentItem)) {
-            return (
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={contentItem.href}
-                key={key}
-              >
-                {contentItem.href} {space}
-              </a>
-            );
-          }
-
-          throw new Error(
-            `Unhandled message type: ${JSON.stringify(contentItem)}`
+        if (typeof contentItem === 'string') {
+          return (
+            <span key={key}>
+              {contentItem} {space}
+            </span>
           );
-        })}
-      </div>
-    );
-  }
+        }
 
-  if (content.block) {
-    return (
-      <div>
-        {content.block.map((contentItem, index) => (
-          <div key={`${contentItem.bock}-${index}`}>{contentItem.block}</div>
-        ))}
-      </div>
-    );
-  }
+        if (isBold(contentItem)) {
+          return (
+            <b key={key}>
+              {contentItem.bold} {space}
+            </b>
+          );
+        }
 
-  throw new Error(`Unhandled message type: ${JSON.stringify(content)}`);
+        if (isItalics(contentItem)) {
+          return (
+            <i key={key}>
+              {contentItem.italics} {space}
+            </i>
+          );
+        }
+
+        if (isLink(contentItem)) {
+          return (
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={contentItem.href}
+              key={key}
+            >
+              {contentItem.href} {space}
+            </a>
+          );
+        }
+
+        throw new Error(
+          `Unhandled message type: ${JSON.stringify(contentItem)}`
+        );
+      })}
+    </div>
+  ));
+
+  const BlockContent = memo(() => (
+    <div>
+      {content.block.map((contentItem, index) => (
+        <div key={`${contentItem.bock}-${index}`}>{contentItem.block}</div>
+      ))}
+    </div>
+  ));
+
+  return (
+    <div>
+      {blockLength > 0 && <BlockContent />}
+      {inlineLength > 0 && <InlineContent />}
+    </div>
+  );
 }
