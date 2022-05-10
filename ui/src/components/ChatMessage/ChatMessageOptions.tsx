d13a3140d1fb5c382f88c95a67b50c52ea17401f
Liam Fitzgerald <liam@tlon.io>
2022-05-10 10:56:40 -0500

    ui: surface message deletion

--- ui/src/components/ChatMessage/ChatMessageOptions.tsx
+++ ui/src/components/ChatMessage/ChatMessageOptions.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useChatState } from '../../state/chat';
+import { ChatWrit } from '../../types/chat';
 import IconButton from '../IconButton';
 import BubbleIcon from '../icons/BubbleIcon';
 import ElipsisIcon from '../icons/ElipsisIcon';
 import FaceIcon from '../icons/FaceIcon';
 import HashIcon from '../icons/HashIcon';
 import ShareIcon from '../icons/ShareIcon';
+import XIcon from '../icons/XIcon';
 
-export default function ChatMessageOptions() {
+export default function ChatMessageOptions(props: {
+  flag: string;
+  writ: ChatWrit;
+}) {
+  const { flag, writ } = props;
+  const onDelete = () => {
+    useChatState.getState().delMessage(flag, writ.seal.time);
+  };
+
   return (
     <div className="z-1 absolute right-2 -top-5 flex flex space-x-[2px] rounded-md border-[1px] border-gray-100 bg-white p-[2px] align-middle opacity-0 group-one-hover:opacity-100">
       <IconButton
@@ -33,6 +44,15 @@ export default function ChatMessageOptions() {
         showTooltip
         action={() => console.log('send to..')}
       />
+      {window.our === writ.memo.author ? (
+        <IconButton
+          icon={<XIcon className="text-red" />}
+          label="Delete"
+          showTooltip
+          action={onDelete}
+        />
+      ) : null}
+
       <IconButton
         icon={<ElipsisIcon className="text-gray-400" />}
         label="More..."
