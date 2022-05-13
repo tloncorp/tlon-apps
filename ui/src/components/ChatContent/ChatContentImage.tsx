9342df2e8f4047ae89825111e05eb345cdb01428
Patrick O'Sullivan <patrick@osullivan.io>
2022-05-13 14:12:40 -0500

    chat message: Address ed's feedback

--- ui/src/components/ChatContent/ChatContentImage.tsx
+++ ui/src/components/ChatContent/ChatContentImage.tsx
@@ -29,22 +29,25 @@ export default function ChatContentImage({
           alt={altText ? altText : 'A chat image'}
         />
       </a>
-      <div className="absolute top-5 right-[11px] flex space-x-2 text-white opacity-0 group-hover:opacity-100">
-        <a
-          className="h-[18px] w-[18px] cursor-pointer"
-          href={src}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <ExpandIcon />
-        </a>
-        <button
-          className="h-[18px] w-[18px] cursor-pointer"
-          onClick={() => console.log('hi')}
-        >
-          <ElipsisCircleIcon />
-        </button>
-      </div>
+      {/*
+        TODO: put these icons back in after they've been finalized by design.
+        <div className="absolute top-5 right-[11px] flex space-x-2 text-white opacity-0 group-hover:opacity-100">
+          <a
+            className="h-[18px] w-[18px] cursor-pointer"
+            href={src}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ExpandIcon />
+          </a>
+          <button
+            className="h-[18px] w-[18px] cursor-pointer"
+            onClick={() => console.log('hi')}
+          >
+            <ElipsisCircleIcon />
+          </button>
+        </div>
+      */}
     </div>
   );
 }
