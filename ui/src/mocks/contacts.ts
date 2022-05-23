1d0a7c3ebc818879f8ffae31928ac64cefbb7343
Patrick O'Sullivan <patrick@osullivan.io>
2022-05-23 13:24:08 -0500

    dms: Add some better/more detailed contacts to mock store

--- ui/src/mocks/contacts.ts
+++ ui/src/mocks/contacts.ts
@@ -2,14 +2,52 @@ import { Rolodex } from '@urbit/api';
 
 const mockContacts: Rolodex = {
   '~finned-palmer': {
-    nickname: '',
-    bio: 'Some guy',
     status: '',
-    color: '#2e3cff',
+    'last-updated': 1628685243041,
     avatar: null,
     cover: null,
-    groups: [''],
-    'last-updated': 0,
+    bio: '',
+    nickname: '',
+    color: '0x2e.3cff',
+    groups: [],
+  },
+  '~nocsyx-lassul': {
+    status: 'technomancing an electron wrapper for urbit',
+    'last-updated': 1652879509836,
+    avatar: null,
+    cover:
+      'https://i.pinimg.com/originals/20/62/59/2062590a440f717a2ae1065ad8e8a4c7.gif',
+    bio: 'Technomancer. Gaining clarity on reality daily, building calm, maintainable, and resilient systems.',
+    nickname: '~nocsyx-lassul ⚗️',
+    color: '0x39.00d6',
+    groups: [
+      '/ship/~nocsyx-lassul/holy-grail-ui',
+      '/ship/~wolref-podlex/foundation',
+      '/ship/~fabled-faster/structure',
+    ],
+  },
+  '~hastuc-dibtux': {
+    status: 'Network Spirituality',
+    'last-updated': 1649439191889,
+    avatar:
+      'https://lh3.googleusercontent.com/M1kx277EWn0x1PR_kMb3hSJLj086mhasqkm3WkdUFLV4lhHJ6mZWFMaPBERn3A2iFubegTHe8dDDcx20iuJSK0o0bmu_UiwKXhNj=s0',
+    cover:
+      'https://urbit.ewr1.vultrobjects.com/hastuc-dibtux/2021.2.12..03.56.32-A2E46340-7F0E-478D-97F5-ED85E0123459.jpeg',
+    bio: "Enfant terrible @ tlon.io\n\ntime warfare\n\nguerilla hooning\n\nif you're having trouble on Urbit, DM me!\n",
+    nickname: '~𝚑𝚊𝚜𝚝𝚞𝚌-𝚍𝚒𝚋𝚝𝚞𝚡 ⟳',
+    color: '0xee.5431',
+    groups: ['/ship/~hastuc-dibtux/pharma', '/ship/~ladpeg-pintem/freenode'],
+  },
+  '~datder-sonnet': {
+    status: 'in ~zod we trust',
+    'last-updated': 1647446196001,
+    avatar: null,
+    cover:
+      'https://pbs.twimg.com/profile_banners/922197314579697666/1508704898/1500x500',
+    bio: 'High-Functioning Urbit Maximalist\n\nFrontend @ Tlon\n\nhttps://tholf.org',
+    nickname: '',
+    color: '0x0',
+    groups: ['/ship/~dister-datder-sonnet/hodl', '/ship/~batbex/gem-rubyists'],
   },
 };
 
