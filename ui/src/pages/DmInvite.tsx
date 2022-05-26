e917a5ac7701bd24fdfa81cf44c75c81c8e9436e
Patrick O'Sullivan <patrick@osullivan.io>
2022-05-26 11:43:35 -0500

    dm-invites: add decline 'are you sure?' modal

--- ui/src/pages/DmInvite.tsx
+++ ui/src/pages/DmInvite.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import Avatar from '../components/Avatar';
+import XIcon from '../components/icons/XIcon';
 import { useChatState } from '../state/chat';
 import { useContact } from '../state/contact';
 
@@ -9,6 +10,7 @@ interface DmInviteProps {
 }
 
 export default function DmInvite({ ship }: DmInviteProps) {
+  const [showDeclineModal, setShowDeclineModal] = useState(false);
   const navigate = useNavigate();
   const contact = useContact(ship);
   const onAccept = () => {
@@ -19,29 +21,69 @@ export default function DmInvite({ ship }: DmInviteProps) {
     useChatState.getState().dmRsvp(ship, false);
   };
   return (
-    <div className="flex h-full w-full flex-col flex-col items-center space-y-4 pt-8">
-      <div className="flex flex-col items-center space-y-1">
-        <Avatar ship={ship} size="huge" icon={false} />
-        {contact?.nickname ? (
-          <span className="font-semibold">{contact.nickname}</span>
-        ) : null}
-        <span className="text-gray-600">{ship}</span>
-        {/*
+    <div className="relative flex h-full w-full">
+      <div className="flex h-full w-full flex-col flex-col items-center space-y-4 pt-8">
+        <div className="flex flex-col items-center space-y-1">
+          <Avatar ship={ship} size="huge" icon={false} />
+          {contact?.nickname ? (
+            <span className="font-semibold">{contact.nickname}</span>
+          ) : null}
+          <span className="text-gray-600">{ship}</span>
+          {/*
           TODO: Show mutual groups.
          */}
+        </div>
+        <span className="font-semibold">
+          {contact?.nickname ? contact.nickname : ship} is sending you a new
+          message
+        </span>
+        <div className="flex space-x-2">
+          <button className="button" onClick={onAccept} type="button">
+            Accept
+          </button>
+          <button
+            className="button"
+            onClick={() => setShowDeclineModal(true)}
+            type="button"
+          >
+            Decline
+          </button>
+        </div>
       </div>
-      <span className="font-semibold">
-        {contact?.nickname ? contact.nickname : ship} is sending you a new
-        message
-      </span>
-      <div className="flex space-x-2">
-        <button className="button" onClick={onAccept} type="button">
-          Accept
-        </button>
-        <button className="button" onClick={onDecline} type="button">
-          Decline
-        </button>
-      </div>
+      {showDeclineModal ? (
+        <div className="absolute z-10 flex h-full w-full flex-col items-center justify-center bg-gray-400/[0.16]">
+          <div className="flex h-1/6 w-2/5 flex-col items-center justify-start rounded-xl bg-white p-6">
+            <div className="flex w-full items-center justify-between">
+              <span className="text-lg font-bold">Decline Message</span>
+              <button
+                onClick={() => setShowDeclineModal(false)}
+                className="flex h-6 w-6 items-center justify-center rounded-xl bg-gray-50"
+              >
+                <XIcon className="h-3 w-3 text-gray-400" />
+              </button>
+            </div>
+            <span className="pt-4 leading-5">
+              Are you sure you want to decline this message? Declining will
+              remove this chat from your messages.{' '}
+              <span className="font-bold">
+                {contact?.nickname ? contact.nickname : ship}
+              </span>{' '}
+              can still send you new message invitations.
+            </span>
+            <div className="flex w-full justify-end space-x-2 pt-7">
+              <button
+                className="button bg-gray-50 text-gray-800"
+                onClick={() => setShowDeclineModal(false)}
+              >
+                Cancel
+              </button>
+              <button className="button bg-red" onClick={onDecline}>
+                Decline
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
