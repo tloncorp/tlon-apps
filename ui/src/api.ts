43ff44aa2abb3d54fdbf4275e3375060ce6abfc5
Hunter Miller <hunter@hmiller.dev>
2022-06-07 16:36:32 -0700

    perf: adding profiler and removing mock data from prod build

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,21 +1,22 @@
 import Urbit from '@urbit/http-api';
-import UrbitMock from '@tloncorp/mock-http-api';
-import mockHandlers from './mocks/handlers';
+import type UrbitMock from '@tloncorp/mock-http-api';
 
 export const IS_MOCK =
   import.meta.env.MODE === 'mock' || import.meta.env.MODE === 'staging';
 const URL = (import.meta.env.VITE_MOCK_URL ||
   import.meta.env.VITE_VERCEL_URL) as string;
 
+let mockApi: Urbit | UrbitMock = new Urbit('', '', window.desk);
 if (IS_MOCK) {
   window.ship = '~zod';
   window.our = '~zod';
   window.desk = 'homestead';
+  const MockUrbit = (await import('@tloncorp/mock-http-api')).default;
+  const mockHandlers = (await import('./mocks/handlers')).default;
+  mockApi = new MockUrbit(mockHandlers, URL || '', '');
 }
 
-const api = IS_MOCK
-  ? new UrbitMock(mockHandlers, URL || '', '')
-  : new Urbit('', '', window.desk);
+const api = IS_MOCK ? mockApi : new Urbit('', '', window.desk);
 api.ship = window.ship;
 api.verbose = true;
 
