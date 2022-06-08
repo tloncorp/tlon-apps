793362d80f16a9dcfd11d6743f4f55b74e8ed6ac
Patrick O'Sullivan <patrick@osullivan.io>
2022-06-08 13:23:04 -0700

    chat split: switch routing scheme based on mode (IS_CHAT?)

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -22,7 +22,6 @@ import Dm from './pages/Dm';
 import NewDM from './pages/NewDm';
 import Gang, { GangModal } from './pages/Gang';
 import JoinGroup, { JoinGroupModal } from './pages/JoinGroup';
-
 import Sidebar from './components/Sidebar/Sidebar';
 import { DmThread, GroupChatThread } from './chat/ChatThread/ChatThread';
 import Policy from './pages/Policy';
@@ -36,42 +35,15 @@ import ErrorAlert from './components/ErrorAlert';
 import DMSidebar from './dms/DMSidebar';
 import DMHome from './dms/DMHome';
 
-function App() {
-  const handleError = useErrorHandler();
-  const location = useLocation();
-  const isMobile = useIsMobile();
+interface RoutesProps {
+  isMobile: boolean;
+  state: { backgroundLocation?: Location } | null;
+  location: Location;
+}
 
-  useEffect(() => {
-    handleError(() => {
-      useGroupState.getState().start();
-      useChatState.getState().start();
-      useChatState.getState().fetchDms();
-      const { initialize: settingsInitialize, fetchAll } =
-        useSettingsState.getState();
-      settingsInitialize(api);
-      fetchAll();
-
-      useContactState.getState().initialize(api);
-    })();
-  }, [handleError]);
-
-  const theme = useTheme();
-  const isDarkMode = useMedia('(prefers-color-scheme: dark)');
-
-  useEffect(() => {
-    if ((isDarkMode && theme === 'auto') || theme === 'dark') {
-      document.body.classList.add('dark');
-      useLocalState.setState({ currentTheme: 'dark' });
-    } else {
-      document.body.classList.remove('dark');
-      useLocalState.setState({ currentTheme: 'light' });
-    }
-  }, [isDarkMode, theme]);
-
-  const state = location.state as { backgroundLocation?: Location } | null;
-
+function ChatRoutes({ isMobile, state, location }: RoutesProps) {
   return (
-    <div className="flex h-full w-full">
+    <>
       <Routes>
         <Route path={isMobile ? '/' : '*'} element={<Sidebar />} />
       </Routes>
@@ -117,6 +89,105 @@ function App() {
           <Route path="/gangs/:ship/:name" element={<GangModal />} />
         </Routes>
       ) : null}
+    </>
+  );
+}
+
+function GroupsRoutes({ isMobile, state, location }: RoutesProps) {
+  return (
+    <>
+      <Routes>
+        <Route path={isMobile ? '/' : '*'} element={<Sidebar />} />
+      </Routes>
+      <Routes>
+        <Route
+          path={isMobile ? '/groups/:ship/:name' : '/groups/:ship/:name/*'}
+          element={<GroupSidebar />}
+        />
+        <Route path={isMobile ? '/dm' : '/dm/*'} element={<DMSidebar />} />
+      </Routes>
+      <Routes location={state?.backgroundLocation || location}>
+        <Route path="/dm/" element={<Dms />}>
+          <Route index element={<DMHome />} />
+          <Route path="new" element={<NewDM />} />
+          <Route path=":ship" element={<Dm />}>
+            <Route path="message/:idShip/:idTime" element={<DmThread />} />
+          </Route>
+        </Route>
+
+        <Route path="/gangs/:ship/:name" element={<Gang />} />
+        <Route path="/groups/new" element={<NewGroup />} />
+        <Route path="/groups/join" element={<JoinGroup />} />
+        <Route path="/groups/:ship/:name/*" element={<Groups />}>
+          <Route path="members" element={<Members />} />
+          <Route path="roles" element={<Roles />} />
+          <Route path="policy" element={<Policy />} />
+          <Route path="channels/:app/:chShip/:chName" element={<Channel />}>
+            <Route
+              path="message/:idShip/:idTime"
+              element={<GroupChatThread />}
+            />
+          </Route>
+          <Route
+            path="channels/:app/:chShip/:chName/settings"
+            element={<ChannelSettings />}
+          />
+          <Route path="channels/new" element={<NewChannel />} />
+        </Route>
+      </Routes>
+      {state?.backgroundLocation ? (
+        <Routes>
+          <Route path="/groups/join" element={<JoinGroupModal />} />
+          <Route path="/gangs/:ship/:name" element={<GangModal />} />
+        </Routes>
+      ) : null}
+    </>
+  );
+}
+
+function App() {
+  const handleError = useErrorHandler();
+  const location = useLocation();
+  const isMobile = useIsMobile();
+  const IS_CHAT =
+    import.meta.env.MODE === 'chat' || import.meta.env.MODE === 'chatmock';
+
+  useEffect(() => {
+    handleError(() => {
+      useGroupState.getState().start();
+      useChatState.getState().start();
+      useChatState.getState().fetchDms();
+      const { initialize: settingsInitialize, fetchAll } =
+        useSettingsState.getState();
+      settingsInitialize(api);
+      fetchAll();
+
+      useContactState.getState().initialize(api);
+    })();
+  }, [handleError]);
+
+  const theme = useTheme();
+  const isDarkMode = useMedia('(prefers-color-scheme: dark)');
+
+  useEffect(() => {
+    if ((isDarkMode && theme === 'auto') || theme === 'dark') {
+      document.body.classList.add('dark');
+      useLocalState.setState({ currentTheme: 'dark' });
+    } else {
+      document.body.classList.remove('dark');
+      useLocalState.setState({ currentTheme: 'light' });
+    }
+  }, [isDarkMode, theme]);
+
+  const state = location.state as { backgroundLocation?: Location } | null;
+
+  return (
+    <div className="flex h-full w-full">
+      {IS_CHAT ? (
+        <ChatRoutes isMobile={isMobile} state={state} location={location} />
+      ) : (
+        <GroupsRoutes isMobile={isMobile} state={state} location={location} />
+      )}
     </div>
   );
 }
