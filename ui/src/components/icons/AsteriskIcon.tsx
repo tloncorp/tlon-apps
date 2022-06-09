fba25a799ffdebb0cf19809fb413d6f666d1e087
Patrick O'Sullivan <patrick@osullivan.io>
2022-06-09 12:46:36 -0500

    Update ui/src/components/icons/AsteriskIcon.tsx
    Co-authored-by: Hunter Miller <hunter@hmiller.dev>

--- ui/src/components/icons/AsteriskIcon.tsx
+++ ui/src/components/icons/AsteriskIcon.tsx
@@ -5,8 +5,6 @@ export default function AsteriskIcon({ className }: IconProps) {
   return (
     <svg
       className={className}
-      width="12"
-      height="12"
       viewBox="0 0 12 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
