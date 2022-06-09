6ec7828cceb2f22f471fbf46891f72514935f3a9
Hunter Miller <hunter@hmiller.dev>
2022-06-09 14:26:57 -0500

    Update ui/src/components/icons/BulletIcon.tsx

--- ui/src/components/icons/BulletIcon.tsx
+++ ui/src/components/icons/BulletIcon.tsx
@@ -9,7 +9,7 @@ export default function BulletIcon({ className }: IconProps) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect width="8" height="8" rx="4" fill="#666666" />
+      <rect width="8" height="8" rx="4" className="fill-current" />
     </svg>
   );
 }
