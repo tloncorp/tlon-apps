2ee8853afe07f36f7cf3fd50283ef33123204f80
Patrick O'Sullivan <patrick@osullivan.io>
2022-06-09 12:46:46 -0500

    Update ui/src/components/icons/BulletIcon.tsx
    Co-authored-by: Hunter Miller <hunter@hmiller.dev>

--- ui/src/components/icons/BulletIcon.tsx
+++ ui/src/components/icons/BulletIcon.tsx
@@ -5,8 +5,6 @@ export default function BulletIcon({ className }: IconProps) {
   return (
     <svg
       className={className}
-      width="8"
-      height="8"
       viewBox="0 0 8 8"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
