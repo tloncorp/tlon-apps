035f8aa6d37d0d6d2508a739c0e18b23afd1fb5d
tomholford <tomholford@users.noreply.github.com>
2022-06-15 09:45:40 -0700

    sidebar: address PR feedback
    - remove explicit height / width from Icons

--- ui/src/components/icons/InviteIcon16.tsx
+++ ui/src/components/icons/InviteIcon16.tsx
@@ -5,8 +5,6 @@ export default function InviteIcon16({ className }: IconProps) {
   return (
     <svg
       className={className}
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
