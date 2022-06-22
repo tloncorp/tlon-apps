649f78f6d8d6d79a59fc950778ddcae7010f0246
Patrick O'Sullivan <patrick@osullivan.io>
2022-06-22 14:46:51 -0500

    Add basic drag/drop to GroupList

--- ui/src/components/Sidebar/GroupList.tsx
+++ ui/src/components/Sidebar/GroupList.tsx
@@ -1,5 +1,8 @@
 import cn from 'classnames';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import useSidebarSort from '../../logic/useSidebarSort';
 import {
   useGangList,
@@ -13,6 +16,38 @@ import GroupAvatar from '../../groups/GroupAvatar';
 import useNavStore from '../Nav/useNavStore';
 import GroupActions from './GroupActions';
 import SidebarItem from './SidebarItem';
+import { useIsMobile } from '../../logic/useMedia';
+
+const dragTypes = {
+  GROUP: 'group',
+};
+
+function DraggableGroupItem({ flag }: { flag: string }) {
+  const group = useGroup(flag);
+  const navPrimary = useNavStore((state) => state.navigatePrimary);
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: dragTypes.GROUP,
+    item: { group },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <div ref={drag} className={cn(isDragging ? 'opacity-0' : 'opacity-100')}>
+      <SidebarItem
+        icon={
+          <GroupAvatar size="h-12 w-12 sm:h-6 sm:w-6" img={group?.meta.image} />
+        }
+        actions={<GroupActions flag={flag} />}
+        onClick={() => navPrimary('group', flag)}
+      >
+        {group?.meta.title}
+      </SidebarItem>
+    </div>
+  );
+}
 
 function GroupItem({ flag }: { flag: string }) {
   const group = useGroup(flag);
@@ -29,6 +64,34 @@ function GroupItem({ flag }: { flag: string }) {
       {group?.meta.title}
     </SidebarItem>
   );
+}
+
+function GroupItemContainer({
+  flag,
+  children,
+}: PropsWithChildren<{ flag: string }>) {
+  const [{ isOver }, drop] = useDrop<
+    { group: string },
+    undefined,
+    { isOver: boolean }
+  >(
+    () => ({
+      accept: dragTypes.GROUP,
+      drop: ({ group }) => {
+        if (!group || group === flag) {
+          return undefined;
+        }
+        console.log({ group });
+        return undefined;
+      },
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      }),
+    }),
+    [flag]
+  );
+
+  return <div ref={drop}>{children}</div>;
 }
 
 // Gang is a pending group invite
@@ -55,13 +118,27 @@ export default function GroupList({
   className,
   pinned = false,
 }: GroupListProps) {
+  const isMobile = useIsMobile();
   const flags = useGroupList();
   const pinnedFlags = usePinnedGroups();
   const gangs = useGangList();
   const { sortFn, sortOptions } = useSidebarSort();
 
   return pinned ? (
-    <>
+    <DndProvider
+      backend={isMobile ? TouchBackend : HTML5Backend}
+      options={
+        isMobile
+          ? {
+              delay: 50,
+              scrollAngleRanges: [
+                { start: 30, end: 150 },
+                { start: 210, end: 330 },
+              ],
+            }
+          : undefined
+      }
+    >
       <li className="flex items-center space-x-2 px-2 py-3">
         <span className="text-lg font-bold text-gray-400 lg:text-xs lg:font-semibold">
           Pinned
@@ -69,9 +146,11 @@ export default function GroupList({
         <div className="grow border-b-2 border-gray-100" />
       </li>
       {pinnedFlags.sort(sortOptions[sortFn]).map((flag) => (
-        <GroupItem key={flag} flag={flag} />
+        <GroupItemContainer flag={flag}>
+          <DraggableGroupItem key={flag} flag={flag} />
+        </GroupItemContainer>
       ))}
-    </>
+    </DndProvider>
   ) : (
     <ul className={cn('h-full space-y-3 p-2 sm:space-y-0', className)}>
       {flags
