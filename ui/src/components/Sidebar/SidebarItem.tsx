eef9b3a8ba5b0b7bcc2270a15975ca661ab279c7
Patrick O'Sullivan <patrick@osullivan.io>
2022-06-23 14:19:53 -0500

    Use wrapper const rather than extra component in SidebarItem

--- ui/src/components/Sidebar/SidebarItem.tsx
+++ ui/src/components/Sidebar/SidebarItem.tsx
@@ -29,19 +29,29 @@ function Action({
   return <button {...rest}>{children}</button>;
 }
 
-function SidebarItemChild({
+export default function SidebarItem({
   icon,
   to,
+  color = 'text-gray-600',
+  highlight = 'bg-gray-50',
   actions,
   className,
   children,
+  div = false,
   ...rest
 }: SidebarProps) {
   const matches = useMatch(to || 'DONT_MATCH');
   const active = !!matches;
+  const Wrapper = div ? 'div' : 'li';
 
   return (
-    <>
+    <Wrapper
+      className={cn(
+        'group relative flex w-full items-center justify-between rounded-lg text-lg font-semibold hover:bg-gray-50 sm:text-base',
+        color,
+        active && highlight
+      )}
+    >
       <Action
         to={to}
         className={cn(
@@ -62,59 +72,6 @@ function SidebarItemChild({
         </div>
       </Action>
       {actions ? <div className={cn('absolute right-0')}>{actions}</div> : null}
-    </>
-  );
-}
-
-export default function SidebarItem({
-  icon,
-  to,
-  color = 'text-gray-600',
-  highlight = 'bg-gray-50',
-  actions,
-  className,
-  children,
-  div = false,
-  ...rest
-}: SidebarProps) {
-  const matches = useMatch(to || 'DONT_MATCH');
-  const active = !!matches;
-
-  return div ? (
-    <div
-      className={cn(
-        'group relative flex w-full items-center justify-between rounded-lg text-lg font-semibold hover:bg-gray-50 sm:text-base',
-        color,
-        active && highlight
-      )}
-    >
-      <SidebarItemChild
-        icon={icon}
-        to={to}
-        actions={actions}
-        className={className}
-        {...rest}
-      >
-        {children}
-      </SidebarItemChild>
-    </div>
-  ) : (
-    <li
-      className={cn(
-        'group relative flex w-full items-center justify-between rounded-lg text-lg font-semibold hover:bg-gray-50 sm:text-base',
-        color,
-        active && highlight
-      )}
-    >
-      <SidebarItemChild
-        icon={icon}
-        to={to}
-        actions={actions}
-        className={className}
-        {...rest}
-      >
-        {children}
-      </SidebarItemChild>
-    </li>
+    </Wrapper>
   );
 }
