a375cad2b22839fc1aceea00a535dba7cf702b5b
tomholford <tomholford@users.noreply.github.com>
2022-07-01 15:17:00 -0700

    devex: replace `any` with FieldValues form type

--- ui/src/components/ColorPicker.tsx
+++ ui/src/components/ColorPicker.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { UseFormRegister } from 'react-hook-form';
+import { UseFormRegister, FieldValues } from 'react-hook-form';
 import { HexColorPicker, HexColorInput } from 'react-colorful';
 import * as Popover from '@radix-ui/react-popover';
 import classNames from 'classnames';
 
 interface ColorPickerProps {
-  register: UseFormRegister<any>;
+  register: UseFormRegister<FieldValues>;
   defaultColor?: string;
   color: string;
   className?: string;
