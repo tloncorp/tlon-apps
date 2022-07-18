6959dcc86c0ccdb2006b1441dc65d0f6e8e4ea88
tomholford <tomholford@users.noreply.github.com>
2022-07-18 16:05:30 -0700

    test: add single char term case

--- ui/src/logic/utils.test.ts
+++ ui/src/logic/utils.test.ts
@@ -5,18 +5,22 @@ import { whomIsFlag } from './utils';
 describe('whomIsFlag', () => {
   it('passes valid flags', () => {
     const flags = [
+      '~zod/t',
       '~zod/test',
       '~zod/test-test',
       '~zod/test-',
       '~zod/test1',
+      '~marzod/t',
       '~marzod/test',
       '~marzod/test-test',
       '~marzod/test-',
       '~marzod/test1',
+      '~wicdev-wisryt/t',
       '~wicdev-wisryt/test',
       '~wicdev-wisryt/test-test',
       '~wicdev-wisryt/test-',
       '~wicdev-wisryt/test1',
+      '~mister-dister-dozzod-dozzod/t',
       '~mister-dister-dozzod-dozzod/test',
       '~mister-dister-dozzod-dozzod/test-test',
       '~mister-dister-dozzod-dozzod/test-',
