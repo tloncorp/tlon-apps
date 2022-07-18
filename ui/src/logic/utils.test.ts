84080288da7847ae38248cb315b8d4d6abece9f9
tomholford <tomholford@users.noreply.github.com>
2022-07-18 15:57:44 -0700

    logic: use regex for `whomIsFlag`
    Previously, flags such as '~zod/' would be deemed as valid. This change replaces the conditional check with a regex that satisfies the flag definition of [patp]/[term].
    
    Cherry-picked from a WIP branch.

---
+++ ui/src/logic/utils.test.ts
@@ -0,0 +1,44 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { describe, expect, it } from 'vitest';
+import { whomIsFlag } from './utils';
+
+describe('whomIsFlag', () => {
+  it('passes valid flags', () => {
+    const flags = [
+      '~zod/test',
+      '~zod/test-test',
+      '~zod/test-',
+      '~zod/test1',
+      '~marzod/test',
+      '~marzod/test-test',
+      '~marzod/test-',
+      '~marzod/test1',
+      '~wicdev-wisryt/test',
+      '~wicdev-wisryt/test-test',
+      '~wicdev-wisryt/test-',
+      '~wicdev-wisryt/test1',
+      '~mister-dister-dozzod-dozzod/test',
+      '~mister-dister-dozzod-dozzod/test-test',
+      '~mister-dister-dozzod-dozzod/test-',
+      '~mister-dister-dozzod-dozzod/test1',
+    ];
+
+    flags.forEach((f) => expect(whomIsFlag(f)).toEqual(true));
+  });
+
+  it('fails invalid flags', () => {
+    const flags = [
+      '~',
+      '~zod/',
+      '~/',
+      '~zod/1',
+      '~zod/1test',
+      '~zod/Test',
+      '~zod/-',
+      '~zod/-test',
+      '~sample-planet/test', // invalid patp
+    ];
+
+    flags.forEach((f) => expect(whomIsFlag(f)).toEqual(false));
+  });
+});
