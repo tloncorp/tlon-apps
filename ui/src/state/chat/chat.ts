8aa51dac84c60f6afb3c4cfcf27829250af43345
Hunter Miller <hunter@hmiller.dev>
2022-07-19 12:34:06 -0500

    chat: handling chat-leave

--- ui/src/state/chat/chat.ts
+++ ui/src/state/chat/chat.ts
@@ -191,7 +191,14 @@ export const useChatState = create<ChatState>(
         api.subscribe({
           app: 'chat',
           path: '/briefs',
-          event: (event: unknown) => {
+          event: (event: unknown, mark: string) => {
+            if (mark === 'chat-leave') {
+              get().batchSet((draft) => {
+                delete draft.briefs[event as string];
+              });
+              return;
+            }
+
             const { whom, brief } = event as ChatBriefUpdate;
             get().batchSet((draft) => {
               draft.briefs[whom] = brief;
