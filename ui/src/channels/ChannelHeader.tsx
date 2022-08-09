37451b58e0978e93672f24b658e635b17c7217db
Patrick O'Sullivan <patrick@osullivan.io>
2022-08-09 12:31:37 -0500

    Add new buttons to ChannelHeader for heaps

--- ui/src/channels/ChannelHeader.tsx
+++ ui/src/channels/ChannelHeader.tsx
@@ -1,8 +1,12 @@
 import cn from 'classnames';
 import React from 'react';
+import { useParams } from 'react-router';
 import BubbleIcon from '@/components/icons/BubbleIcon';
 import CaretLeftIcon from '@/components/icons/CaretLeftIcon';
 import EllipsisIcon from '@/components/icons/EllipsisIcon';
+import GridIcon from '@/components/icons/GridIcon';
+import SortIcon from '@/components/icons/SortIcon';
+import ShareIcon from '@/components/icons/ShareIcon';
 import useNavStore from '@/components/Nav/useNavStore';
 import { useIsMobile } from '@/logic/useMedia';
 import { useGroup, useChannel } from '@/state/groups';
@@ -13,8 +17,26 @@ export interface ChannelHeaderProps {
   nest: string;
 }
 
+function ChannelHeaderButton({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex items-center space-x-1 rounded-lg bg-transparent p-2 text-gray-600 hover:bg-gray-50 focus:outline-none focus-visible:ring-2"
+    >
+      {children}
+    </button>
+  );
+}
+
 export default function ChannelHeader({ flag, nest }: ChannelHeaderProps) {
   const group = useGroup(flag);
+  const { app } = useParams();
   const isMobile = useIsMobile();
   const navPrimary = useNavStore((state) => state.navigatePrimary);
   const channel = useChannel(flag, nest);
@@ -22,7 +44,9 @@ export default function ChannelHeader({ flag, nest }: ChannelHeaderProps) {
 
   return (
     <div
-      className={cn('flex h-full items-center border-b-2 border-gray-50 p-2')}
+      className={cn(
+        'flex h-full items-center justify-between border-b-2 border-gray-50 p-2'
+      )}
     >
       <button
         className={cn(
@@ -49,12 +73,39 @@ export default function ChannelHeader({ flag, nest }: ChannelHeaderProps) {
         </div>
       </button>
 
-      <Link
-        className="icon-button ml-auto h-8 w-8 bg-transparent"
-        to={`/groups/${flag}/info/channels`}
-      >
-        <EllipsisIcon className="h-6 w-6" />
-      </Link>
+      {app === 'heap' ? (
+        <div className="flex items-center space-x-12">
+          <div className="flex items-center space-x-2">
+            <ChannelHeaderButton onClick={() => console.log('toggle grid')}>
+              <GridIcon className="-m-1 h-8 w-8" />
+              <span className="font-semibold">Grid</span>
+            </ChannelHeaderButton>
+            <ChannelHeaderButton onClick={() => console.log('toggle sort')}>
+              <SortIcon className="h-6 w-6" />
+              <span className="font-semibold">Sort</span>
+            </ChannelHeaderButton>
+          </div>
+          <div className="flex items-center space-x-3">
+            <ChannelHeaderButton onClick={() => console.log('share')}>
+              <ShareIcon className="h-6 w-6" />
+              <span className="font-semibold">Share</span>
+            </ChannelHeaderButton>
+            <Link
+              className="icon-button h-8 w-8 bg-transparent"
+              to={`/groups/${flag}/info/channels`}
+            >
+              <EllipsisIcon className="h-6 w-6" />
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <Link
+          className="icon-button h-8 w-8 bg-transparent"
+          to={`/groups/${flag}/info/channels`}
+        >
+          <EllipsisIcon className="h-6 w-6" />
+        </Link>
+      )}
     </div>
   );
 }
