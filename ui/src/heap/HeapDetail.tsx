576fb6be5707584f7d6b1100d797491ca1dae40a
rcrdlbl <dan@tlon.io>
2022-08-24 22:22:08 -0400

    mobile view fixes, main nav width fix

--- ui/src/heap/HeapDetail.tsx
+++ ui/src/heap/HeapDetail.tsx
@@ -40,7 +40,7 @@ export default function HeapDetail() {
     >
       <div className="flex flex-wrap lg:h-full lg:flex-nowrap">
         <HeapDetailBody curio={curio} />
-        <div className="mt-5 flex h-full w-96 flex-col border-gray-50 bg-white sm:w-full lg:mt-0 lg:w-72 lg:border-l-2 xl:w-96">
+        <div className="mt-5 flex h-full w-full flex-col border-gray-50 bg-white lg:mt-0 lg:w-72 lg:border-l-2 xl:w-96">
           <HeapDetailSidebarInfo curio={curio} time={time} />
           <HeapDetailComments time={time} />
         </div>
