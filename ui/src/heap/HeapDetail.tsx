6146eebad97f70692b2ebd9e4f7fb60e5aa2c4d9
rcrdlbl <dan@tlon.io>
2022-08-24 23:41:56 -0400

    sidebar info spacing fix, removed elipse menu

--- ui/src/heap/HeapDetail.tsx
+++ ui/src/heap/HeapDetail.tsx
@@ -41,7 +41,7 @@ export default function HeapDetail() {
       <div className="flex flex-wrap lg:h-full lg:flex-nowrap">
         <HeapDetailBody curio={curio} />
         <div className="mt-5 flex h-full w-full flex-col border-gray-50 bg-white lg:mt-0 lg:w-72 lg:border-l-2 xl:w-96">
-          <HeapDetailSidebarInfo curio={curio} time={time} />
+          <HeapDetailSidebarInfo curio={curio} />
           <HeapDetailComments time={time} />
         </div>
       </div>
