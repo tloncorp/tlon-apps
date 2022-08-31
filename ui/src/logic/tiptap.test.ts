5df94977b01f7be9d80717746ce6931b225b179a
tomholford <tomholford@users.noreply.github.com>
2022-08-31 02:50:07 -0700

    test: more coverage
    Import a bunch of tests from the offiical Prosemirror repo to verify our own parser implementation.

--- ui/src/logic/tiptap.test.ts
+++ ui/src/logic/tiptap.test.ts
@@ -1,87 +1,88 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { describe, expect, it } from 'vitest';
+import { JSONContent } from '@tiptap/react';
+import { Inline } from '@/types/content';
 import { parseInline } from './tiptap';
 
-/**
- * To dump JSON to a file
- */
-// import { writeFile } from 'fs';
-// await writeFile('empty.json', JSON.stringify(output), (err) => { if (err) { throw err; }});
-
 describe('parseInline', () => {
-  // This one works with TipTap
-  it('parses empty case', () => {
-    const input = [''];
+  it('empty case', () => {
+    const input: Inline[] = [''];
     const output = parseInline(input);
-    const expected = {
+    const expected: JSONContent = {
       type: 'doc',
       content: [{ type: 'paragraph', content: [{ type: 'text', text: '' }] }],
     };
     expect(output).toEqual(expected);
   });
 
-  /**
-   * This test is green, but shouldn't be; the output JSON is rejected by TipTap
-   * with the following warning:
-   *
-   * react_devtools_backend.js:4026 [tiptap warn]: Invalid content. Passed value: {type: 'doc', content: Array(3)} Error: RangeError: Invalid text node in JSON
-   */
-  it('parses basic case', async () => {
-    // HeapInline[]
-    const input = [
-      'test text',
-      {
-        break: null,
-      },
-      {
-        break: null,
-      },
-      'with ',
-      {
-        bold: ['style'],
-      },
-      {
-        break: null,
-      },
-    ];
-
+  it('single paragraph', () => {
+    const input: Inline[] = ['urbit fixes this'];
     const output = parseInline(input);
+    const expected: JSONContent = {
+      type: 'doc',
+      content: [{ type: 'paragraph', content: [{ type: 'text', text: 'urbit fixes this' }] }],
+    };
+    expect(output).toEqual(expected);
+  });
 
-    // JSONContent
-    const expected = {
+  it('two paragraphs', () => {
+    const input: Inline[] = ['urbit fixes this', 'foo foo'];
+    const output = parseInline(input);
+    const expected: JSONContent = {
       type: 'doc',
       content: [
-        {
-          type: 'paragraph',
-          content: [
-            {
-              type: 'text',
-              text: 'test text',
-            },
-          ],
-        },
-        {
-          type: 'paragraph',
-        },
-        {
-          type: 'paragraph',
-          content: [
-            {
-              type: 'text',
-              text: 'with ',
-            },
-            {
-              type: 'text',
-              marks: [
-                {
-                  type: 'bold',
-                },
-              ],
-            },
-          ],
-        },
+        { type: 'paragraph', content: [{ type: 'text', text: 'urbit fixes this' }] },
+        { type: 'paragraph', content: [{ type: 'text', text: 'foo foo' }] }
       ],
     };
+    expect(output).toEqual(expected);
+  });
+
+  it('two paragraphs with style', () => {
+    const input: Inline[] = [ { "bold": ["foo"] }, { "break": null }, { "break": null }, "foo", { "break": null } ];
+    const output = parseInline(input);
+    const expected: JSONContent = {
+      type: 'doc',
+      content: [
+        { type: "paragraph", content: [{ type: "text", marks: [{ type: "bold" }], text: "foo" }] },
+        { type: "paragraph" },
+        { type: "paragraph", content: [{ type: "text", "text": "foo" }] }
+      ],
+    };
+    expect(output).toEqual(expected);
+  });
+
+  it('element with multiple styles', () => {
+    const input: Inline[] = [ { "italics": [ { "bold": [ "foobar" ] } ] } ];
+    const output = parseInline(input);
+    const expected: JSONContent = {
+      type: 'doc',
+      content: [
+        { type: "doc", content: [ { type: "paragraph", content: [ { type: "text", marks: [ { type: "bold" }, { type: "italic" } ], text: "foobar" } ] } ] }
+      ],
+    };
+    expect(output).toEqual(expected);
+  });
+
+  it('bold', () => {
+    const input: Inline[] = [{ bold: ['foo foo'] }, ' and ', { bold: ['bar baz'] }];
+    const output = parseInline(input);
+    const expected: JSONContent = {
+      type: 'doc',
+      content: [
+        { type: 'paragraph', content: [{ type: 'text', text: 'foo foo', marks: [{ type: 'bold' }] }] },
+        { type: 'paragraph', content: [{ type: 'text', text: ' and ' }] },
+        { type: 'paragraph', content: [{ type: 'text', text: 'bar baz', marks: [{ type: 'bold' }] }] },
+      ],
+    };
+    expect(output).toEqual(expected);
+  });
+
+
+  it('parses basic case', async () => {
+    const input: Inline[] = [ 'test text', { break: null, }, { break: null, }, 'with ', { bold: ['style'], }, { break: null, }, ];
+    const output = parseInline(input);
+    const expected: JSONContent = { type: 'doc', content: [ { type: 'paragraph', content: [ { type: 'text', text: 'test text', }, ], }, { type: 'paragraph', }, { type: 'paragraph', content: [ { type: 'text', text: 'with ', }, { type: 'text', text: 'style', marks: [ { type: 'bold', }, ], }, ], }, ], };
     expect(output).toEqual(expected);
   });
 });
