554de0581fb959ed6d62d7f18d7ed9686ef02b9d
rcrdlbl <dan@tlon.io>
2022-08-31 21:59:09 -0400

    first run: nav animation

--- ui/src/components/Nav/Nav.tsx
+++ ui/src/components/Nav/Nav.tsx
@@ -1,18 +1,26 @@
-import React, { ReactElement, useEffect } from 'react';
-import Sidebar from '../Sidebar/Sidebar';
-import GroupSidebar from '../../groups/GroupSidebar/GroupSidebar';
-import MessagesSidebar from '../../dms/MessagesSidebar';
+import React, { useEffect, useState } from 'react';
+import { animated, useSpring } from '@react-spring/web';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import GroupSidebar from '@/groups/GroupSidebar/GroupSidebar';
+import MessagesSidebar from '@/dms/MessagesSidebar';
+import useIsChat from '@/logic/useIsChat';
+import { useIsMobile } from '@/logic/useMedia';
 import useNavStore from './useNavStore';
-import useIsChat from '../../logic/useIsChat';
 
 export default function Nav() {
-  const navLocation = useNavStore((s) => s.primary);
+  const navLocation = useNavStore((s) => s.primary) as string;
+  const [slid, setSlid] = useState(navLocation === 'group' ? true : false);
+  const isMobile = useIsMobile();
   const isChat = useIsChat();
-  let selectedSidebar: ReactElement | null = isChat ? (
-    <MessagesSidebar />
-  ) : (
-    <Sidebar />
-  );
+  const springStyles = useSpring({
+    config: {
+      mass: 1,
+      stiffness: 2880,
+      damping: 120,
+    },
+    from: { x: slid ? 0 : -256 },
+    x: slid ? -256 : 0,
+  });
 
   useEffect(() => {
     if (isChat && (navLocation === 'group' || navLocation === 'main')) {
@@ -20,15 +28,28 @@ export default function Nav() {
     }
   }, [isChat, navLocation]);
 
-  if (navLocation === 'main') {
-    selectedSidebar = <Sidebar />;
-  } else if (navLocation === 'dm') {
-    selectedSidebar = <MessagesSidebar />;
-  } else if (navLocation === 'group') {
-    selectedSidebar = <GroupSidebar />;
-  } else {
-    selectedSidebar = null;
+  useEffect(() => {
+    if (navLocation === 'group') {
+      setSlid(true);
+    } else {
+      setSlid(false);
+    }
+  }, [navLocation]);
+
+  if (navLocation === 'dm') {
+    return <MessagesSidebar />;
   }
 
-  return selectedSidebar;
+  if (navLocation === 'group' || navLocation === 'main') {
+    return (
+      <div className="h-full w-64 flex-none overflow-hidden border-r-2 border-gray-50 bg-white">
+        <animated.div className="flex w-[201%]" style={springStyles}>
+          <Sidebar />
+          <GroupSidebar />
+        </animated.div>
+      </div>
+    );
+  }
+
+  return null;
 }
--- ui/src/groups/GroupSidebar/GroupSidebar.tsx
+++ ui/src/groups/GroupSidebar/GroupSidebar.tsx
@@ -35,7 +35,7 @@ export default function GroupSidebar() {
   }
 
   return (
-    <nav className="slide-in flex h-full w-64 flex-none flex-col overflow-hidden border-r-2 border-gray-50 bg-white sm:p-2">
+    <nav className="flex h-full w-64 flex-none flex-col bg-white sm:p-2">
       <header className="flex-none px-2 py-1 sm:p-0">
         <button
           className="default-focus flex w-full items-center rounded-lg p-2 text-base font-semibold text-gray-600 hover:bg-gray-50"
