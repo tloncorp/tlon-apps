a29019f0ca49b8f1fba7fc115600e2ef5d5cbb72
James Acklin <james@tlon.io>
2022-09-07 13:03:34 -0400

    groups/nav: use color2k
    fixes tloncorp/homestead#747

--- ui/src/components/Sidebar/SidebarItem.tsx
+++ ui/src/components/Sidebar/SidebarItem.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import Color from 'color';
+import { mix } from 'color2k';
 import React, {
   ButtonHTMLAttributes,
   PropsWithChildren,
@@ -74,8 +74,8 @@ export default function SidebarItem({
       return {
         backgroundColor:
           currentTheme === 'dark'
-            ? Color(highlight).mix(Color('black'), 0.7)
-            : Color(highlight).mix(Color('white'), 0.8),
+            ? mix(highlight, 'black', 0.7)
+            : mix(highlight, 'white', 0.85),
       };
     return null;
   };
