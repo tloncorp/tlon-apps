31f434ecd6888aeb760278d735c39fa4f335d145
James Acklin <james@tlon.io>
2022-09-08 15:55:53 -0400

    groups/channel-create: add colors + fix icons for channel types

--- ui/src/channels/ChannelTypeSelector.tsx
+++ ui/src/channels/ChannelTypeSelector.tsx
@@ -3,13 +3,14 @@ import { ChannelType, NewChannelFormSchema } from '@/types/groups';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 import BubbleIcon from '@/components/icons/BubbleIcon';
-import LinkIcon from '@/components/icons/LinkIcon';
+import ShapesIcon from '@/components/icons/ShapesIcon';
 import NotebookIcon from '@/components/icons/NotebookIcon';
 
 interface ChannelTypeMetadata {
   title: string;
   icon: React.ReactElement;
   description: string;
+  color: string;
 }
 
 const CHANNEL_TYPE: Record<ChannelType, ChannelTypeMetadata> = {
@@ -17,16 +18,19 @@ const CHANNEL_TYPE: Record<ChannelType, ChannelTypeMetadata> = {
     icon: <BubbleIcon className="h-6 w-6 text-gray-600" />,
     title: 'Chat',
     description: 'A simple, standard text chat',
+    color: 'blue',
   },
   heap: {
-    icon: <LinkIcon className="h-6 w-6 text-gray-600" />,
+    icon: <ShapesIcon className="h-6 w-6 text-gray-600" />,
     title: 'Collection',
     description: 'Gather, connect, and arrange rich media',
+    color: 'green',
   },
   diary: {
     icon: <NotebookIcon className="h-6 w-6 text-gray-600" />,
     title: 'Notebook',
     description: 'Longform publishing and discussion',
+    color: 'red',
   },
 };
 
@@ -35,20 +39,24 @@ interface ChannelTypeSelectionProps {
 }
 
 function ChannelTypeSelection({ type }: ChannelTypeSelectionProps) {
-  const { title, description, icon } = CHANNEL_TYPE[type];
+  const { title, description, icon, color } = CHANNEL_TYPE[type];
   const { register, watch } = useFormContext<NewChannelFormSchema>();
   const selected = type === watch('type');
 
   return (
     <label
       className={cn(
-        'flex cursor-pointer items-center justify-between rounded-lg border-2 p-2',
-        selected ? 'border-gray-200 bg-gray-50' : 'border-gray-100 bg-white'
+        'flex cursor-pointer items-center justify-between rounded-lg p-2',
+        selected ? `bg-${color}-soft` : 'bg-white'
       )}
     >
       <div className="flex w-full flex-col">
         <div className="flex flex-row items-center space-x-2">
-          <div className="p-2">{icon}</div>
+          <div
+            className={cn('rounded p-2 mix-blend-multiply', `bg-${color}-soft`)}
+          >
+            {icon}
+          </div>
           <div className="flex w-full flex-col justify-start text-left">
             <span className="font-semibold">{title}</span>
             <span className="text-sm font-medium text-gray-600">
@@ -63,13 +71,6 @@ function ChannelTypeSelection({ type }: ChannelTypeSelectionProps) {
         type="radio"
         value={type}
       />
-      <div className="flex items-center">
-        {selected ? (
-          <div className="h-4 w-4 rounded-xl border-4 border-gray-400" />
-        ) : (
-          <div className="h-4 w-4 rounded-xl border-2 border-gray-200" />
-        )}
-      </div>
     </label>
   );
 }
