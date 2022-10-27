cb9c3c3b4eff7510ba399fb7332011050dfbbb02
Hunter Miller <hunter@hmiller.dev>
2022-10-27 16:04:14 -0500

    nav: adding missing routes to mobile nav

--- ui/src/components/Sidebar/MobileSidebar.tsx
+++ ui/src/components/Sidebar/MobileSidebar.tsx
@@ -1,13 +1,19 @@
+import cn from 'classnames';
 import React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation, useMatch } from 'react-router';
 import { useNotifications } from '@/notifications/useNotifications';
 import NavTab from '../NavTab';
 import GroupIcon from '../icons/GroupIcon';
 import ActivityIndicator from './ActivityIndicator';
 import AsteriskIcon from '../icons/Asterisk16Icon';
+import Avatar from '../Avatar';
+import AddIcon16 from '../icons/Add16Icon';
+import MagnifyingGlassIcon from '../icons/MagnifyingGlassIcon';
 
 export default function MobileSidebar() {
   const { count } = useNotifications();
+  const location = useLocation();
+  const isProfile = useMatch('/profile/*');
 
   return (
     <section className="fixed inset-0 z-40 flex h-full w-full flex-col border-r-2 border-gray-50 bg-white">
@@ -15,22 +21,35 @@ export default function MobileSidebar() {
       <footer className="flex-none border-t-2 border-gray-50">
         <nav>
           <ul className="flex justify-items-stretch">
-            <NavTab to="/">
+            <NavTab to="/" aria-label="Groups">
               <GroupIcon className="mb-0.5 h-6 w-6" />
-              Groups
             </NavTab>
-            <NavTab to="/notifications">
+            <NavTab to="/notifications" aria-label="Notifications">
               <ActivityIndicator count={count} className="mb-0.5" />
-              Notifications
+            </NavTab>
+            <NavTab to="/groups/new" state={{ backgroundLocation: location }}>
+              <div className="icon-button bg-blue text-white dark:bg-blue-900 dark:text-blue">
+                <AddIcon16 className="h-4 w-4" />
+              </div>
+            </NavTab>
+            <NavTab to="/find">
+              <MagnifyingGlassIcon className="mb-0.5 h-6 w-6" />
             </NavTab>
             <NavTab
               linkClass="no-underline"
               href="https://github.com/tloncorp/homestead/issues/new?assignees=&amp;labels=bug&amp;template=bug_report.md&amp;title=groups:"
               target="_blank"
               rel="noreferrer"
+              aria-label="Submit Issue"
             >
               <AsteriskIcon className="mb-0.5 h-6 w-6" />
-              Submit Issue
+            </NavTab>
+            <NavTab to="/profile/edit" aria-label="Profile">
+              <Avatar
+                size="xs"
+                ship={window.our}
+                className={cn('mb-0.5', !isProfile && 'opacity-70 grayscale')}
+              />
             </NavTab>
           </ul>
         </nav>
--- ui/src/groups/GroupSidebar/MobileGroupSidebar.tsx
+++ ui/src/groups/GroupSidebar/MobileGroupSidebar.tsx
@@ -6,6 +6,7 @@ import HashIcon from '@/components/icons/HashIcon';
 import AsteriskIcon from '@/components/icons/Asterisk16Icon';
 import NavTab from '@/components/NavTab';
 import ActivityIndicator from '@/components/Sidebar/ActivityIndicator';
+import ElipsisIcon from '@/components/icons/EllipsisIcon';
 import GroupAvatar from '../GroupAvatar';
 
 export default function MobileGroupSidebar() {
@@ -21,30 +22,30 @@ export default function MobileGroupSidebar() {
       <footer className="mt-auto flex-none border-t-2 border-gray-50">
         <nav>
           <ul className="flex items-center">
-            <NavTab to="." end>
+            <NavTab to="." end aria-label="Channels">
               <HashIcon className="mb-0.5 h-6 w-6" />
-              Channels
             </NavTab>
-            <NavTab to="./info">
+            <NavTab to="./info" aria-label="Group Info">
               <GroupAvatar
                 {...group?.meta}
                 size="h-6 w-6"
                 className={cn('mb-0.5', !match && 'opacity-50')}
               />
-              Group
             </NavTab>
-            <NavTab to="./activity">
+            <NavTab to="./activity" aria-label="Activity">
               <ActivityIndicator count={activityCount} className="mb-0.5" />
-              Activity
             </NavTab>
             <NavTab
               linkClass="flex-1 no-underline"
               href="https://github.com/tloncorp/homestead/issues/new?assignees=&amp;labels=bug&amp;template=bug_report.md&amp;title=groups:"
               target="_blank"
               rel="noreferrer"
+              aria-label="Submit Issue"
             >
               <AsteriskIcon className="mb-0.5 h-6 w-6" />
-              Submit Issue
+            </NavTab>
+            <NavTab to="./actions" aria-label="Group Actions">
+              <ElipsisIcon className="mb-0.5 h-6 w-6" />
             </NavTab>
           </ul>
         </nav>
