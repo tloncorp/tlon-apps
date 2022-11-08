43a912148cba5e68be8d153886520ad3dbceaf58
Hunter Miller <hunter@hmiller.dev>
2022-11-08 11:27:03 -0600

    Update ui/src/logic/tiptap.ts

--- ui/src/logic/tiptap.ts
+++ ui/src/logic/tiptap.ts
@@ -279,7 +279,7 @@ export function JSONToInlines(
           return [{ break: null }];
         }
         const isContentFinal = idx === json.content.length - 1;
-        const isBreakDirectlyBefore = memo[idx - 1] && typeof memo[idx - 1] !== 'string' && memo[idx-1].hasOwnProperty('break');
+        const isBreakDirectlyBefore = memo[idx - 1] && typeof memo[idx - 1] !== 'string' && 'break' in memo[idx-1];
         if (isContentFinal && !isBreakDirectlyBefore) {
           return memo.concat(JSONToInlines(c, limitNewlines), [
             { break: null },
