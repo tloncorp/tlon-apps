aaf3046084394e8b287c0d5f842b01c835e04fb4
rcrdlbl <dan@tlon.io>
2022-12-08 18:43:41 -0500

    actually using the group privacy

--- ui/src/groups/GroupAdmin/GroupInfo.tsx
+++ ui/src/groups/GroupAdmin/GroupInfo.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { capitalize } from 'lodash';
 import { Helmet } from 'react-helmet';
 import { ViewProps } from '@/types/groups';
+import useGroupPrivacy from '@/logic/useGroupPrivacy';
 import { useRouteGroup, useGroup, useAmAdmin } from '../../state/groups/groups';
 import GroupAvatar from '../GroupAvatar';
 import GroupInfoEditor from './GroupInfoEditor';
@@ -9,6 +11,7 @@ import GroupMemberManager from './GroupMemberManager';
 export default function GroupInfo({ title }: ViewProps) {
   const flag = useRouteGroup();
   const group = useGroup(flag);
+  const { privacy } = useGroupPrivacy(flag);
 
   const isAdmin = useAmAdmin(flag);
   if (isAdmin) {
@@ -31,8 +34,9 @@ export default function GroupInfo({ title }: ViewProps) {
         <GroupAvatar {...meta} size="h-20 w-20" />
         <div className="my-4 text-center">
           <h2 className="center mb-2 font-semibold">{meta.title}</h2>
-          {/* Current group meta object doesn't contain public/private info  */}
-          <h3 className="text-base text-gray-600">Private Group</h3>
+          <h3 className="text-base text-gray-600">
+            {capitalize(privacy)} Group
+          </h3>
         </div>
         <p className="w-full leading-5">{meta.description}</p>
       </div>
