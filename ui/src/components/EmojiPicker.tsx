8f6ce33191f942a1469cef6b0085cd064fc3e992
rcrdlbl <dan@tlon.io>
2022-12-08 11:53:40 -0500

    fixed emoji picker screen collision issues

--- ui/src/components/EmojiPicker.tsx
+++ ui/src/components/EmojiPicker.tsx
@@ -9,11 +9,13 @@ import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
 interface EmojiPickerProps extends Record<string, any> {
   open: boolean;
   setOpen: (open: boolean) => void;
+  children: React.ReactChild;
 }
 
 export default function EmojiPicker({
   open,
   setOpen,
+  children,
   ...props
 }: EmojiPickerProps) {
   const { data, load } = useEmoji();
@@ -27,21 +29,30 @@ export default function EmojiPicker({
 
   return (
     <Popover.Root open={open} onOpenChange={setOpen}>
-      <Popover.Anchor className={isMobile ? 'fixed inset-x-0 top-12' : ''} />
-      <Popover.Content side={isMobile ? 'bottom' : 'left'} sideOffset={40}>
-        {data ? (
-          <Picker
-            data={data}
-            autoFocus={isMobile}
-            perLine={isMobile ? mobilePerLineCount : 9}
-            previewPosition="none"
-            {...props}
-          />
-        ) : (
-          <div className="flex h-96 w-72 items-center justify-center">
-            <LoadingSpinner className="h-6 w-6" />
-          </div>
-        )}
+      <Popover.Trigger>{children}</Popover.Trigger>
+      {isMobile && (
+        <Popover.Anchor className={isMobile ? 'fixed inset-x-0 top-12' : ''} />
+      )}
+      <Popover.Content
+        side={isMobile ? 'bottom' : 'left'}
+        sideOffset={40}
+        collisionPadding={15}
+      >
+        <div className="flex h-96 w-72 items-center justify-center">
+          {data ? (
+            <Picker
+              data={data}
+              autoFocus={isMobile}
+              perLine={isMobile ? mobilePerLineCount : 9}
+              previewPosition="none"
+              {...props}
+            />
+          ) : (
+            <div className="flex h-96 w-72 items-center justify-center">
+              <LoadingSpinner className="h-6 w-6" />
+            </div>
+          )}
+        </div>
       </Popover.Content>
     </Popover.Root>
   );
