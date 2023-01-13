2e8693125f6f4422eaf46f0470475bfc2ca0650d
tomholford <tomholford@users.noreply.github.com>
2023-01-13 09:54:16 -0800

    devex: why-did-you-render

---
+++ ui/src/wdyr.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="@welldone-software/why-did-you-render" />
+
+import * as React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import wdyr from '@welldone-software/why-did-you-render';
+
+wdyr(React, {
+  include: [/^ChannelHeader/, /^HeapChannel/, /^App/, /^RoutedApp/],
+  exclude: [/^BrowserRouter/, /^Link/, /^Route/],
+  trackHooks: true,
+  trackAllPureComponents: true,
+});
