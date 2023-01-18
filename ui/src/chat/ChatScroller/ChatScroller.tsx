bb0e1e2d2b6707c97a97350607e935571845bcf8
Patrick O'Sullivan <patrick@osullivan.io>
2023-01-18 10:47:28 -0600

    chat: add placeholder for chat scroller

--- ui/src/chat/ChatScroller/ChatScroller.tsx
+++ ui/src/chat/ChatScroller/ChatScroller.tsx
@@ -28,6 +28,7 @@ import { IChatScroller } from './IChatScroller';
 import ChatMessage from '../ChatMessage/ChatMessage';
 import { ChatInfo, useChatStore } from '../useChatStore';
 import ChatNotice from '../ChatNotice';
+import ChatScrollerPlaceholder from '../ChatScoller/ChatScrollerPlaceholder';
 
 interface CreateRendererParams {
   messages: BigIntOrderedMap<ChatWrit>;
@@ -274,7 +275,11 @@ export default function ChatScroller({
   );
 
   if (keys.length === 0) {
-    return null;
+    return (
+      <div className="h-full">
+        <ChatScrollerPlaceholder count={30} />
+      </div>
+    );
   }
 
   return (
