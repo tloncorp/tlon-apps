3876252951d0345465319905dd5265ba3fa8e540
tomholford <tomholford@users.noreply.github.com>
2023-01-31 06:16:44 -0800

    devex: replace garden-dev with desk-dev

--- ops/garden.sh
+++ ops/garden.sh
@@ -11,6 +11,8 @@ SHIP="${1:-zod}"
 URBIT_REPO_PATH=~/dev/urbit/urbit
 # path to landscape-apps git repo
 REPO_PATH=~/dev/urbit/landscape
+# path to landscape git repo
+LANDSCAPE_PATH=~/dev/urbit/landscape
 # path to directory of fake ship piers
 SHIP_PATH=~/dev/urbit/ships
 # name of src desk in repo
@@ -19,5 +21,5 @@ SRC_DESK="${2:-desk}"
 DEST_DESK="${3:-garden}"
 
 rsync -avL --delete $URBIT_REPO_PATH/pkg/base-dev/* $SHIP_PATH/$SHIP/$DEST_DESK/
-rsync -avL $URBIT_REPO_PATH/pkg/garden-dev/* $SHIP_PATH/$SHIP/$DEST_DESK/
+rsync -avL $LANDSCAPE_PATH/desk-dev/* $SHIP_PATH/$SHIP/$DEST_DESK/
 rsync -avL $REPO_PATH/$SRC_DESK/* $SHIP_PATH/$SHIP/$DEST_DESK/
