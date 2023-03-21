c2205b7c46345412ca71a30f670f8a4e713703b0
James Acklin <james.acklin@gmail.com>
2023-03-21 17:02:55 -0400

    groups: restore membermanager height

--- ui/src/groups/GroupAdmin/GroupAdmin.tsx
+++ ui/src/groups/GroupAdmin/GroupAdmin.tsx
@@ -8,45 +8,43 @@ export default function GroupAdmin() {
   const isAdmin = useAmAdmin(flag);
 
   return (
-    <section className="flex h-full w-full flex-1 grow flex-col overflow-y-auto bg-gray-50">
-      <div className="bg-gray-50 p-6">
-        {isAdmin ? (
-          <header className="card mb-4 p-2">
-            <nav>
-              <ul className="flex items-center font-semibold text-gray-400">
-                <li>
-                  <NavLink
-                    to="../info"
-                    end
-                    className={({ isActive }) =>
-                      cn(
-                        'default-focus inline-block rounded-md p-2 hover:bg-gray-50',
-                        isActive && 'text-gray-800'
-                      )
-                    }
-                  >
-                    Group Info
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="members"
-                    className={({ isActive }) =>
-                      cn(
-                        'default-focus inline-block rounded-md p-2 hover:bg-gray-50',
-                        isActive && 'text-gray-800'
-                      )
-                    }
-                  >
-                    Members
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-          </header>
-        ) : null}
-        <Outlet />
-      </div>
+    <section className="flex h-full w-full flex-1 grow flex-col overflow-y-auto bg-gray-50 p-6">
+      {isAdmin ? (
+        <header className="card mb-4 p-2">
+          <nav>
+            <ul className="flex items-center font-semibold text-gray-400">
+              <li>
+                <NavLink
+                  to="../info"
+                  end
+                  className={({ isActive }) =>
+                    cn(
+                      'default-focus inline-block rounded-md p-2 hover:bg-gray-50',
+                      isActive && 'text-gray-800'
+                    )
+                  }
+                >
+                  Group Info
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="members"
+                  className={({ isActive }) =>
+                    cn(
+                      'default-focus inline-block rounded-md p-2 hover:bg-gray-50',
+                      isActive && 'text-gray-800'
+                    )
+                  }
+                >
+                  Members
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      ) : null}
+      <Outlet />
     </section>
   );
 }
