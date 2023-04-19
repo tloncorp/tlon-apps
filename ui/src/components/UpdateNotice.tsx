eab294cdfa909539312a5f87213c5c81fe345114
Hunter Miller <hunter@hmiller.dev>
2023-04-19 10:02:08 -0500

    app: better communication about updates

--- ui/src/components/UpdateNotice.tsx
+++ ui/src/components/UpdateNotice.tsx
@@ -4,6 +4,8 @@ import AsteriskIcon from '@/components/icons/Asterisk16Icon';
 import { isTalk } from '@/logic/utils';
 import useKilnState, { usePike } from '@/state/kiln';
 
+type UpdateStatus = 'none' | 'glob' | 'sw';
+
 export default function UpdateNotice() {
   const appName = isTalk ? 'Talk' : 'Groups';
   const {
@@ -12,7 +14,7 @@ export default function UpdateNotice() {
   } = useRegisterSW();
   const pike = usePike(appName.toLocaleLowerCase());
   const [baseHash, setBaseHash] = useState('');
-  const [needsUpdate, setNeedsUpdate] = useState(false);
+  const [updateStatus, setUpdateStatus] = useState<UpdateStatus>('none');
 
   const fetchPikes = useCallback(async () => {
     try {
@@ -33,13 +35,18 @@ export default function UpdateNotice() {
   }, [fetchPikes]);
 
   useEffect(() => {
-    if (pike && pike.hash !== baseHash && baseHash !== '' && !needsUpdate) {
-      setNeedsUpdate(true);
+    if (needRefresh) {
+      setUpdateStatus('sw');
     }
-    if (needRefresh && !needsUpdate) {
-      setNeedsUpdate(true);
+    if (
+      pike &&
+      pike.hash !== baseHash &&
+      baseHash !== '' &&
+      updateStatus === 'none'
+    ) {
+      setUpdateStatus('glob');
     }
-  }, [pike, baseHash, needsUpdate, needRefresh]);
+  }, [pike, baseHash, updateStatus, needRefresh]);
 
   const onClick = useCallback(() => {
     if (needRefresh) {
@@ -49,7 +56,7 @@ export default function UpdateNotice() {
     }
   }, [needRefresh, updateServiceWorker]);
 
-  if (!needsUpdate) {
+  if (updateStatus === 'none') {
     return null;
   }
 
@@ -57,12 +64,17 @@ export default function UpdateNotice() {
     <div className="z-50 flex items-center justify-between bg-yellow py-1 px-2 text-sm font-medium text-black dark:text-white">
       <div className="flex items-center">
         <AsteriskIcon className="mr-3 h-4 w-4" />
-        <span className="mr-1">
-          {appName} has updated in the background. Please reload.
-        </span>
+        {updateStatus === 'glob' && (
+          <span className="mr-1">
+            {appName} has updated in the background. Please reload.
+          </span>
+        )}
+        {updateStatus === 'sw' && (
+          <span className="mr-1">{appName} has updates waiting.</span>
+        )}
       </div>
       <button className="py-1 px-2" onClick={onClick}>
-        Reload
+        {updateStatus === 'sw' ? 'Update' : 'Reload'}
       </button>
     </div>
   );
