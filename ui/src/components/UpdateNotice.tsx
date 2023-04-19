49193f73dc00b0a6326ac53a972d2ee84af4d6c6
Hunter Miller <hunter@hmiller.dev>
2023-04-19 11:35:53 -0500

    updates: switch to sw only checking and update language

--- ui/src/components/UpdateNotice.tsx
+++ ui/src/components/UpdateNotice.tsx
@@ -1,62 +1,51 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import { useRegisterSW } from 'virtual:pwa-register/react';
 import AsteriskIcon from '@/components/icons/Asterisk16Icon';
 import { isTalk } from '@/logic/utils';
-import useKilnState, { usePike } from '@/state/kiln';
 
-type UpdateStatus = 'none' | 'glob' | 'sw';
+const updateInterval =
+  import.meta.env.MODE === 'sw' ? 10 * 1000 : 5 * 60 * 1000;
 
 export default function UpdateNotice() {
   const appName = isTalk ? 'Talk' : 'Groups';
   const {
     needRefresh: [needRefresh],
     updateServiceWorker,
-  } = useRegisterSW();
-  const pike = usePike(appName.toLocaleLowerCase());
-  const [baseHash, setBaseHash] = useState('');
-  const [updateStatus, setUpdateStatus] = useState<UpdateStatus>('none');
+  } = useRegisterSW({
+    onRegisteredSW(swUrl, r) {
+      if (!r) {
+        return;
+      }
 
-  const fetchPikes = useCallback(async () => {
-    try {
-      await useKilnState.getState().fetchPikes();
-    } catch (e) {
-      console.error(e);
-    }
-  }, []);
+      setInterval(async () => {
+        if (r.installing || !navigator) {
+          return;
+        }
 
-  useEffect(() => {
-    if (pike && baseHash === '') {
-      setBaseHash(pike.hash);
-    }
-  }, [pike, baseHash]);
+        if ('connection' in navigator && !navigator.onLine) {
+          return;
+        }
 
-  useEffect(() => {
-    setInterval(fetchPikes, 1000 * 60 * 5);
-  }, [fetchPikes]);
+        const resp = await fetch(swUrl, {
+          cache: 'no-store',
+          headers: {
+            cache: 'no-store',
+            'cache-control': 'no-cache',
+          },
+        });
 
-  useEffect(() => {
-    if (needRefresh) {
-      setUpdateStatus('sw');
-    }
-    if (
-      pike &&
-      pike.hash !== baseHash &&
-      baseHash !== '' &&
-      updateStatus === 'none'
-    ) {
-      setUpdateStatus('glob');
-    }
-  }, [pike, baseHash, updateStatus, needRefresh]);
+        if (resp?.status === 200) {
+          await r.update();
+        }
+      }, updateInterval);
+    },
+  });
 
   const onClick = useCallback(() => {
-    if (needRefresh) {
-      updateServiceWorker(true);
-    } else {
-      window.location.reload();
-    }
-  }, [needRefresh, updateServiceWorker]);
+    updateServiceWorker(true);
+  }, [updateServiceWorker]);
 
-  if (updateStatus === 'none') {
+  if (!needRefresh) {
     return null;
   }
 
@@ -64,17 +53,13 @@ export default function UpdateNotice() {
     <div className="z-50 flex items-center justify-between bg-yellow py-1 px-2 text-sm font-medium text-black dark:text-white">
       <div className="flex items-center">
         <AsteriskIcon className="mr-3 h-4 w-4" />
-        {updateStatus === 'glob' && (
-          <span className="mr-1">
-            {appName} has updated in the background. Please reload.
-          </span>
-        )}
-        {updateStatus === 'sw' && (
-          <span className="mr-1">{appName} has updates waiting.</span>
-        )}
+        <span className="mr-1">
+          {appName} has updated in the background. Please click update to load
+          the latest version.
+        </span>
       </div>
       <button className="py-1 px-2" onClick={onClick}>
-        {updateStatus === 'sw' ? 'Update' : 'Reload'}
+        Update
       </button>
     </div>
   );
