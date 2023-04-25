d666e504a47cf55555809b3793e5ae85211c5858
Hunter Miller <hunter@hmiller.dev>
2023-04-25 14:41:23 -0500

    ops: ensure we pull from master

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -19,9 +19,9 @@ cmds='
 source_repo=$(mktemp --dry-run /tmp/repo.janeway.XXXXXXXXX)
 git clone --depth 1 --branch '$ref' git@github.com:'$repo'.git $source_repo
 urbit_repo=$(mktemp --dry-run /tmp/repo.urbit.XXXXXXXXX)
-git clone --depth 1 git@github.com:urbit/urbit.git $urbit_repo
+git clone --depth 1 --branch master git@github.com:urbit/urbit.git $urbit_repo
 landscape_repo=$(mktemp --dry-run /tmp/repo.landscape.XXXXXXXXX)
-git clone --depth 1 git@github.com:tloncorp/landscape.git $landscape_repo
+git clone --depth 1 --branch master git@github.com:tloncorp/landscape.git $landscape_repo
 cd $source_repo
 cd /home/urb || return
 curl -s --data '\''{"source":{"dojo":"+hood/mount %'$desk'"},"sink":{"app":"hood"}}'\'' http://localhost:12321
