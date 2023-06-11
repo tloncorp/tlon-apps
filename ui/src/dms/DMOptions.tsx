3315acef73ea7055949623718642ba3c0392ad28
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-06-10 19:55:27 -0700

    talk: fix inability to leave group channel

--- ui/src/dms/DMOptions.tsx
+++ ui/src/dms/DMOptions.tsx
@@ -7,7 +7,7 @@ import Dialog from '@/components/Dialog';
 import EllipsisIcon from '@/components/icons/EllipsisIcon';
 import { useChatState, usePinned } from '@/state/chat';
 import BulletIcon from '@/components/icons/BulletIcon';
-import { whomIsMultiDm } from '@/logic/utils';
+import { whomIsDm, whomIsMultiDm } from '@/logic/utils';
 import useIsChannelUnread from '@/logic/useIsChannelUnread';
 import DmInviteDialog from './DmInviteDialog';
 
@@ -48,8 +48,10 @@ export default function DmOptions({
     navigate('/');
     if (whomIsMultiDm(whom)) {
       await useChatState.getState().multiDmRsvp(whom, false);
-    } else {
+    } else if (whomIsDm(whom)) {
       await useChatState.getState().dmRsvp(whom, false);
+    } else {
+      await useChatState.getState().leaveChat(whom);
     }
   };
   const closeDialog = () => {
