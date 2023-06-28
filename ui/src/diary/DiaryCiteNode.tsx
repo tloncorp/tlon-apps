e1b563513b9309416e3167c43a3950892145afa4
James Acklin <james.acklin@gmail.com>
2023-06-28 09:02:07 -0400

    notebook: rearrange cite block editor

--- ui/src/diary/DiaryCiteNode.tsx
+++ ui/src/diary/DiaryCiteNode.tsx
@@ -13,14 +13,19 @@ function DiaryCiteComponent(props: NodeViewProps) {
 
   return (
     <NodeViewWrapper>
-      <div className="my-4">
-        <div className="mb-2 rounded-xl bg-gray-50 p-3">
+      <div className="not-prose my-4">
+        <div className="rounded-xl bg-gray-100 p-3">
+          {cite && (
+            <div className="mb-2 text-base">
+              <ContentReference cite={cite} />
+            </div>
+          )}
           <div className="input flex items-center space-x-3 rounded-lg bg-white p-2">
             <Sig16Icon className="h-4 w-4" />
             <input
               className="input-transparent w-full flex-1 bg-transparent leading-5"
               {...bind}
-              placeholder="Add an urbit reference"
+              placeholder="Paste an urbit reference"
             />
             <button
               title="Remove"
@@ -31,7 +36,6 @@ function DiaryCiteComponent(props: NodeViewProps) {
             </button>
           </div>
         </div>
-        {cite && <ContentReference cite={cite} />}
       </div>
     </NodeViewWrapper>
   );
