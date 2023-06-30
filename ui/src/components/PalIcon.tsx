6cf47d5853d98543ea5659aa777792d4a534fed5
fang <git@fang.io>
2023-06-30 12:02:47 +0200

    chat: remove unused import from pal icon component

--- ui/src/components/PalIcon.tsx
+++ ui/src/components/PalIcon.tsx
@@ -1,7 +1,6 @@
 import { useMemo } from 'react';
 import _ from 'lodash';
 import { usePals } from '@/state/pals';
-import * as Tooltip from '@radix-ui/react-tooltip';
 
 const outgoingSvg = (
   <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
