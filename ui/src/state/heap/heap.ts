090d7416972f97514788060c5352be18325be8c4
~latter-bolden <brian@tlon.io>
2023-07-31 21:36:52 -0400

    merge previous work with added state functions

--- ui/src/state/heap/heap.ts
+++ ui/src/state/heap/heap.ts
@@ -23,6 +23,8 @@ import {
   CurioHeart,
   HeapUpdate,
   HeapCurioWithComments,
+  HeapBriefs,
+  Stash,
 } from '@/types/heap';
 import api from '@/api';
 import { nestToFlag, canWriteChannel, restoreMap } from '@/logic/utils';
@@ -37,7 +39,7 @@ import { useGroup, useVessel } from '../groups';
 import { createState } from '../base';
 import useSchedulerStore from '../scheduler';
 
-const CURIO_PAGE_SIZE = 2;
+const CURIO_PAGE_SIZE = 250;
 
 setAutoFreeze(false);
 
@@ -78,6 +80,285 @@ function getTime() {
   return decToUd(unixToDa(Date.now()).toString());
 }
 
+function useBriefsNew(): HeapBriefs {
+  const emptyBriefs = useMemo(() => ({} as HeapBriefs), []);
+
+  const { data } = useReactQuerySubscription({
+    queryKey: ['heap', 'briefs'],
+    app: 'heap',
+    path: '/briefs',
+    scry: '/breifs',
+  });
+
+  return data ? (data as HeapBriefs) : emptyBriefs;
+}
+
+export function useHeapBrief(flag: HeapFlag) {
+  const briefs = useBriefsNew();
+  return briefs[flag];
+}
+
+export function useStash(): Stash {
+  const emptyStash = useMemo(() => ({} as Stash), []);
+  const { data } = useReactQuerySubscription({
+    queryKey: ['heap', 'stash'],
+    app: 'heap',
+    path: '/ui',
+    scry: '/stash',
+  });
+
+  return data ? (data as Stash) : emptyStash;
+}
+
+export function useHeapNew(flag: HeapFlag) {
+  const stash = useStash();
+  return stash[flag];
+}
+
+export function useHeapPermsNew(flag: HeapFlag) {
+  const defaultPerms = useMemo(() => ({ writers: [] }), []);
+  const stash = useStash();
+
+  if (!stash) {
+    return defaultPerms;
+  }
+
+  return stash[flag]?.perms || defaultPerms;
+}
+
+export function useCanWriteToHeapNew(groupFlag: string) {
+  const group = useGroup(groupFlag);
+  const vessel = useVessel(groupFlag, window.our);
+  const nest = useNest();
+  const perms = useHeapPermsNew(nest);
+  const canWrite = canWriteChannel(perms, vessel, group?.bloc);
+
+  return canWrite;
+}
+
+export function useHeapIsJoinedNew(flag: HeapFlag) {
+  const briefs = useBriefsNew();
+  return useMemo(() => _.has(briefs, flag), [flag, briefs]);
+}
+
+export function useCreateHeapMutation() {
+  const queryClient = useQueryClient();
+
+  const mutationFn = async (variables: HeapCreate) => {
+    await api.trackedPoke<HeapCreate, HeapAction>(
+      {
+        app: 'heap',
+        mark: 'heap-create',
+        json: variables,
+      },
+      { app: 'heap', path: '/ui' },
+      (event) => {
+        const { update, flag } = event;
+        return (
+          'create' in update.diff && flag === `${window.our}/${variables.name}`
+        );
+      }
+    );
+  };
+
+  return useMutation({
+    mutationFn,
+    onSettled: async () => {
+      await queryClient.refetchQueries(['heap', 'briefs']);
+      await queryClient.refetchQueries(['heap', 'stash']);
+    },
+  });
+}
+
+export function useJoinHeapMutation() {
+  const queryClient = useQueryClient();
+  const mutationFn = async ({
+    group,
+    chan,
+  }: {
+    group: string;
+    chan: string;
+  }) => {
+    await api.trackedPoke<HeapJoin, HeapAction>(
+      {
+        app: 'heap',
+        mark: 'channel-join',
+        json: {
+          group,
+          chan,
+        },
+      },
+      { app: 'heap', path: 'ui' },
+      (event) => event.flag === chan && 'create' in event.update.diff
+    );
+  };
+
+  return useMutation({
+    mutationFn,
+    onSettled: async () => {
+      await queryClient.refetchQueries(['heap', 'breifs']);
+      await queryClient.refetchQueries(['heap', 'stash']);
+    },
+  });
+}
+
+export function useLeaveHeapMutation() {
+  const queryClient = useQueryClient();
+
+  const mutationFn = async (variables: { flag: HeapFlag }) => {
+    await api.poke({
+      app: 'heap',
+      mark: 'heap-leave',
+      json: variables.flag,
+    });
+  };
+
+  return useMutation({
+    mutationFn,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries(['heap', 'stash']);
+      await queryClient.cancelQueries(['heap', 'breifs']);
+      await queryClient.cancelQueries(['heap', 'perms', variables.flag]);
+      await queryClient.cancelQueries(['heap', 'blocks', variables.flag]);
+
+      queryClient.removeQueries(['heap', 'perms', variables.flag]);
+      queryClient.removeQueries(['heap', 'blocks', variables.flag]);
+    },
+    onSettled: async (variables) => {
+      await queryClient.refetchQueries(['heap', 'briefs']);
+      await queryClient.refetchQueries(['heap', 'stash']);
+    },
+  });
+}
+
+export function useAddHeapSectsMutation() {
+  const queryClient = useQueryClient();
+
+  const mutationFn = async ({
+    flag,
+    sects,
+  }: {
+    flag: HeapFlag;
+    sects: string[];
+  }) => {
+    await api.poke(heapAction(flag, { 'add-sects': sects }));
+  };
+
+  return useMutation({
+    mutationFn,
+    onSettled: async () => {
+      await queryClient.refetchQueries(['heap', 'stash']);
+    },
+  });
+}
+
+export function useDelHeapSectsMutation() {
+  const queryClient = useQueryClient();
+
+  const mutationFn = async ({
+    flag,
+    sects,
+  }: {
+    flag: HeapFlag;
+    sects: string[];
+  }) => {
+    await api.poke(heapAction(flag, { 'del-sects': sects }));
+  };
+
+  return useMutation({
+    mutationFn,
+    onSettled: async () => {
+      await queryClient.refetchQueries(['heap', 'stash']);
+    },
+  });
+}
+
+export function useAddCurioFeelMutation() {
+  const queryClient = useQueryClient();
+
+  const mutationFn = async ({
+    flag,
+    time,
+    feel,
+  }: {
+    flag: HeapFlag;
+    time: string;
+    feel: string;
+  }) => {
+    const ud = decToUd(time);
+    await api.poke(
+      heapCurioDiff(flag, ud, {
+        'add-feel': {
+          time: ud,
+          feel,
+          ship: window.our,
+        },
+      })
+    );
+  };
+
+  return useMutation({
+    mutationFn,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries([
+        'heap',
+        variables.flag,
+        'curio',
+        variables.time,
+      ]);
+      await queryClient.cancelQueries(['heap', variables.flag, 'blocks']);
+    },
+    onSettled: async (_data, _error, variables) => {
+      await queryClient.refetchQueries([
+        'heap',
+        variables.flag,
+        'curio',
+        variables.time,
+      ]);
+      await queryClient.refetchQueries(['heap', variables.flag, 'blocks']);
+    },
+  });
+}
+
+export function useDelCurioFeelMutation() {
+  const queryClient = useQueryClient();
+
+  const mutationFn = async ({
+    flag,
+    time,
+    feel,
+  }: {
+    flag: HeapFlag;
+    time: string;
+    feel: string;
+  }) => {
+    const ud = decToUd(time);
+    await api.poke(heapCurioDiff(flag, ud, { 'del-feel': time }));
+  };
+
+  return useMutation({
+    mutationFn,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries([
+        'heap',
+        variables.flag,
+        'curio',
+        variables.time,
+      ]);
+      await queryClient.cancelQueries(['heap', variables.flag, 'blocks']);
+    },
+    onSettled: async (_data, _error, variables) => {
+      await queryClient.refetchQueries([
+        'heap',
+        variables.flag,
+        'curio',
+        variables.time,
+      ]);
+      await queryClient.refetchQueries(['heap', variables.flag, 'blocks']);
+    },
+  });
+}
+
 function formatCurios(curios: HeapCurios): HeapCurioMap {
   let curioMap = restoreMap<HeapCurio>({});
   Object.entries(curios)
@@ -89,8 +370,7 @@ function formatCurios(curios: HeapCurios): HeapCurioMap {
   return curioMap;
 }
 
-export function useCuriosNew(flag: HeapFlag): HeapCurioMap {
-  console.log('useCuriosNew');
+export function useCurioBlocks(flag: HeapFlag): HeapCurioMap {
   const def = useMemo(() => new BigIntOrderedMap<HeapCurio>(), []);
 
   const { data, isLoading, isError, ...rest } = useReactQuerySubscription({
@@ -99,6 +379,7 @@ export function useCuriosNew(flag: HeapFlag): HeapCurioMap {
     path: `/heap/${flag}/ui`,
     scry: `/heap/${flag}/curios/newest/${CURIO_PAGE_SIZE}/blocks`,
     options: {
+      keepPreviousData: true,
       refetchOnMount: true,
       retryOnMount: true,
     },
@@ -107,12 +388,17 @@ export function useCuriosNew(flag: HeapFlag): HeapCurioMap {
   return data ? formatCurios(data as HeapCurios) : def;
 }
 
-export function useCurioWithCommentsNew(flag: HeapFlag, time: string) {
+export function useCurioWithComments(flag: HeapFlag, time: string) {
   const ud = useMemo(() => decToUd(time), [time]);
   const { data, ...query } = useReactQueryScry<HeapCurios>({
     queryKey: ['heap', 'curios', flag, 'curio', time],
     app: 'heap',
     path: `/heap/${flag}/curios/curio/id/${ud}/full`,
+    options: {
+      keepPreviousData: true,
+      refetchOnMount: true,
+      retryOnMount: true,
+    },
   });
 
   return useMemo(() => {
@@ -162,39 +448,124 @@ export function useAddCurioMutation(flag: HeapFlag) {
 
   return useMutation({
     mutationFn,
-    // onSettled: () => {
-    //   queryClient.invalidateQueries(['heap', 'curios', flag]);
-    // }
   });
 }
 
-export function useCommentsNew(flag: HeapFlag, time: string) {
-  const curios = useCuriosNew(flag);
-  return useMemo(() => {
-    if (!curios) {
-      return new BigIntOrderedMap<HeapCurio>();
-    }
+export function useDelCurioMutation() {
+  const queryClient = useQueryClient();
+  const mutationFn = async ({
+    flag,
+    time,
+  }: {
+    flag: HeapFlag;
+    time: string;
+  }) => {
+    const ud = decToUd(time);
+    await api.poke(heapCurioDiff(flag, ud, { del: null }));
+  };
 
-    const curio = curios.get(bigInt(time));
-    const replies = (curio?.seal?.replied || ([] as number[]))
-      .map((r: string) => {
-        const t = bigInt(udToDec(r));
-        const c = curios.get(t);
-        return c ? ([t, c] as const) : undefined;
-      })
-      .filter((r: unknown): r is [BigInteger, HeapCurio] => !!r) as [
-      BigInteger,
-      HeapCurio
-    ][];
-    return new BigIntOrderedMap<HeapCurio>().gas(replies);
-  }, [curios, time]);
+  return useMutation({
+    mutationFn,
+    onSuccess: async (_data, variables) => {
+      queryClient.removeQueries([
+        'heap',
+        variables.flag,
+        'curio',
+        variables.time,
+      ]);
+    },
+    onSettled: async (_data, _error, variables) => {
+      await queryClient.refetchQueries(['heap', variables.flag, 'blocks']);
+      await queryClient.invalidateQueries(['heap', variables.flag]);
+    },
+  });
 }
+
+export function useEditCurioMutation() {
+  const queryClient = useQueryClient();
+  const mutationFn = async ({
+    flag,
+    time,
+    heart,
+  }: {
+    flag: HeapFlag;
+    time: string;
+    heart: CurioHeart;
+  }) => {
+    const ud = decToUd(time);
+    await api.poke(heapCurioDiff(flag, ud, { edit: heart }));
+  };
+
+  return useMutation({
+    mutationFn,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries([
+        'heap',
+        variables.flag,
+        'curio',
+        variables.time,
+      ]);
+    },
+    onSettled: async (_data, _error, variables) => {
+      await queryClient.refetchQueries(['heap', variables.flag, 'blocks']);
+      await queryClient.refetchQueries([
+        'heap',
+        variables.flag,
+        'curio',
+        variables.time,
+      ]);
+    },
+  });
+}
+
+export function useMarkHeapReadMutation() {
+  const queryClient = useQueryClient();
+  const mutationFn = async ({ flag }: { flag: HeapFlag }) => {
+    await api.poke({
+      app: 'heap',
+      mark: 'heap-remark-action',
+      json: {
+        flag,
+        diff: { read: null },
+      },
+    });
+  };
+
+  return useMutation({
+    mutationFn,
+    onSettled: async () => {
+      await queryClient.refetchQueries(['heap', 'briefs']);
+    },
+  });
+}
+
+// export function useCommentsNew(flag: HeapFlag, time: string) {
+//   const curios = useCuriosNew(flag);
+//   return useMemo(() => {
+//     if (!curios) {
+//       return new BigIntOrderedMap<HeapCurio>();
+//     }
+
+//     const curio = curios.get(bigInt(time));
+//     const replies = (curio?.seal?.replied || ([] as number[]))
+//       .map((r: string) => {
+//         const t = bigInt(udToDec(r));
+//         const c = curios.get(t);
+//         return c ? ([t, c] as const) : undefined;
+//       })
+//       .filter((r: unknown): r is [BigInteger, HeapCurio] => !!r) as [
+//       BigInteger,
+//       HeapCurio
+//     ][];
+//     return new BigIntOrderedMap<HeapCurio>().gas(replies);
+//   }, [curios, time]);
+// }
 
 export function useOrderedCuriosNew(
   flag: HeapFlag,
   currentId: bigInt.BigInteger | string
 ) {
-  const curios = useCuriosNew(flag);
+  const curios = useCurioBlocks(flag);
   const sortedCurios = Array.from(curios).filter(
     ([, c]) => c.heart.replying === null
   );
@@ -232,6 +603,7 @@ export const useHeapState = createState<HeapState>(
     loadedRefs: {},
     briefs: {},
     pendingImports: {},
+    // donezo
     markRead: async (flag) => {
       await api.poke({
         app: 'heap',
@@ -242,6 +614,7 @@ export const useHeapState = createState<HeapState>(
         },
       });
     },
+    // eliminated
     start: async ({ briefs, stash }) => {
       get().batchSet((draft) => {
         draft.briefs = briefs;
@@ -290,6 +663,7 @@ export const useHeapState = createState<HeapState>(
         },
       });
     },
+    // donezo
     joinHeap: async (group, chan) => {
       await api.trackedPoke<HeapJoin, HeapAction>(
         {
@@ -304,6 +678,7 @@ export const useHeapState = createState<HeapState>(
         (event) => event.flag === chan && 'create' in event.update.diff
       );
     },
+    // donezo
     leaveHeap: async (flag) => {
       await api.poke({
         app: 'heap',
@@ -311,6 +686,7 @@ export const useHeapState = createState<HeapState>(
         json: flag,
       });
     },
+    // unneeded?
     viewHeap: async (flag, view) => {
       await api.poke(
         heapAction(flag, {
@@ -318,17 +694,21 @@ export const useHeapState = createState<HeapState>(
         })
       );
     },
+    // donezo
     addCurio: async (flag, heart) => {
       await api.poke(heapCurioDiff(flag, getTime(), { add: heart }));
     },
+    // kind of donezo?
     delCurio: async (flag, time) => {
       const ud = decToUd(time);
       await api.poke(heapCurioDiff(flag, ud, { del: null }));
     },
+    // donezo
     editCurio: async (flag, time, heart) => {
       const ud = decToUd(time);
       await api.poke(heapCurioDiff(flag, ud, { edit: heart }));
     },
+    // donezo
     create: async (req) => {
       get().batchSet((draft) => {
         const flag = `${window.our}/${req.name}`;
@@ -354,6 +734,7 @@ export const useHeapState = createState<HeapState>(
         }
       );
     },
+    // donezo
     addSects: async (flag, sects) => {
       await api.poke(heapAction(flag, { 'add-sects': sects }));
       const perms = await api.scry<HeapPerm>({
@@ -364,6 +745,7 @@ export const useHeapState = createState<HeapState>(
         draft.stash[flag].perms = perms;
       });
     },
+    // donezo
     delSects: async (flag, sects) => {
       await api.poke(heapAction(flag, { 'del-sects': sects }));
       const perms = await api.scry<HeapPerm>({
@@ -374,6 +756,7 @@ export const useHeapState = createState<HeapState>(
         draft.stash[flag].perms = perms;
       });
     },
+    // don't need
     fetchCurio: async (flag, time) => {
       const ud = decToUd(time);
       const curio = await api.scry<HeapCurio>({
@@ -384,6 +767,7 @@ export const useHeapState = createState<HeapState>(
         draft.curios[flag] = draft.curios[flag].set(bigInt(time), curio);
       });
     },
+    // donezo
     addFeel: async (flag, time, feel) => {
       const ud = decToUd(time);
       await api.poke(
@@ -396,6 +780,7 @@ export const useHeapState = createState<HeapState>(
         })
       );
     },
+    // donezo
     delFeel: async (flag, time) => {
       const ud = decToUd(time);
       await api.poke(
@@ -404,6 +789,7 @@ export const useHeapState = createState<HeapState>(
         })
       );
     },
+    // don't need
     initialize: async (flag) => {
       useSchedulerStore.getState().wait(async () => {
         const perms = await api.scry<HeapPerm>({
@@ -423,12 +809,14 @@ export const useHeapState = createState<HeapState>(
         `/heap/${flag}/ui`
       ).initialize();
     },
+    // TODO: understand
     initImports: (init) => {
       get().batchSet((draft) => {
         draft.pendingImports = init;
       });
     },
   }),
+  // this is for persisting zustand state
   {
     partialize: (state) => {
       const saved = _.pick(state, ['briefs', 'stash', 'curios']);
