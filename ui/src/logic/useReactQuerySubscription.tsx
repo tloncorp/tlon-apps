7034b2508554f63ddfcb6e0f9bbe27777a22447d
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-08-27 19:02:48 +0300

    Use `staleTime` to prevent infinite loop of refetching/re-rendering

--- ui/src/logic/useReactQuerySubscription.tsx
+++ ui/src/logic/useReactQuerySubscription.tsx
@@ -55,8 +55,7 @@ export default function useReactQuerySubscription({
   }, [app, path, queryClient, queryKey]);
 
   return useQuery(queryKey, fetchData, {
-    retryOnMount: false,
-    refetchOnMount: false,
+    staleTime: 20 * 60 * 1000,
     ...options,
   });
 }
