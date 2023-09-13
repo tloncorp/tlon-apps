034ab1f28cb37109df636ebf32cf83602dda3efd
~latter-bolden <brian@tlon.io>
2023-09-13 10:37:29 -0400

    state: remove logs from ephemeral state

--- ui/src/state/ephemeral.ts
+++ ui/src/state/ephemeral.ts
@@ -11,11 +11,9 @@ export const useEphemeralState = create<EphemeralState>((set, get) => ({
   groupsLocation: '/',
   messagesLocation: '/messages',
   setGroupsLocation: (location: string) => {
-    console.log('setGroupsLocation', location);
     set({ groupsLocation: location });
   },
   setMessagesLocation: (location: string) => {
-    console.log('setMessagesLocation', location);
     set({ messagesLocation: location });
   },
 }));
