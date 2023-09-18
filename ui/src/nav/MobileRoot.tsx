181475cecc2ba0208334fe1c7d64b1e1154ff1a0
Hunter Miller <hunter@hmiller.dev>
2023-09-18 09:36:02 -0500

    groups: add empty state prompt nown that redirect is gone

--- ui/src/nav/MobileRoot.tsx
+++ ui/src/nav/MobileRoot.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { debounce } from 'lodash';
 import useGroupSort from '@/logic/useGroupSort';
 import { usePinnedGroups } from '@/state/chat';
-import { useGangList, useGroups } from '@/state/groups';
+import { useGangList, useGroupsWithQuery } from '@/state/groups';
 import GroupList from '@/components/Sidebar/GroupList';
 import SidebarSorter from '@/components/Sidebar/SidebarSorter';
 import GroupsSidebarItem from '@/components/Sidebar/GroupsSidebarItem';
@@ -13,6 +13,7 @@ import ReconnectingSpinner from '@/components/ReconnectingSpinner';
 import MobileHeader from '@/components/MobileHeader';
 import Layout from '@/components/Layout/Layout';
 import AddIconMobileNav from '@/components/icons/AddIconMobileNav';
+import MagnifyingGlass16Icon from '@/components/icons/MagnifyingGlass16Icon';
 
 export default function MobileRoot() {
   const [isScrolling, setIsScrolling] = useState(false);
@@ -20,7 +21,7 @@ export default function MobileRoot() {
     debounce((scrolling: boolean) => setIsScrolling(scrolling), 200)
   );
   const { sortFn, setSortFn, sortOptions, sortGroups } = useGroupSort();
-  const groups = useGroups();
+  const { data: groups, isLoading } = useGroupsWithQuery();
   const gangs = useGangList();
   const pinnedGroups = usePinnedGroups();
   const sortedGroups = sortGroups(groups);
@@ -59,33 +60,41 @@ export default function MobileRoot() {
     >
       <nav className="flex h-full flex-1 flex-col overflow-y-auto overflow-x-hidden">
         <div className="flex-1">
-          <GroupsScrollingContext.Provider value={isScrolling}>
-            <GroupList
-              groups={sortedGroups}
-              pinnedGroups={Object.entries(pinnedGroups)}
-              isScrolling={scroll.current}
-            >
-              {Object.entries(pinnedGroups).length > 0 && (
-                <>
-                  <div className="px-4">
-                    <h2 className="mb-0.5 p-2 font-system-sans  text-gray-900">
-                      Pinned Groups
+          {sortedGroups.length === 0 && !isLoading ? (
+            <div className="mx-4 my-2 rounded-lg bg-indigo-50 p-4 leading-5 text-gray-700 dark:bg-indigo-900/50">
+              Tap the <span className="sr-only">find icon</span>
+              <MagnifyingGlass16Icon className="inline-flex h-4 w-4" /> below to
+              find new groups in your network or view group invites.
+            </div>
+          ) : (
+            <GroupsScrollingContext.Provider value={isScrolling}>
+              <GroupList
+                groups={sortedGroups}
+                pinnedGroups={Object.entries(pinnedGroups)}
+                isScrolling={scroll.current}
+              >
+                {Object.entries(pinnedGroups).length > 0 && (
+                  <>
+                    <div className="px-4">
+                      <h2 className="mb-0.5 p-2 font-system-sans  text-gray-900">
+                        Pinned Groups
+                      </h2>
+                      {pinnedGroupsOptions}
+                    </div>
+                    <h2 className="my-2 ml-2 p-2 pl-4 font-system-sans  text-gray-900">
+                      All Groups
                     </h2>
-                    {pinnedGroupsOptions}
-                  </div>
-                  <h2 className="my-2 ml-2 p-2 pl-4 font-system-sans  text-gray-900">
-                    All Groups
-                  </h2>
-                </>
-              )}
+                  </>
+                )}
 
-              <div className="px-4">
-                {gangs.map((flag) => (
-                  <GangItem key={flag} flag={flag} />
-                ))}
-              </div>
-            </GroupList>
-          </GroupsScrollingContext.Provider>
+                <div className="px-4">
+                  {gangs.map((flag) => (
+                    <GangItem key={flag} flag={flag} />
+                  ))}
+                </div>
+              </GroupList>
+            </GroupsScrollingContext.Provider>
+          )}
         </div>
       </nav>
     </Layout>
