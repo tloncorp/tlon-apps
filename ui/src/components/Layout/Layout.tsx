3c4acbd2b38e2e882aed31758ec4602a6e3582d9
~latter-bolden <brian@tlon.io>
2023-09-29 10:24:07 -0400

    groups: remove unnecessary select restriction from layout

--- ui/src/components/Layout/Layout.tsx
+++ ui/src/components/Layout/Layout.tsx
@@ -20,13 +20,7 @@ export default function Layout({
   stickyHeader = false,
 }: LayoutProps) {
   return (
-    <div
-      className={cn(
-        className,
-        'layout select-none',
-        stickyHeader ? 'relative' : ''
-      )}
-    >
+    <div className={cn(className, 'layout', stickyHeader ? 'relative' : '')}>
       {header && (
         <header
           className={cn('header z-40', stickyHeader ? ' sticky top-0' : '')}
