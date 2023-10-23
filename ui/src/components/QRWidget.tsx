ca04aea5c23b7d49cab6fefd3c949620a39e59e8
~latter-bolden <brian@tlon.io>
2023-10-23 00:42:56 -0400

    profile: add share dm QR option

---
+++ ui/src/components/QRWidget.tsx
@@ -0,0 +1,56 @@
+import { useMemo } from 'react';
+import cn from 'classnames';
+import QRCode from 'react-qr-code';
+import { useCopy } from '@/logic/utils';
+import CopyIcon from '@/components/icons/CopyIcon';
+import CheckIcon from './icons/CheckIcon';
+import ShareIcon from './icons/ShareIcon';
+
+export default function QRWidget({
+  link,
+  className,
+  navigatorTitle,
+}: {
+  link: string;
+  className?: string;
+  navigatorTitle: string;
+}) {
+  const url = useMemo(() => new URL(link), [link]);
+  const { didCopy, doCopy } = useCopy(link);
+  const displayURL = url.hostname + url.pathname;
+
+  const handleCopy = () => {
+    if (navigator.share !== undefined) {
+      navigator.share({
+        title: navigatorTitle,
+        url: link,
+      });
+    } else {
+      doCopy();
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        'flex h-full w-full flex-col rounded-xl bg-blue-200',
+        className
+      )}
+      onClick={handleCopy}
+    >
+      <div className="flex flex-1 items-center items-center justify-center py-8">
+        <QRCode value={link} size={200} fgColor="#bfdbfe" bgColor="#3b82f6" />
+      </div>
+      <div className="flex items-center justify-between rounded-b-xl bg-blue-500 py-4 px-6 text-blue-100">
+        <span className="w-4/5 truncate text-lg">{displayURL}</span>
+        {navigator.share !== undefined ? (
+          <ShareIcon className="w-6" />
+        ) : didCopy ? (
+          <CheckIcon className="w-6" />
+        ) : (
+          <CopyIcon className="w-6" />
+        )}
+      </div>
+    </div>
+  );
+}
