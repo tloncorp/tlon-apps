804b6874ed6dee85b9e79bd2b1c9f6ad195c5278
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2023-11-20 15:01:55 -0800

    update copy

--- ui/src/components/WelcomeCard.tsx
+++ ui/src/components/WelcomeCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useLocalStorage } from 'usehooks-ts';
 
 import { createStorageKey } from '@/logic/utils';
@@ -14,19 +15,27 @@ export default function WelcomeCard() {
 
   return (
     <div className="mx-3 mt-1 mb-4 space-y-6 rounded-[32px] bg-[#edf3fb] p-9 text-[#165FCD]">
-      <h3 className="text-[17px] font-medium">Welcome to Tlon</h3>
-      <p>Tip 1 of 3</p>
+      <h3 className="text-[17px] font-medium">Welcome to our pilot program</h3>
       <p>
-        Visit the Tlon Studio group to learn more about what we&apos;re working
-        on now.
+        Tlon is built on an open source, user-owned network. This presents
+        unique development challenges. You may encounter turbulence while we
+        make the experience as smooth as any other app. Help us improve by
+        providing feedback.
       </p>
       <div className="space-x-3 text-right text-[17px] font-medium">
-        <button className="rounded-lg bg-[#E4E9F1] px-4 py-2.5 active:opacity-90">
+        <button
+          className="rounded-lg bg-[#E4E9F1] px-4 py-2.5 active:opacity-90"
+          onClick={() => setIsWelcomeSeen(true)}
+        >
           Close
         </button>
-        <button className="rounded-lg bg-[#165FCD] px-4 py-2.5 text-[#FFF] active:opacity-90">
-          Next Tip
-        </button>
+        <Link
+          to="/profile"
+          className="rounded-lg bg-[#165FCD] px-4 py-2.5 text-[#FFF] active:opacity-90"
+          onClick={() => setIsWelcomeSeen(true)}
+        >
+          Go to Profile
+        </Link>
       </div>
     </div>
   );
