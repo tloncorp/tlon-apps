8cece3bbc0da8555e4765060e592898237351b75
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-01-08 10:06:47 -0800

    remove outdated useNest hook

--- ui/src/heap/HeapDetail/HeapDetailSidebar/HeapDetailCommentField.tsx
+++ ui/src/heap/HeapDetail/HeapDetailSidebar/HeapDetailCommentField.tsx
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
 import { useParams } from 'react-router';
-import { JSONContent } from '@tiptap/core';
-import useNest from '@/logic/useNest';
-import { nestToFlag } from '@/logic/utils';
 import { useRouteGroup } from '@/state/groups';
 import DiaryCommentField from '@/diary/DiaryCommentField';
+import { useChannelFlag } from '@/logic/channel';
 
 export default function HeapDetailCommentField() {
-  const nest = useNest();
   const { idTime } = useParams();
   const groupFlag = useRouteGroup();
-  const [, chFlag] = nestToFlag(nest);
-  const [draftText, setDraftText] = useState<JSONContent>();
-
+  const chFlag = useChannelFlag();
   return (
     <div className="border-t-2 border-gray-50 p-3 sm:p-4">
       <DiaryCommentField
         groupFlag={groupFlag}
-        flag={chFlag}
+        flag={chFlag || ''}
         han="heap"
         replyTo={idTime || ''}
       />
--- ui/src/logic/useNest.ts
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router';
-
-export default function useNest() {
-  const { app, chShip, chName } = useParams();
-
-  const chFlag = `${chShip}/${chName}`;
-  return `${app}/${chFlag}`;
-}
