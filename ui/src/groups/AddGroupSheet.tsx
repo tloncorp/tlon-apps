1eefe66d6fca0f523fe4215805f66be58557fc2d
~latter-bolden <brian@tlon.io>
2024-01-15 19:06:04 -0500

    add group sheet: move CreateOrJoin component out of AddGroupSheet body

--- ui/src/groups/AddGroupSheet.tsx
+++ ui/src/groups/AddGroupSheet.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import cn from 'classnames';
 import WidgetDrawer from '@/components/WidgetDrawer';
 import HomeIconMobileNav from '@/components/icons/HomeIconMobileNav';
@@ -6,6 +6,40 @@ import NewRaysIcon from '@/components/icons/NewRaysIcon';
 import { isNativeApp } from '@/logic/native';
 import JoinGroup from './AddGroupSheet/JoinGroup';
 import CreateGroup from './AddGroupSheet/CreateGroup';
+
+function CreateOrJoin(props: { onCreate: () => void; onJoin: () => void }) {
+  return (
+    <div className="flex w-full flex-col items-center pb-4">
+      <h3 className="mb-6 mt-4 text-[17px] ">Add a group</h3>
+      <div className="flex flex-col rounded-lg border border-gray-100">
+        <button
+          className="flex items-center border-b border-gray-100 px-6 py-4 active:bg-blue-100"
+          onClick={props.onJoin}
+        >
+          <HomeIconMobileNav className="mr-4 h-6 w-6" isInactive asIcon />
+          <div className="flex flex-col items-start">
+            <h4 className="mb-1.5 text-[17px] tracking-wide">Join a group</h4>
+            <p className="text-gray-300">
+              Join with short code or host's Urbit ID
+            </p>
+          </div>
+        </button>
+        <button
+          className="flex items-center px-6 py-4 text-blue active:bg-blue-100"
+          onClick={props.onCreate}
+        >
+          <NewRaysIcon className="mr-4 h-6 w-6" />
+          <div className="flex flex-col items-start">
+            <h4 className="mb-1.5 text-[17px] tracking-wide">
+              Create new group
+            </h4>
+            <p className="text-gray-300">Start a group from scratch</p>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default function AddGroupSheet(props: {
   open: boolean;
@@ -28,40 +62,6 @@ export default function AddGroupSheet(props: {
     }, 100);
   };
 
-  const CreateOrJoin = useCallback(() => {
-    return (
-      <div className="flex w-full flex-col items-center pb-4">
-        <h3 className="mb-6 mt-4 text-[17px] ">Add a group</h3>
-        <div className="flex flex-col rounded-lg border border-gray-100">
-          <button
-            className="flex items-center border-b border-gray-100 px-6 py-4 active:bg-blue-100"
-            onClick={() => delayedViewNav('join')}
-          >
-            <HomeIconMobileNav className="mr-4 h-6 w-6" isInactive asIcon />
-            <div className="flex flex-col items-start">
-              <h4 className="mb-1.5 text-[17px] tracking-wide">Join a group</h4>
-              <p className="text-gray-300">
-                Join with short code or host's Urbit ID
-              </p>
-            </div>
-          </button>
-          <button
-            className="flex items-center px-6 py-4 text-blue active:bg-blue-100"
-            onClick={() => delayedViewNav('create')}
-          >
-            <NewRaysIcon className="mr-4 h-6 w-6" />
-            <div className="flex flex-col items-start">
-              <h4 className="mb-1.5 text-[17px] tracking-wide">
-                Create new group
-              </h4>
-              <p className="text-gray-300">Start a group from scratch</p>
-            </div>
-          </button>
-        </div>
-      </div>
-    );
-  }, []);
-
   return (
     <WidgetDrawer
       open={props.open}
@@ -70,7 +70,12 @@ export default function AddGroupSheet(props: {
       withGrabber={true}
     >
       <div className="mt-4">
-        {view === 'root' && <CreateOrJoin />}
+        {view === 'root' && (
+          <CreateOrJoin
+            onCreate={() => delayedViewNav('create')}
+            onJoin={() => delayedViewNav('join')}
+          />
+        )}
         {view === 'create' && <CreateGroup back={() => setView('root')} />}
         {view === 'join' && (
           <JoinGroup back={() => setView('root')} onOpenChange={onOpenChange} />
