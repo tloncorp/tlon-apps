15c29fb5c1cb9e855865bb2f953d181343ee9c6d
bᵣᵢₐₙ <90741358+latter-bolden@users.noreply.github.com>
2024-02-02 16:40:20 -0500

    Update ui/src/components/Sidebar/useSearchFilter.ts
    Co-authored-by: Hunter Miller <hunter@hmiller.dev>

--- ui/src/components/Sidebar/useSearchFilter.ts
+++ ui/src/components/Sidebar/useSearchFilter.ts
@@ -55,25 +55,4 @@ export default function useSearchFilter(query: string): GroupSearchRecord[] {
       deSig(record.flag)?.startsWith(deSig(query) || '')
   );
 
-  // const searchSpace = useMemo(() => records.map(record => `${record.title}${record.flag}`), [records]);
-
-  // const fuzzScores = useMemo(() => (
-  //   fuzzy
-  //     .filter(query, searchSpace)
-  //     .sort((a, b) => {
-  //       const filter = deSig(query) || '';
-  //       const left = deSig(a.string)?.startsWith(filter)
-  //         ? a.score + 1
-  //         : a.score;
-  //       const right = deSig(b.string)?.startsWith(filter)
-  //         ? b.score + 1
-  //         : b.score;
-
-  //       return right - left;
-  //     })
-  // ), [query, searchSpace]);
-
-  // const results = useMemo(() => fuzzScores.map(result => records[result.index]), [fuzzScores, records]);
-
-  // return results;
 }
