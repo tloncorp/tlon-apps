c3a7bfbd520ef2a41a0fbe58652812e1689a54f4
~latter-bolden <brian@tlon.io>
2024-02-22 10:59:40 -0500

    wip: add singleton webview

--- apps/tlon-mobile/src/navigation/TabStack.tsx
+++ apps/tlon-mobile/src/navigation/TabStack.tsx
@@ -8,8 +8,9 @@ import {
   MessagesFilled,
   MessagesOutlined,
 } from '../../assets/icons';
+import { WebviewProvider } from '../contexts/webview';
 import type { TabParamList } from '../types';
-import { WebViewStack } from './WebViewStack';
+import { WebViewStack, WebviewSingletonStack } from './WebViewStack';
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
@@ -19,60 +20,62 @@ const ICON_SIZE = {
 };
 
 export const TabStack = () => (
-  <Tab.Navigator
-    id="TabBar"
-    initialRouteName="Groups"
-    screenOptions={{ headerShown: false }}
-  >
-    <Tab.Screen
-      name="Groups"
-      component={WebViewStack}
-      initialParams={{ initialPath: '/' }}
-      options={{
-        tabBarIcon: ({ focused }) =>
-          focused ? (
-            <HomeFilled {...ICON_SIZE} />
-          ) : (
-            <HomeOutlined {...ICON_SIZE} />
-          ),
-        tabBarShowLabel: false,
-      }}
-    />
-    <Tab.Screen
-      name="Messages"
-      component={WebViewStack}
-      initialParams={{ initialPath: '/messages' }}
-      options={{
-        tabBarIcon: ({ focused }) =>
-          focused ? (
-            <MessagesFilled {...ICON_SIZE} />
-          ) : (
-            <MessagesOutlined {...ICON_SIZE} />
-          ),
-        tabBarShowLabel: false,
-      }}
-    />
-    <Tab.Screen
-      name="Activity"
-      component={WebViewStack}
-      initialParams={{ initialPath: '/notifications' }}
-      options={{
-        tabBarIcon: ({ focused }) =>
-          focused ? (
-            <ActivityFilled {...ICON_SIZE} />
-          ) : (
-            <ActivityOutlined {...ICON_SIZE} />
-          ),
-        tabBarShowLabel: false,
-      }}
-    />
-    <Tab.Screen
-      name="Profile"
-      component={WebViewStack}
-      initialParams={{ initialPath: '/profile' }}
-      options={{
-        tabBarShowLabel: false,
-      }}
-    />
-  </Tab.Navigator>
+  <WebviewProvider>
+    <Tab.Navigator
+      id="TabBar"
+      initialRouteName="Groups"
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen
+        name="Groups"
+        component={WebViewStack}
+        initialParams={{ initialPath: '/' }}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <HomeFilled {...ICON_SIZE} />
+            ) : (
+              <HomeOutlined {...ICON_SIZE} />
+            ),
+          tabBarShowLabel: false,
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={WebViewStack}
+        initialParams={{ initialPath: '/messages' }}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <MessagesFilled {...ICON_SIZE} />
+            ) : (
+              <MessagesOutlined {...ICON_SIZE} />
+            ),
+          tabBarShowLabel: false,
+        }}
+      />
+      <Tab.Screen
+        name="Activity"
+        component={WebViewStack}
+        initialParams={{ initialPath: '/notifications' }}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <ActivityFilled {...ICON_SIZE} />
+            ) : (
+              <ActivityOutlined {...ICON_SIZE} />
+            ),
+          tabBarShowLabel: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={WebviewSingletonStack}
+        initialParams={{ initialPath: '/profile' }}
+        options={{
+          tabBarShowLabel: false,
+        }}
+      />
+    </Tab.Navigator>
+  </WebviewProvider>
 );
--- apps/tlon-mobile/src/navigation/WebViewStack.tsx
+++ apps/tlon-mobile/src/navigation/WebViewStack.tsx
@@ -1,6 +1,9 @@
 import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { View } from 'react-native';
 
+import { useWebViewContext } from '../contexts/webview';
 import { useScreenOptions } from '../hooks/useScreenOptions';
 import { ExternalWebViewScreen } from '../screens/ExternalWebViewScreen';
 import { WebViewScreen } from '../screens/WebViewScreen';
@@ -27,3 +30,26 @@ export const WebViewStack = (props: Props) => {
     </Stack.Navigator>
   );
 };
+
+export const WebviewSingletonStack = (props: Props) => {
+  const screenOptions = useScreenOptions();
+  return (
+    <Stack.Navigator initialRouteName="WebView" screenOptions={screenOptions}>
+      <Stack.Screen
+        name="WebView"
+        component={WebviewSingletonScreen}
+        initialParams={props.route.params}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="ExternalWebView" component={ExternalWebViewScreen} />
+    </Stack.Navigator>
+  );
+};
+
+function WebviewSingletonScreen(
+  props: NativeStackScreenProps<WebViewStackParamList, 'WebView'>
+) {
+  console.log(props);
+  const webviewContext = useWebViewContext();
+  return <View>{webviewContext.webview}</View>;
+}
