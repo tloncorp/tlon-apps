1dcb1ae127a8bc11ed3f20d5747ff1798d086365
~latter-bolden <brian@tlon.io>
2024-02-29 03:18:33 -0500

    constants: add search result page size

--- apps/tlon-web/src/constants.ts
+++ apps/tlon-web/src/constants.ts
@@ -8,6 +8,7 @@ export const LEAP_DESCRIPTION_TRUNCATE_LENGTH = 48;
 export const LEAP_RESULT_TRUNCATE_SIZE = 5;
 export const LEAP_RESULT_SCORE_THRESHOLD = 10;
 export const CURIO_PAGE_SIZE = 250;
+export const CHANNEL_SEARCH_RESULT_SIZE = 20;
 
 export const PASTEABLE_MEDIA_TYPES = [
   'image/gif',
