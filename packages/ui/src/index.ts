268536c2258d9c7c5629f456840e9313f8be4342
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-03-04 18:29:33 -0800

    remove unused exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,14 +1,3 @@
-// import { FontFamilyTokens } from "@tamagui/web";
-import { FontFamilyTokens } from "@tamagui/core";
-import {
-  getTokenValue,
-  SizeTokens,
-  SpaceTokens,
-  Token,
-  getConfig,
-  Variable,
-} from "tamagui";
-
 export { useTheme } from "tamagui";
 export * from "./components/Button";
 export * from "./components/Icon";
@@ -19,39 +8,4 @@ export * from "./components/View";
 export * from "./tamagui.config";
 export * from "./components/UrbitSigil";
 
-export const token = {
-  size: (size: SizeTokens) => {
-    return getTokenValue(size as Token, "size");
-  },
-  space: (space: SpaceTokens) => {
-    return getTokenValue(space as Token, "space");
-  },
-  radius: (radius: string) => {
-    return getTokenValue(radius as Token, "radius");
-  },
-};
-
-export function getFont(name: FontFamilyTokens, size = "$true") {
-  const font = getConfig().fontsParsed[name];
-  if (!font) {
-    console.warn("invalid font name", name);
-    return null;
-  }
-  return {
-    fontFamily: getFontTokenValue(font.family),
-    fontSize: getFontTokenValue(font.size[size]),
-    lineHeight: getFontTokenValue(font.lineHeight?.[size]),
-    fontWeight: getFontTokenValue(font.weight?.[size]),
-  };
-}
-
-function getFontTokenValue(
-  token: string | number | Variable | undefined
-): string | number | undefined {
-  if (!token || typeof token === "string" || typeof token === "number") {
-    return token;
-  } else {
-    return token.val;
-  }
-}
 export { useStyle, ZStack, View } from "tamagui";
