2304178f7404d0f2da35cfb3709765dd9d946856
~latter-bolden <brian@tlon.io>
2024-03-11 03:38:42 -0400

    ops: bump build 58

--- apps/tlon-mobile/app.config.ts
+++ apps/tlon-mobile/app.config.ts
@@ -30,14 +30,14 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   ios: {
     runtimeVersion: '4.0.0',
-    buildNumber: '57',
+    buildNumber: '58',
     config: {
       usesNonExemptEncryption: false,
     },
   },
   android: {
     runtimeVersion: '4.0.0',
-    versionCode: 57,
+    versionCode: 58,
   },
   updates: {
     url: `https://u.expo.dev/${projectId}`,
