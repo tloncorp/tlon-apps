d35345e2483c715def91aea72b6d92d7e7e8fde6
~latter-bolden <brian@tlon.io>
2024-03-12 01:02:29 -0400

    mobile: add unread conversion tests

---
+++ apps/tlon-mobile/src/lib/unreadsApi.test.ts
@@ -0,0 +1,60 @@
+import type * as ubChan from '@tloncorp/shared/dist/urbit/channel';
+import type * as ubDM from '@tloncorp/shared/dist/urbit/dms';
+import { expect, test } from 'vitest';
+
+import type { UnreadType } from '../db';
+import { toClientUnread, toClientUnreads } from './unreadsApi';
+
+const inputUnread: [string, ubChan.Unread, UnreadType] = [
+  'chat/~nibset-napwyn/commons',
+  {
+    unread: null,
+    count: 0,
+    recency: 1684342021902,
+    threads: {},
+  },
+  'channel',
+];
+
+const expectedChannelUnread = {
+  channelId: 'chat/~nibset-napwyn/commons',
+  type: 'channel',
+  totalCount: 0,
+};
+
+test('converts a channel unread from server to client format', () => {
+  expect(toClientUnread(...inputUnread)).toStrictEqual(expectedChannelUnread);
+});
+
+test('converts an array of contacts from server to client format', () => {
+  expect(
+    toClientUnreads({ [inputUnread[0]]: inputUnread[1] }, inputUnread[2])
+  ).toStrictEqual([expectedChannelUnread]);
+});
+
+const inputDMUnread: [string, ubDM.DMUnread, UnreadType] = [
+  'dm/~pondus-latter',
+  {
+    unread: null,
+    count: 0,
+    recency: 1684342021902,
+    threads: {},
+  },
+  'dm',
+];
+
+const expectedDMUnread = {
+  channelId: 'dm/~pondus-latter',
+  type: 'channel',
+  totalCount: 0,
+};
+
+test('converts a channel unread from server to client format', () => {
+  expect(toClientUnread(...inputDMUnread)).toStrictEqual(expectedDMUnread);
+});
+
+test('converts an array of contacts from server to client format', () => {
+  expect(
+    toClientUnreads({ [inputDMUnread[0]]: inputDMUnread[1] }, inputDMUnread[2])
+  ).toStrictEqual([expectedDMUnread]);
+});
