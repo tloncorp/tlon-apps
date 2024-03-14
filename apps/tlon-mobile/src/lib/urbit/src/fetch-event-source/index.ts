53b592d6cd8253e359298fbd6a75c80c2944213d
Dan Brewster <dnbrwstr@gmail.com>
2024-03-14 09:58:02 -0400

    mobile: remove urbit module

--- apps/tlon-mobile/src/lib/urbit/src/fetch-event-source/index.ts
+++
@@ -1,4 +0,0 @@
-// copied from https://github.com/gfortaine/fetch-event-source with added modifications
-export { fetchEventSource, EventStreamContentType } from './fetch';
-export type { FetchEventSourceInit } from './fetch';
-export type { EventSourceMessage } from './parse';
--- apps/tlon-mobile/src/lib/urbit/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types';
-export * from './events';
-import { Urbit } from './Urbit';
-export { Urbit as default, Urbit };
