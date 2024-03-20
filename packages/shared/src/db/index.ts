5867af16bdde71bab88c4dd69c2f619275f8bbf6
Patrick O'Sullivan <patrick@osullivan.io>
2024-03-20 06:05:16 -0500

    Don't open db on each operation

--- packages/shared/src/db/index.ts
+++ packages/shared/src/db/index.ts
@@ -3,22 +3,19 @@ import { SQLocalDrizzle } from 'sqlocal/drizzle';
 import { drizzle as drizzleSqlLiteProxy } from 'drizzle-orm/sqlite-proxy';
 import { drizzle as drizzleExpoSqlLite } from 'drizzle-orm/expo-sqlite';
 import { openDatabaseSync } from 'expo-sqlite/next';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 const sqliteFile = 'tlon.sqlite';
+const { driver } = new SQLocalDrizzle(sqliteFile);
+const expoDriver = openDatabaseSync(sqliteFile);
 
 const openDatabaseWeb = () => {
-  const { driver } = new SQLocalDrizzle(sqliteFile);
-
   const db = drizzleSqlLiteProxy(driver, { schema });
 
   return db;
 };
 
 const openDatabaseExpo = () => {
-  const driver = openDatabaseSync(sqliteFile);
-
-  const db = drizzleExpoSqlLite(driver, { schema });
+  const db = drizzleExpoSqlLite(expoDriver, { schema });
 
   return db;
 };
