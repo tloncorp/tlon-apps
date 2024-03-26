8c70b836dccba3aa22f0a4238df0536a60caad2b
Patrick O'Sullivan <patrick@osullivan.io>
2024-03-26 12:53:16 -0500

    Remove commented out code from ui/src/index.ts

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,12 +1,9 @@
-// export { Circle, useTheme, useStyle, View, ZStack } from "tamagui";
 export * from "./core";
 
 export * from "./components/Avatar";
-// export * from "./components/Button";
 export * from "./components/Icon";
 export * from "./components/ListItem";
 export * from "./components/TamaguiProvider";
-// export * from "./core/Text";
 export * from "./components/UrbitSigil";
 export * from "./components/View";
 export * from "./components/Sheet";
