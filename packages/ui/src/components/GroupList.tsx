c129cc53adab7bda63cf74f22407cad3f3023a6d
Patrick O'Sullivan <patrick@osullivan.io>
2024-03-26 12:45:19 -0500

    Remove use of react-native components from GroupList, rewrite with tamagui's YGroup

--- packages/ui/src/components/GroupList.tsx
+++ packages/ui/src/components/GroupList.tsx
@@ -1,66 +1,60 @@
-import { SectionList, StyleProp, ViewStyle } from "react-native";
-import type { ClientTypes } from "@tloncorp/shared";
-import { GroupListItem } from "./GroupListItem";
-import { Text, useStyle } from "../core";
+import type { ClientTypes } from '@tloncorp/shared';
+import { GroupListItem } from './GroupListItem';
+import { YGroup, Text } from 'tamagui';
 
 export function GroupList({
   pinned,
   other,
   onGroupLongPress,
-  onGroupPress,
+  onGroupPress
 }: {
   pinned: ClientTypes.Group[];
   other: ClientTypes.Group[];
   onGroupPress?: (group: ClientTypes.Group) => void;
   onGroupLongPress?: (group: ClientTypes.Group) => void;
 }) {
-  const contentContainerStyle = useStyle(
-    {
-      gap: "$s",
-      padding: "$l",
-    },
-    { resolveValues: "value" }
-    // Shouldn't have to cast this, since gap and padding will resolve to
-    // numeric values, but I think tamagui's types are off here.
-  ) as StyleProp<ViewStyle>;
-
   return (
-    <SectionList
-      sections={[
-        ...(pinned.length > 0
-          ? [{ name: "Pinned", data: Array.from(pinned) }]
-          : []),
-        {
-          name: pinned.length > 0 ? "Other" : undefined,
-          data: Array.from(other),
-        },
-      ]}
-      renderItem={({ item }) => (
-        <GroupListItem
-          model={item}
-          onPress={() => onGroupPress?.(item)}
-          onLongPress={() => onGroupLongPress?.(item)}
-        />
-      )}
-      renderSectionHeader={({ section }) =>
-        section.name ? (
+    <YGroup alignSelf="stretch" gap="$s" padding="$l">
+      <YGroup.Item>
+        {pinned.length > 0 && (
           <Text
             paddingHorizontal="$l"
             paddingVertical="$xl"
-            fontSize={"$s"}
+            fontSize="$s"
             color="$secondaryText"
           >
-            {section.name}
+            Pinned
           </Text>
-        ) : null
-      }
-      stickySectionHeadersEnabled={false}
-      contentContainerStyle={contentContainerStyle}
-      keyExtractor={getGroupId}
-    />
+        )}
+        {pinned.map(item => (
+          <GroupListItem
+            model={item}
+            onPress={() => onGroupPress?.(item)}
+            onLongPress={() => onGroupLongPress?.(item)}
+            key={item.id}
+          />
+        ))}
+      </YGroup.Item>
+      <YGroup.Item>
+        {other.length > 0 && (
+          <Text
+            paddingHorizontal="$l"
+            paddingVertical="$xl"
+            fontSize="$s"
+            color="$secondaryText"
+          >
+            Other
+          </Text>
+        )}
+        {other.map(item => (
+          <GroupListItem
+            model={item}
+            onPress={() => onGroupPress?.(item)}
+            onLongPress={() => onGroupLongPress?.(item)}
+            key={item.id}
+          />
+        ))}
+      </YGroup.Item>
+    </YGroup>
   );
-}
-
-function getGroupId(group: ClientTypes.Group) {
-  return group.id;
 }
