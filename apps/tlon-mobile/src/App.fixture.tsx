73d9d50a084f751e849f063510e3d156fc5d1c41
Patrick O'Sullivan <patrick@osullivan.io>
2024-03-28 10:14:25 -0500

    Mobile: add react-cosmos

---
+++ apps/tlon-mobile/src/App.fixture.tsx
@@ -0,0 +1,4 @@
+import App from "./App.main";
+
+// eslint-disable-next-line
+export default () => <App />;
---
+++ apps/tlon-mobile/src/fixtures/Button.fixture.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@tloncorp/ui";
+
+export default {
+  primary: () => (
+    <Button>
+      <Button.Text>Primary</Button.Text>
+    </Button>
+  ),
+};
