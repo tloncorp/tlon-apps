06dd65bec84fd219da8a147cf9edda408a611d39
Patrick O'Sullivan <patrick@osullivan.io>
2024-03-28 13:12:17 -0500

    Cosmos: move decorator file to fixures folder, otherwise it'll apply to the App.tsx (in dev mode)

--- apps/tlon-mobile/src/fixtures/cosmos.decorator.tsx
+++ apps/tlon-mobile/src/fixtures/cosmos.decorator.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // eslint-disable-next-line
 export default ({ children }: { children: React.ReactNode }) => (
