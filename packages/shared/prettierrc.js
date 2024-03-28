17ef19c5e05c2ffac4fb61c0522fa585fb2489bd
Dan Brewster <dnbrwstr@gmail.com>
2024-03-28 12:45:49 -0400

    cleanup: better prettier config fix

--- packages/shared/prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
-  importOrder: ['<THIRD_PARTY_MODULES>', '^@/(.*)', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-};
