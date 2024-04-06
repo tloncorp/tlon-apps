d7905ca686d002256ea9840ed225de5319a0d7fa
Patrick O'Sullivan <patrick@osullivan.io>
2024-04-06 06:34:01 -0500

    Perf: set cache time to 5m

--- apps/tlon-web/src/queryClient.ts
+++ apps/tlon-web/src/queryClient.ts
@@ -3,7 +3,7 @@ import { QueryClient } from '@tanstack/react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      cacheTime: Infinity,
+      cacheTime: 5 * 60 * 1000,
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
     },
