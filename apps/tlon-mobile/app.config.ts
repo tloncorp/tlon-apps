6428abd91f98adb0b4764ec643fe1ea67efec117
~latter-bolden <brian@tlon.io>
2024-04-17 02:33:36 -0400

    fix types

--- apps/tlon-mobile/app.config.ts
+++ apps/tlon-mobile/app.config.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config';
 import type { ConfigContext, ExpoConfig } from 'expo/config';
 
+declare const process: {
+  env: Record<string, string | undefined>;
+};
+
 const projectId = '617bb643-5bf6-4c40-8af6-c6e9dd7e3bd0';
-const isPreview = (process.env as any).APP_VARIANT === 'preview';
+const isPreview = process.env.APP_VARIANT === 'preview';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
@@ -15,23 +19,22 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     eas: {
       projectId,
     },
-    postHogApiKey: (process.env as any).POST_HOG_API_KEY,
-    notifyProvider: (process.env as any).NOTIFY_PROVIDER,
-    notifyService: (process.env as any).NOTIFY_SERVICE,
-    apiUrl: (process.env as any).API_URL,
-    apiAuthUsername: (process.env as any).API_AUTH_USERNAME,
-    apiAuthPassword: (process.env as any).API_AUTH_PASSWORD,
-    shipUrlPattern: (process.env as any).SHIP_URL_PATTERN,
-    defaultLure: (process.env as any).DEFAULT_LURE,
-    defaultPriorityToken: (process.env as any).DEFAULT_PRIORITY_TOKEN,
-    defaultTlonLoginEmail: (process.env as any).DEFAULT_TLON_LOGIN_EMAIL,
-    defaultTlonLoginPassword: (process.env as any).DEFAULT_TLON_LOGIN_PASSWORD,
-    defaultShipLoginUrl: (process.env as any).DEFAULT_SHIP_LOGIN_URL,
-    defaultShipLoginAccessCode: (process.env as any)
-      .DEFAULT_SHIP_LOGIN_ACCESS_CODE,
-    recaptchaSiteKeyAndroid: (process.env as any).RECAPTCHA_SITE_KEY_ANDROID,
-    recaptchaSiteKeyIOS: (process.env as any).RECAPTCHA_SITE_KEY_IOS,
-    enabledLoggers: (process.env as any).ENABLED_LOGGERS,
+    postHogApiKey: process.env.POST_HOG_API_KEY,
+    notifyProvider: process.env.NOTIFY_PROVIDER,
+    notifyService: process.env.NOTIFY_SERVICE,
+    apiUrl: process.env.API_URL,
+    apiAuthUsername: process.env.API_AUTH_USERNAME,
+    apiAuthPassword: process.env.API_AUTH_PASSWORD,
+    shipUrlPattern: process.env.SHIP_URL_PATTERN,
+    defaultLure: process.env.DEFAULT_LURE,
+    defaultPriorityToken: process.env.DEFAULT_PRIORITY_TOKEN,
+    defaultTlonLoginEmail: process.env.DEFAULT_TLON_LOGIN_EMAIL,
+    defaultTlonLoginPassword: process.env.DEFAULT_TLON_LOGIN_PASSWORD,
+    defaultShipLoginUrl: process.env.DEFAULT_SHIP_LOGIN_URL,
+    defaultShipLoginAccessCode: process.env.DEFAULT_SHIP_LOGIN_ACCESS_CODE,
+    recaptchaSiteKeyAndroid: process.env.RECAPTCHA_SITE_KEY_ANDROID,
+    recaptchaSiteKeyIOS: process.env.RECAPTCHA_SITE_KEY_IOS,
+    enabledLoggers: process.env.ENABLED_LOGGERS,
   },
   ios: {
     runtimeVersion: '4.0.0',
