0c9174902594eed0231b20118117b0f482f7618e
James Acklin <james.acklin@gmail.com>
2024-04-18 15:52:07 -0400

    native: eliminate left-side icons in MessageInputBase

--- packages/ui/src/components/MessageInput/MessageInputBase.tsx
+++ packages/ui/src/components/MessageInput/MessageInputBase.tsx
@@ -1,7 +1,7 @@
 import { JSONContent } from '@tiptap/core';
 import { PropsWithChildren } from 'react';
 
-import { Attachment, Camera, ChannelGalleries, Send } from '../../assets/icons';
+import { ArrowUp } from '../../assets/icons';
 import { XStack } from '../../core';
 import { IconButton } from '../IconButton';
 
@@ -23,22 +23,11 @@ export const MessageInputContainer = ({
       gap="$l"
       alignItems="center"
     >
-      <XStack gap="$l">
-        <IconButton onPress={() => {}}>
-          <Camera />
-        </IconButton>
-        <IconButton onPress={() => {}}>
-          <Attachment />
-        </IconButton>
-        <IconButton onPress={() => {}}>
-          <ChannelGalleries />
-        </IconButton>
-      </XStack>
       <XStack flex={1} gap="$l" alignItems="center">
         {children}
         <IconButton onPress={onPressSend}>
           {/* TODO: figure out what send button should look like */}
-          <Send />
+          <ArrowUp />
         </IconButton>
       </XStack>
     </XStack>
