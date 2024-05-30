9c21ce652a1c61f8f9b2b2841e5fa01c6f9ff410
Patrick O'Sullivan <patrick@osullivan.io>
2024-05-30 12:32:00 -0500

    Remove animations from the top button, just show the camera icon. Press it to attach an image.

--- packages/ui/src/components/BigInput.tsx
+++ packages/ui/src/components/BigInput.tsx
@@ -3,20 +3,10 @@ import * as db from '@tloncorp/shared/dist/db';
 import { useRef, useState } from 'react';
 import { Dimensions, KeyboardAvoidingView, Platform } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import Animated, {
-  interpolate,
-  useAnimatedReaction,
-  useAnimatedScrollHandler,
-  useAnimatedStyle,
-  useDerivedValue,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
-import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Image, Input, getToken } from 'tamagui';
 
-import { Text, View, YStack } from '../core';
+import { ScrollView, View, YStack } from '../core';
 import AttachmentSheet from './AttachmentSheet';
 import { Icon } from './Icon';
 import { LoadingSpinner } from './LoadingSpinner';
@@ -49,75 +39,32 @@ export function BigInput({
     editor: EditorBridge | null;
     setEditor: (editor: EditorBridge) => void;
   }>(null);
-  const scrollY = useSharedValue(0);
-  const scrollViewRef = useRef<ScrollView>(null);
-  const isScrolledPastThreshold = useSharedValue(false);
-
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    scrollY.value = event.contentOffset.y;
-  });
-
-  useAnimatedReaction(
-    () => scrollY.value,
-    (currentScrollY) => {
-      if (currentScrollY <= -150) {
-        isScrolledPastThreshold.value = true;
-      } else {
-        isScrolledPastThreshold.value = false;
-      }
-    }
-  );
-
-  const height = useDerivedValue(() => {
-    return isScrolledPastThreshold.value
-      ? withTiming(150, { duration: 300 })
-      : interpolate(scrollY.value, [0, -150], [50, 150], 'clamp');
-  });
-
-  const imageHeightChangeStyle = useAnimatedStyle(() => {
-    return {
-      height: height.value,
-    };
-  });
-
-  const onScrollEndDrag = () => {
-    if (isScrolledPastThreshold.value) {
-      scrollViewRef.current?.scrollTo({ y: -150, animated: true });
-    }
-  };
-
   const { top } = useSafeAreaInsets();
   const { width } = Dimensions.get('screen');
-  const titleInputHeight = getToken('$4xl', 'space');
+  const titleInputHeight = getToken('$4xl', 'size');
+  const imageButtonHeight = getToken('$4xl', 'size');
   const keyboardVerticalOffset =
     Platform.OS === 'ios' ? top + titleInputHeight : top;
 
   return (
     <YStack height="100%" width="100%">
       {channelType === 'notebook' && (
-        <Animated.View
+        <View
           style={[
             {
               position: 'absolute',
               top: 0,
               left: 0,
               width: '100%',
-              // height: 150,
+              height: imageButtonHeight,
               zIndex: 10,
-              // paddingTop: topOffset,
             },
-            // imageTranslateYStyle,
-            imageHeightChangeStyle,
           ]}
         >
           <TouchableOpacity
             onPress={() => {
-              if (isScrolledPastThreshold.value) {
-                setShowAttachmentSheet(true);
-                editorRef.current?.editor?.blur();
-              } else {
-                scrollViewRef.current?.scrollTo({ y: -150, animated: true });
-              }
+              setShowAttachmentSheet(true);
+              editorRef.current?.editor?.blur();
             }}
           >
             {uploadInfo?.imageAttachment && !uploadInfo.uploading ? (
@@ -132,7 +79,7 @@ export function BigInput({
                 }}
               />
             ) : (
-              <YStack
+              <View
                 backgroundColor="$primaryText"
                 width="100%"
                 height="100%"
@@ -146,14 +93,9 @@ export function BigInput({
                 {uploadInfo?.uploading ? (
                   <LoadingSpinner />
                 ) : (
-                  <>
-                    <Icon type="Camera" color="$background" />
-                    <Text color="$background">
-                      Choose an optional cover image
-                    </Text>
-                  </>
+                  <Icon type="Camera" color="$background" />
                 )}
-              </YStack>
+              </View>
             )}
           </TouchableOpacity>
           {channelType === 'notebook' && (
@@ -168,48 +110,46 @@ export function BigInput({
               />
             </View>
           )}
-        </Animated.View>
+        </View>
       )}
-      <Animated.ScrollView
-        ref={scrollViewRef}
-        style={[{ height: '100%', width: '100%' }]}
-        onScroll={scrollHandler}
-        onScrollEndDrag={onScrollEndDrag}
+      <ScrollView
         scrollEventThrottle={16}
         scrollToOverflowEnabled
         overScrollMode="always"
         contentContainerStyle={{
-          paddingTop: channelType === 'notebook' ? 100 : 0,
+          paddingTop:
+            channelType === 'notebook'
+              ? titleInputHeight + imageButtonHeight
+              : 0,
+          flex: 1,
         }}
       >
-        <View width="100%" height="100%">
-          <MessageInput
-            shouldBlur={shouldBlur}
-            setShouldBlur={setShouldBlur}
-            send={send}
-            title={title}
-            image={uploadInfo?.uploadedImage ?? undefined}
-            channelId={channelId}
-            groupMembers={groupMembers}
-            storeDraft={storeDraft}
-            clearDraft={clearDraft}
-            getDraft={getDraft}
-            editingPost={editingPost}
-            setEditingPost={setEditingPost}
-            editPost={editPost}
-            setShowBigInput={setShowBigInput}
-            floatingActionButton
-            showAttachmentButton={false}
-            backgroundColor="$background"
-            paddingHorizontal="$m"
-            placeholder={placeholder}
-            bigInput
-            showToolbar={channelType === 'notebook'}
-            channelType={channelType}
-            ref={editorRef}
-          />
-        </View>
-      </Animated.ScrollView>
+        <MessageInput
+          shouldBlur={shouldBlur}
+          setShouldBlur={setShouldBlur}
+          send={send}
+          title={title}
+          image={uploadInfo?.uploadedImage ?? undefined}
+          channelId={channelId}
+          groupMembers={groupMembers}
+          storeDraft={storeDraft}
+          clearDraft={clearDraft}
+          getDraft={getDraft}
+          editingPost={editingPost}
+          setEditingPost={setEditingPost}
+          editPost={editPost}
+          setShowBigInput={setShowBigInput}
+          floatingActionButton
+          showAttachmentButton={false}
+          backgroundColor="$background"
+          paddingHorizontal="$m"
+          placeholder={placeholder}
+          bigInput
+          showToolbar={channelType === 'notebook'}
+          channelType={channelType}
+          ref={editorRef}
+        />
+      </ScrollView>
       {channelType === 'notebook' &&
         editorRef.current &&
         editorRef.current.editor && (
