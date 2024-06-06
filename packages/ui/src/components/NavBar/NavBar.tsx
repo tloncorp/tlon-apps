ec901aac8d2675fbd4cd098dc0e9661c315d211a
James Acklin <james.acklin@gmail.com>
2024-06-06 11:48:28 -0400

    NavBar: pad bottom up conditionally for Android

--- packages/ui/src/components/NavBar/NavBar.tsx
+++ packages/ui/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { getToken } from 'tamagui';
 
 import { XStack } from '../../core';
 import { View } from '../View';
@@ -8,13 +10,15 @@ const NavBar = React.memo(function NavBar(props: {
   children: React.ReactNode | React.ReactNode[] | null | undefined;
 }) {
   const { bottom } = useSafeAreaInsets();
+  const isAndroid = Platform.OS === 'android';
+  const largeSize = getToken('$l');
 
   return (
     <View
       width="100%"
       backgroundColor="$background"
-      paddingTop="$s"
-      paddingBottom={bottom}
+      paddingTop={isAndroid ? '$m' : '$s'}
+      paddingBottom={isAndroid ? largeSize + bottom : bottom}
     >
       <XStack justifyContent="space-around" alignItems="flex-start">
         {props.children}
