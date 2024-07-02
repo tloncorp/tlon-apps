21c084ef415e375c7357e4302acb9fc796e02c9f
~latter-bolden <brian@tlon.io>
2024-07-02 01:20:38 -0400

    only dot the bell when you have _meaningful_ unseen activity

--- apps/tlon-mobile/src/navigation/NavBarView.tsx
+++ apps/tlon-mobile/src/navigation/NavBarView.tsx
@@ -9,7 +9,7 @@ const NavBarView = (props: { navigation: any }) => {
   const isRouteActive = (routeName: string) => {
     return state.routes[state.index].name === routeName;
   };
-  const haveUnseenActivity = store.useHaveUnseenActivity();
+  const haveUnreadUnseenActivity = store.useHaveUnreadUnseenActivity();
   const currentUserId = useCurrentUserId();
 
   return (
@@ -26,7 +26,7 @@ const NavBarView = (props: { navigation: any }) => {
       <NavIcon
         type="Notifications"
         activeType="NotificationsFilled"
-        hasUnreads={haveUnseenActivity}
+        hasUnreads={haveUnreadUnseenActivity}
         isActive={isRouteActive('Activity')}
         onPress={() => props.navigation.navigate('Activity')}
       />
