e2cd5bd927ffcfd9c24b74114c90dd28661552fe
Patrick O'Sullivan <patrick@osullivan.io>
2024-07-09 06:59:56 -0500

    remove unused imports

--- packages/ui/src/components/ContentReference/index.tsx
+++ packages/ui/src/components/ContentReference/index.tsx
@@ -1,8 +1,6 @@
 import { ContentReference as ContentReferenceType } from '@tloncorp/shared/dist/api';
 
-import { Text } from '../../core';
 import { PostViewMode } from '../ContentRenderer';
-import Pressable from '../Pressable';
 import ChannelReference from './ChannelReference';
 import { GroupReference } from './GroupReference';
 import ReferenceSkeleton from './ReferenceSkeleton';
