a3eec6753315ae2b3100b57249bf9febce78a53c
~latter-bolden <brian@tlon.io>
2024-07-10 00:24:39 -0400

    add message when no users blocked

--- packages/ui/src/components/BlockedContactsWidget.tsx
+++ packages/ui/src/components/BlockedContactsWidget.tsx
@@ -1,4 +1,5 @@
 import * as db from '@tloncorp/shared/dist/db';
+import { SizableText, XStack } from 'tamagui';
 
 import { Badge } from './Badge';
 import { ContactList } from './ContactList';
@@ -10,6 +11,14 @@ export function BlockedContactsWidget({
   blockedContacts: db.Contact[];
   onBlockedContactPress: (contact: db.Contact) => void;
 }) {
+  if (blockedContacts.length === 0) {
+    return (
+      <XStack marginTop="$4xl" justifyContent="center">
+        <SizableText color="$secondaryText">No blocked users</SizableText>
+      </XStack>
+    );
+  }
+
   return (
     <ContactList borderWidth={0}>
       {blockedContacts.map((contact) => (
