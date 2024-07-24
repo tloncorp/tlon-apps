776026734f84be2609d6220e4a5f05befaacff62
David Lee <david.isaac.lee@gmail.com>
2024-07-24 13:43:58 -0700

    [wip] Split file along dependency boundary
    I marked this as [wip] because I think the pbxproj is corrupt at this
    commit - subsequent commits should fix if so.

--- apps/tlon-mobile/ios/Landscape/Extensions/Error+logWithDomain.swift
+++ apps/tlon-mobile/ios/Landscape/Extensions/Error+logWithDomain.swift
@@ -9,14 +9,6 @@ import Foundation
 import RNFBCrashlytics
 
 extension Error {
-    var isAFTimeout: Bool {
-        guard let afError = asAFError else {
-            return false
-        }
-
-        return afError.isSessionTaskError && (afError.underlyingError as? NSError)?.code == NSURLErrorTimedOut
-    }
-
     func logWithDomain(_ domain: String) {
         RNFBCrashlyticsNativeHelper.recordNativeError(NSError(
             domain: domain,
