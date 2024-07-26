ab472e204af77162b2a4ea5227431180b4171f48
David Lee <david.isaac.lee@gmail.com>
2024-07-26 10:45:28 -0700

    Add and use separate preview app group

--- apps/tlon-mobile/ios/Shared/Extensions/UserDefaults+standard.swift
+++ apps/tlon-mobile/ios/Shared/Extensions/UserDefaults+standard.swift
@@ -5,6 +5,10 @@ extension UserDefaults {
      * Accesses app group's shared user defaults. Almost all user defaults should go here.
      */
     class var appGroup: UserDefaults {
-        UserDefaults(suiteName: "group.io.tlon.groups")!
+        if let suiteName = Bundle.main.object(forInfoDictionaryKey: "TlonDefaultUserDefaultsSuiteName") as? String {
+            return UserDefaults(suiteName: suiteName)!
+        } else {
+            return UserDefaults.standard
+        }
     }
 }
