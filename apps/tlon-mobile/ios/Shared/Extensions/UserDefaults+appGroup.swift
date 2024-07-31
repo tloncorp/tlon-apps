e50c02a1a07c5b5b60b8aaf94de855182ff1d609
David Lee <david.isaac.lee@gmail.com>
2024-07-31 15:33:49 -0700

    Change TlonDefaultUserDefaultsSuiteName -> TlonDefaultAppGroup

--- apps/tlon-mobile/ios/Shared/Extensions/UserDefaults+appGroup.swift
+++ apps/tlon-mobile/ios/Shared/Extensions/UserDefaults+appGroup.swift
@@ -5,7 +5,7 @@ extension UserDefaults {
      * Accesses app group's shared user defaults. Almost all user defaults should go here.
      */
     class var appGroup: UserDefaults {
-        if let suiteName = Bundle.main.object(forInfoDictionaryKey: "TlonDefaultUserDefaultsSuiteName") as? String {
+        if let suiteName = Bundle.main.object(forInfoDictionaryKey: "TlonDefaultAppGroup") as? String {
             return UserDefaults(suiteName: suiteName)!
         } else {
             return UserDefaults.standard
