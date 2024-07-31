04ab6102d3044b906f090c3e548990d7d3a63b8b
David Lee <david.isaac.lee@gmail.com>
2024-07-31 15:35:50 -0700

    Use config to set HTTPCookieStorage per target

--- apps/tlon-mobile/ios/Shared/Extensions/Alamofire+sessionWithSharedCookieStorage.swift
+++ apps/tlon-mobile/ios/Shared/Extensions/Alamofire+sessionWithSharedCookieStorage.swift
@@ -3,9 +3,12 @@ import Foundation
 
 extension Session {
     static func withSharedCookieStorage() -> Session {
+        guard let appGroupId = Bundle.main.object(forInfoDictionaryKey: "TlonDefaultAppGroup") as? String else {
+            fatalError("Missing required plist entry: TlonDefaultAppGroup")
+        }
+
         let configuration = URLSessionConfiguration.af.default
-        // TODO: use active app group
-        configuration.httpCookieStorage = .sharedCookieStorage(forGroupContainerIdentifier: "group.io.tlon.groups.preview")
+        configuration.httpCookieStorage = .sharedCookieStorage(forGroupContainerIdentifier: appGroupId)
         return Session(configuration: configuration)
     }
 }
