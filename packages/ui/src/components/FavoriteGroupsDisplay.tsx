58b48e427aaee74fc06468d4b88fd5e0805edb30
~latter-bolden <brian@tlon.io>
2024-08-06 15:09:57 -0400

    hide last post info in favorite display

--- packages/ui/src/components/FavoriteGroupsDisplay.tsx
+++ packages/ui/src/components/FavoriteGroupsDisplay.tsx
@@ -127,7 +127,9 @@ export function FavoriteGroupsDisplay(props: {
                       />
                     </ListItem.EndContent>
                   </TouchableOpacity>
-                ) : null
+                ) : (
+                  <></>
+                )
               }
             />
           );
