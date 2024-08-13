ed355c5896e1ab14395110ed876ca5bccae5c8ad
David Lee <david.isaac.lee@gmail.com>
2024-08-13 13:11:31 -0700

    Add simple Jest config

---
+++ apps/tlon-mobile/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  moduleNameMapper: {
+    // mock react-native-svg-transformer
+    // https://github.com/kristerkari/react-native-svg-transformer?tab=readme-ov-file#usage-with-jest
+    '\\.svg': '<rootDir>/__mocks__/svgMock.ts',
+  },
+  preset: 'jest-expo',
+  transform: {
+    // from babel-jest-preset
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+
+    // from babel-jest-preset
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$':
+      require.resolve('jest-expo/src/preset/assetFileTransformer.js'),
+  },
+  transformIgnorePatterns: [
+    '/node_modules/\\@urbit/sigil-js/',
+    '\\.pnp\\.[^\\/]+$',
+  ],
+  testMatch: ['**/__uitests__/**/*.[jt]s?(x)'],
+};
