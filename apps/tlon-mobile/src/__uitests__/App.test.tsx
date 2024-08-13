09ae3bc5e2eace9fa1bd7b2cfa7b4d74b014e719
David Lee <david.isaac.lee@gmail.com>
2024-08-13 14:36:02 -0700

    Add App login UI test

---
+++ apps/tlon-mobile/src/__uitests__/App.test.tsx
@@ -0,0 +1,86 @@
+import {
+  render,
+  screen,
+  userEvent,
+  waitFor,
+} from '@testing-library/react-native';
+import fetchMock from 'jest-fetch-mock';
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
+import { act } from 'react-test-renderer';
+
+import App from '../App.main';
+import { setupDb } from '../lib/nativeDb';
+
+describe('App', () => {
+  beforeAll(() => {
+    setupDb();
+  });
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    fetchMock.resetMocks();
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it('renders', async () => {
+    fetchMock.enableMocks();
+
+    const user = userEvent.setup();
+
+    const app = await waitFor(() => {
+      return render(
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+          <App />
+        </SafeAreaProvider>
+      );
+    });
+
+    await user.press(await app.findByText('Configure self-hosted'));
+
+    await user.type(
+      screen.getByTestId('textInput shipUrl'),
+      'http://localhost'
+    );
+    await user.type(
+      screen.getByTestId('textInput accessCode'),
+      'sicben-tocdep-shanem-gillis'
+    );
+
+    fetchMock.once(async (req) => {
+      expect(req.url).toBe('http://localhost/~/login');
+      return {
+        status: 200,
+        headers: {
+          'set-cookie': 'urbauth-~botnul-banpex-ravseg-nosduc=',
+        },
+      };
+    });
+
+    await user.press(screen.getByText('Connect'));
+
+    // Wait for animations to complete
+    await act(async () => {
+      await jest.runAllTimersAsync();
+    });
+
+    const eulaLabel = screen.queryByText(/End-User License Agreement/);
+    expect(eulaLabel).not.toBeNull();
+
+    await user.press(screen.getByText('I Agree'));
+
+    // Wait for animations to complete
+    await act(async () => {
+      await jest.runAllTimersAsync();
+    });
+
+    // See mock of `AuthenticatedApp` in `jestSetup.tsx`
+    expect(screen.queryByText('AuthenticatedApp')).not.toBeNull();
+  });
+});
--- apps/tlon-mobile/src/test/mocksInternal.tsx
+++ apps/tlon-mobile/src/test/mocksInternal.tsx
@@ -1,5 +1,18 @@
 // Mocks for Tlon modules
 
+// HACK: `AuthenticatedApp` triggers a lot of setup that is hard to mock. We'll
+// need to remove this mock to test anything within `AuthenticatedApp`.
+jest.mock('../components/AuthenticatedApp', () => {
+  const { Text } = require('react-native');
+
+  return {
+    __esModule: true,
+    default: function AuthenticatedApp() {
+      return <Text>AuthenticatedApp</Text>;
+    },
+  };
+});
+
 jest.mock('../lib/opsqliteConnection', () => {
   const {
     OPSQLite$SQLiteConnection,
