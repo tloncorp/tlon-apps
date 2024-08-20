3284342febdd7fd6125710d930ae3173d4e482e4
David Lee <david.isaac.lee@gmail.com>
2024-08-20 10:33:54 -0700

    Explicitly exclude @react-native-firebase/perf from shared bundle

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
     'expo-image-picker',
     'expo-file-system',
     '@react-native-firebase/crashlytics',
+    '@react-native-firebase/perf',
     '@react-native-community/netinfo',
     'expo-battery',
   ],
