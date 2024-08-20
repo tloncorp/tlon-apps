75b69e96df5911b9fe6fdc3fc8dd9dd8f57d0d77
David Lee <david.isaac.lee@gmail.com>
2024-08-20 10:32:06 -0700

    Mock Firebase perf module in test

--- packages/shared/src/test/setup.ts
+++ packages/shared/src/test/setup.ts
@@ -19,6 +19,12 @@ vi.mock('@react-native-firebase/crashlytics', () => {
   };
 });
 
+vi.mock('@react-native-firebase/perf', () => {
+  return {
+    firebase: vi.fn(),
+  };
+});
+
 vi.mock('@react-native-community/netinfo', () => {
   return {
     fetch: async () => ({ isConnected: true, type: 'wifi' }),
