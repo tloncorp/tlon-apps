9c11454366f3ddee5d5e1f06c907df753cad6e24
David Lee <david.isaac.lee@gmail.com>
2024-08-29 21:37:16 -0700

    Add channel fixture with simulated post load

--- apps/tlon-mobile/src/fixtures/Channel.fixture.tsx
+++ apps/tlon-mobile/src/fixtures/Channel.fixture.tsx
@@ -7,11 +7,14 @@ import {
 import type { Upload } from '@tloncorp/shared/dist/api';
 import type * as db from '@tloncorp/shared/dist/db';
 import { Channel, ChannelSwitcherSheet } from '@tloncorp/ui';
+import { range } from 'lodash';
 import type { ComponentProps, PropsWithChildren } from 'react';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Button, SafeAreaView, View } from 'react-native';
 
 import { FixtureWrapper } from './FixtureWrapper';
 import {
+  createFakePost,
   createFakePosts,
   group,
   initialContacts,
@@ -248,6 +251,178 @@ function SwitcherFixture({
   );
 }
 
+function useSimulatedPostsQuery({
+  getPostAt = () => createFakePost(),
+}: Partial<{
+  getPostAt: (index: number) => db.Post;
+}> = {}) {
+  const postIndex = useRef(0);
+  const [posts, setPosts] = useState<db.Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadMore = useCallback(
+    async ({
+      limit = 10,
+      simulateLoadMs = 200,
+      insertionPoint = 'end',
+    }: Partial<{
+      limit: number;
+      simulateLoadMs: number;
+      insertionPoint: 'start' | 'end';
+    }>) => {
+      if (isLoading) {
+        return;
+      }
+      setIsLoading(true);
+      const page = range(postIndex.current, postIndex.current + limit).map(
+        (i) => getPostAt(i)
+      );
+      postIndex.current = postIndex.current + limit;
+      await new Promise((resolve) => setTimeout(resolve, simulateLoadMs));
+      setPosts((prev) =>
+        insertionPoint === 'start' ? [...page, ...prev] : [...prev, ...page]
+      );
+      setIsLoading(false);
+    },
+    [isLoading, getPostAt, postIndex]
+  );
+
+  return {
+    posts,
+    loadMore,
+    isLoading,
+  };
+}
+
+function FixtureToolbar({
+  children,
+}: {
+  children: (opts: {
+    doBusyWork: (fn: () => Promise<void>) => Promise<void>;
+  }) => React.ReactNode;
+}) {
+  const [isBusy, setIsBusy] = useState(false);
+  const doBusyWork = useCallback(async (fn: () => Promise<void>) => {
+    setIsBusy(true);
+    try {
+      await fn();
+    } finally {
+      setIsBusy(false);
+    }
+  }, []);
+
+  return (
+    <SafeAreaView
+      style={{
+        position: 'absolute',
+        right: 10,
+        top: 10,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'hsla(0, 0%, 100%, 0.8)',
+          borderColor: 'black',
+          borderWidth: 1,
+          borderRadius: 5,
+          opacity: isBusy ? 0.5 : 1,
+        }}
+        pointerEvents={isBusy ? 'none' : 'auto'}
+      >
+        {children({ doBusyWork })}
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function ChannelWithControlledPostLoading() {
+  const anchorPost = useMemo(() => createFakePost(), []);
+  const { posts, loadMore, isLoading } = useSimulatedPostsQuery({
+    getPostAt: (index) => {
+      // Insert anchor post near start, but enough to warrant scroll
+      if (index === 8) {
+        return anchorPost;
+      }
+      return createFakePost();
+    },
+  });
+
+  return (
+    <>
+      <ChannelFixture
+        negotiationMatch={true}
+        theme={'light'}
+        headerMode={'default'}
+        passedProps={(baseProps) => ({
+          posts,
+          isLoading,
+          initialChannelUnread: createTestChannelUnread({
+            channel: baseProps.channel,
+            post: anchorPost,
+          }),
+        })}
+      />
+      <FixtureToolbar>
+        {({ doBusyWork }) => (
+          <>
+            <Button
+              title="Load older"
+              onPress={() =>
+                doBusyWork(() => loadMore({ limit: 10, insertionPoint: 'end' }))
+              }
+            />
+            <Button
+              title="Load newer"
+              onPress={() =>
+                doBusyWork(() =>
+                  loadMore({ limit: 10, insertionPoint: 'start' })
+                )
+              }
+            />
+            <Button
+              title="Load blitz"
+              onPress={() => {
+                doBusyWork(async () => {
+                  for (let i = 0; i < 3; i++) {
+                    await loadMore({
+                      limit: 5,
+                      insertionPoint: 'start',
+                      simulateLoadMs: 30,
+                    });
+                    await loadMore({
+                      limit: 5,
+                      insertionPoint: 'end',
+                      simulateLoadMs: 30,
+                    });
+                  }
+                });
+              }}
+            />
+          </>
+        )}
+      </FixtureToolbar>
+    </>
+  );
+}
+
+function createTestChannelUnread({
+  channel,
+  post,
+}: {
+  channel: db.Channel;
+  post: db.Post;
+}): db.ChannelUnread {
+  return {
+    channelId: channel.id,
+    type: 'channel',
+    notify: false,
+    count: 1,
+    countWithoutThreads: 1,
+    updatedAt: post.sentAt,
+    firstUnreadPostId: post.id,
+  };
+}
+
 export default {
   chat: (
     <ChannelFixture
@@ -256,21 +431,17 @@ export default {
       headerMode={'default'}
     />
   ),
+  chatWithSimulatedLoad: <ChannelWithControlledPostLoading />,
   chatWithUnreadAnchor: (
     <ChannelFixture
       negotiationMatch={true}
       theme={'light'}
       headerMode={'default'}
       passedProps={(baseProps) => ({
-        initialChannelUnread: {
-          channelId: baseProps.channel.id,
-          type: 'channel',
-          notify: false,
-          count: 1,
-          countWithoutThreads: 1,
-          updatedAt: Date.now(),
-          firstUnreadPostId: baseProps.posts!.at(10)!.id,
-        },
+        initialChannelUnread: createTestChannelUnread({
+          channel: baseProps.channel,
+          post: baseProps.posts!.at(10)!,
+        }),
       })}
     />
   ),
