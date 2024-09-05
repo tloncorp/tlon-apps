47672cf20c24b5ef96f0edcebcd782189940bbd3
James Acklin <james.acklin@gmail.com>
2024-09-05 16:38:54 -0400

    web-new: update PostScreenController

--- apps/tlon-web-new/src/controllers/PostScreenController.tsx
+++ apps/tlon-web-new/src/controllers/PostScreenController.tsx
@@ -1,15 +1,29 @@
 import PostScreen from '@tloncorp/app/features/top/PostScreen';
 import { usePostWithThreadUnreads } from '@tloncorp/shared/dist';
+import { useCallback } from 'react';
 import { useNavigate, useParams } from 'react-router';
 
-export function PostScreenController() {
+export function PostScreenController(props: { navigation: any }) {
   const { postId } = useParams();
   const navigate = useNavigate();
   const { data: post } = usePostWithThreadUnreads({ id: postId ?? '' });
+
+  const handleGoToUserProfile = useCallback(
+    (userId: string) => {
+      props.navigation.push('UserProfile', { userId });
+    },
+    [props.navigation]
+  );
 
   if (!post) {
     return null;
   }
 
-  return <PostScreen postParam={post} goBack={() => navigate(-1)} />;
+  return (
+    <PostScreen
+      handleGoToUserProfile={handleGoToUserProfile}
+      postParam={post}
+      goBack={() => navigate(-1)}
+    />
+  );
 }
