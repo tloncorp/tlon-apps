299927985328bd08ca69c751ea5ed074badb25ca
~latter-bolden <brian@tlon.io>
2024-10-08 16:42:50 -0400

    store changes

--- packages/app/contexts/signup.tsx
+++ packages/app/contexts/signup.tsx
@@ -1,10 +1,29 @@
-import { createContext, useCallback, useContext, useState } from 'react';
+import * as store from '@tloncorp/shared/dist/store';
+import { createDevLogger } from 'packages/shared/dist';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+import { NodeBootPhase } from '../lib/bootHelpers';
+import BootHelpers from '../lib/bootHelpers';
+import { useLureMetadata } from './branch';
+import { useShip } from './ship';
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 interface SignupValues {
   nickname?: string;
   notificationToken?: string;
   telemetry?: boolean;
   didSignup?: boolean;
+
+  hostingUser: { id: string } | null;
+  reservedNodeId: string | null;
+  bootPhase: NodeBootPhase;
 }
 
 interface SignupContext extends SignupValues {
@@ -15,8 +34,11 @@ interface SignupContext extends SignupValues {
   clear: () => void;
 }
 
-const defaultContext = {
+const defaultContext: SignupContext = {
   nickname: undefined,
+  hostingUser: null,
+  reservedNodeId: null,
+  bootPhase: NodeBootPhase.IDLE,
   setNickname: () => {},
   setNotificationToken: () => {},
   setTelemetry: () => {},
@@ -24,10 +46,171 @@ const defaultContext = {
   clear: () => {},
 };
 
+const logger = createDevLogger('signup', true);
+
 const SignupContext = createContext<SignupContext>(defaultContext);
 
 export const SignupProvider = ({ children }: { children: React.ReactNode }) => {
-  const [values, setValues] = useState<SignupValues>({});
+  const [values, setValues] = useState<SignupValues>(defaultContext);
+  const { setShip } = useShip();
+  const lureMeta = useLureMetadata();
+
+  const setBootPhase = useCallback((bootPhase: NodeBootPhase) => {
+    setValues((current) => ({
+      ...current,
+      bootPhase,
+    }));
+  }, []);
+
+  const initializeBootSequence = useCallback(() => {
+    if (values.bootPhase === NodeBootPhase.IDLE) {
+      setValues((current) => ({
+        ...current,
+        bootPhase: NodeBootPhase.RESERVING,
+      }));
+    }
+  }, [values]);
+
+  const runBootSequence = useCallback(async () => {
+    const { hostingUser, bootPhase } = values;
+    logger.log(`running boot sequence phase: ${bootPhase}`);
+
+    if (!hostingUser) {
+      logger.log('no hosting user found, skipping');
+      return;
+    }
+
+    // Step 1: reserve a node
+    if (bootPhase === NodeBootPhase.RESERVING) {
+      const reservedNodeId = await BootHelpers.reserveNode(hostingUser.id);
+      setValues((current) => ({
+        ...current,
+        reservedNodeId,
+        bootPhase: NodeBootPhase.BOOTING,
+      }));
+      logger.log(`reserved node`, reservedNodeId);
+      return;
+    }
+
+    if (!values.reservedNodeId) {
+      throw new Error(
+        `cannot run boot phase ${bootPhase} without a reserved node`
+      );
+    }
+
+    // Step 2: confirm the node has finished booting on hosting
+    if (bootPhase === NodeBootPhase.BOOTING) {
+      const isReady = await BootHelpers.checkNodeBooted(values.reservedNodeId);
+      if (isReady) {
+        setValues((current) => ({
+          ...current,
+          bootPhase: NodeBootPhase.AUTHENTICATING,
+        }));
+        logger.log('checked hosting, node is ready');
+      } else {
+        logger.log('checked hosting, node still booting');
+      }
+      return;
+    }
+
+    // Step 3: authenticate with the node itself
+    if (bootPhase === NodeBootPhase.AUTHENTICATING) {
+      const auth = await BootHelpers.authenticateNode(values.reservedNodeId);
+      setShip({
+        ship: auth.nodeId,
+        shipUrl: auth.nodeUrl,
+        authCookie: auth.authCookie,
+      });
+
+      // TODO: connect to the API client?
+
+      const signedUpWithInvite = Boolean(lureMeta?.id);
+      setValues((current) => ({
+        ...current,
+        bootPhase: signedUpWithInvite
+          ? NodeBootPhase.CHECKING_FOR_INVITE
+          : NodeBootPhase.READY,
+      }));
+      return;
+    }
+
+    // Step 4 [optional]: if we used an invite code to signup, see if we got the invites
+    if (bootPhase === NodeBootPhase.CHECKING_FOR_INVITE) {
+      const { invitedDm, invitedGroup } =
+        await BootHelpers.getInvitedGroupAndDm(lureMeta);
+
+      if (invitedDm && invitedGroup) {
+        setValues((current) => ({
+          ...current,
+          bootPhase: NodeBootPhase.ACCEPTING_INVITES,
+        }));
+        logger.log('confirmed node has the invites');
+      } else {
+        logger.log('checked node for invites, not yet found');
+      }
+      return;
+    }
+
+    // Step 5 [optional]: join the invited groups
+    if (bootPhase === NodeBootPhase.ACCEPTING_INVITES) {
+      const { invitedDm, invitedGroup } =
+        await BootHelpers.getInvitedGroupAndDm(lureMeta);
+
+      if (invitedDm && invitedDm.isDmInvite) {
+        logger.log(`accepting dm invitation`);
+        await store.respondToDMInvite({ channel: invitedDm, accept: true });
+      }
+
+      if (
+        invitedGroup &&
+        !invitedGroup.currentUserIsMember &&
+        invitedGroup.haveInvite
+      ) {
+        logger.log('accepting group invitation');
+        await store.joinGroup(invitedGroup);
+      }
+
+      // give the join & accept some time to process, the hard refresh data
+      await wait(2000);
+      if (invitedGroup) {
+        await store.syncGroup(invitedGroup?.id);
+      }
+      if (invitedDm) {
+        await store.syncDms();
+      }
+
+      const { invitedDm: updatedDm, invitedGroup: updatedGroup } =
+        await BootHelpers.getInvitedGroupAndDm(lureMeta);
+
+      const dmIsGood = updatedDm && !updatedDm.isDmInvite;
+      const groupIsGood =
+        updatedGroup &&
+        updatedGroup.currentUserIsMember &&
+        updatedGroup.channels &&
+        updatedGroup.channels.length > 0;
+
+      if (dmIsGood && groupIsGood) {
+        setValues((current) => ({
+          ...current,
+          bootPhase: NodeBootPhase.READY,
+        }));
+        logger.log('successfully accepted invites');
+      } else {
+        logger.log('still waiting on invites to be accepted');
+      }
+      return;
+    }
+  }, [lureMeta, setShip, values]);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (values.bootPhase !== NodeBootPhase.READY) {
+      timer = setInterval(() => {
+        runBootSequence();
+      }, 5000);
+    }
+    return () => clearInterval(timer);
+  }, [values.bootPhase, runBootSequence]);
 
   const setNickname = useCallback((nickname: string | undefined) => {
     setValues((current) => ({
@@ -61,7 +244,11 @@ export const SignupProvider = ({ children }: { children: React.ReactNode }) => {
   }, []);
 
   const clear = useCallback(() => {
-    setValues({});
+    setValues({
+      bootPhase: NodeBootPhase.IDLE,
+      hostingUser: null,
+      reservedNodeId: null,
+    });
   }, []);
 
   return (
---
+++ packages/app/lib/bootHelpers.ts
@@ -0,0 +1,159 @@
+import { configureApi } from '@tloncorp/shared/dist/api';
+import { getLandscapeAuthCookie } from '@tloncorp/shared/dist/api';
+import * as db from '@tloncorp/shared/dist/db';
+import { useCallback, useEffect, useState } from 'react';
+
+import { LureData } from '../contexts/branch';
+import { useShip } from '../contexts/ship';
+import { useSignupContext } from '../contexts/signup';
+import * as hostingApi from '../lib/hostingApi';
+import { trackError, trackOnboardingAction } from '../utils/posthog';
+import { getShipFromCookie, getShipUrl } from '../utils/ship';
+
+export enum NodeBootPhase {
+  IDLE = 'idle',
+  RESERVING = 'reserving',
+  BOOTING = 'booting',
+  AUTHENTICATING = 'authenticating',
+  CHECKING_FOR_INVITE = 'checking-for-invite',
+  ACCEPTING_INVITES = 'accepting-invites',
+  READY = 'ready',
+  ERROR = 'error',
+}
+
+export default {
+  NodeBootPhase,
+  reserveNode,
+  checkNodeBooted,
+  authenticateNode,
+  getInvitedGroupAndDm,
+};
+
+export async function reserveNode(
+  hostingUserId: string,
+  skipShipIds: string[] = []
+): Promise<string> {
+  const user = await hostingApi.getHostingUser(hostingUserId);
+  // const shipIds = user.ships ?? [];
+
+  // if the hosting user already has a ship tied to their account, use that
+  if (user.ships?.length) {
+    return user.ships[0];
+  }
+
+  // otherwise reserve a new ship
+  const ships = await hostingApi.getReservableShips(hostingUserId);
+  const ship = ships.find(
+    ({ id, readyForDistribution }) =>
+      !skipShipIds.includes(id) && readyForDistribution
+  );
+  if (!ship) {
+    throw new Error('No available ships found.');
+  }
+
+  const { reservedBy } = await hostingApi.reserveShip(hostingUserId, ship.id);
+  if (reservedBy !== hostingUserId) {
+    return reserveNode(hostingUserId, [ship.id]);
+  }
+
+  await hostingApi.allocateReservedShip(hostingUserId);
+  // shipIds.push(ship.id);
+  trackOnboardingAction({
+    actionName: 'Urbit ID Selected',
+    ship: ship.id,
+  });
+
+  return ship.id;
+  // } catch (err) {
+  //   console.error('Error reserving ship:', err);
+  //   if (err instanceof Error) {
+  //     trackError(err);
+  //     // setError(err.message);
+  //   }
+  //   // setStatus(NodeBootPhase.ERROR);
+  //   // return false;
+  // }
+}
+
+async function checkNodeBooted(nodeId: string): Promise<boolean> {
+  // try {
+  const shipsWithStatus = await hostingApi.getShipsWithStatus([nodeId]);
+  if (!shipsWithStatus) {
+    return false;
+  }
+
+  const { status: shipStatus } = shipsWithStatus;
+
+  if (shipStatus !== 'Ready') {
+    return false;
+  }
+
+  return true;
+
+  // const { code: accessCode } = await hostingApi.getShipAccessCode(shipId);
+  // const shipUrl = getShipUrl(shipId);
+  // const authCookie = await getLandscapeAuthCookie(shipUrl, accessCode);
+  // if (!authCookie) {
+  //   throw new Error("Couldn't log you into your ship.");
+  // }
+
+  // const ship = getShipFromCookie(authCookie);
+  // configureApi(ship, shipUrl);
+
+  // setShip({
+  //   ship,
+  //   shipUrl,
+  //   authCookie,
+  // });
+
+  // setStatus(NodeBootPhase.READY);
+  // return NodeBootPhase.READY;
+  // } catch (err) {
+  //   console.error('Error starting ship:', err);
+  //   if (err instanceof Error) {
+  //     trackError(err);
+  //     // setError(err.message);
+  //   }
+  //   // setStatus(NodeBootPhase.ERROR);
+  //   return NodeBootPhase.ERROR;
+  // }
+}
+
+async function authenticateNode(
+  nodeId: string
+): Promise<{ nodeId: string; nodeUrl: string; authCookie: string }> {
+  const { code: accessCode } = await hostingApi.getShipAccessCode(nodeId);
+  const nodeUrl = getShipUrl(nodeId);
+  const authCookie = await getLandscapeAuthCookie(nodeUrl, accessCode);
+  if (!authCookie) {
+    throw new Error("Couldn't log you into your ship.");
+  }
+
+  // TODO: shouldn't this be the same?
+  const ship = getShipFromCookie(authCookie);
+
+  return {
+    nodeId,
+    nodeUrl,
+    authCookie,
+  };
+}
+
+async function getInvitedGroupAndDm(
+  lureMeta: LureData | null
+): Promise<{ invitedDm: db.Channel | null; invitedGroup: db.Group | null }> {
+  if (!lureMeta) {
+    throw new Error('no stored invite found, cannot check');
+  }
+  const { invitedGroupId, inviterUserId } = lureMeta;
+  if (!invitedGroupId || !inviterUserId) {
+    throw new Error(
+      `invalid invite metadata: group[${invitedGroupId}] inviter[${inviterUserId}]`
+    );
+  }
+  // use api client to see if you have pending DM and group invite
+  const invitedDm = await db.getChannel({ id: inviterUserId });
+  const invitedGroup = await db.getGroup({ id: invitedGroupId });
+
+  return { invitedDm, invitedGroup };
+}
