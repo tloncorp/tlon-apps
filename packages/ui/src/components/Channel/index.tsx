51fef66b918cd206719d2d09f30f5b0b09550e23
David Lee <david.isaac.lee@gmail.com>
2024-10-10 10:55:19 -0700

    Rename PostContentRendererContext -> ComponentsKitContext

--- packages/ui/src/components/Channel/index.tsx
+++ packages/ui/src/components/Channel/index.tsx
@@ -27,9 +27,9 @@ import {
 } from '../../contexts';
 import { Attachment, AttachmentProvider } from '../../contexts/attachment';
 import {
-  PostContentRendererContextProvider,
+  ComponentsKitContextProvider,
   RenderItemType,
-  usePostContentRenderersContext,
+  useComponentsKitContext,
 } from '../../contexts/componentsKits';
 import { RequestsProvider } from '../../contexts/requests';
 import { ScrollContextProvider } from '../../contexts/scroll';
@@ -269,7 +269,7 @@ export function Channel({
     <ScrollContextProvider>
       <GroupsProvider groups={groups}>
         <ChannelProvider value={{ channel }}>
-          <PostContentRendererContextProvider>
+          <ComponentsKitContextProvider>
             <RequestsProvider
               usePost={usePost}
               usePostReference={usePostReference}
@@ -432,7 +432,7 @@ export function Channel({
                 </AttachmentProvider>
               </NavigationProvider>
             </RequestsProvider>
-          </PostContentRendererContextProvider>
+          </ComponentsKitContextProvider>
         </ChannelProvider>
       </GroupsProvider>
     </ScrollContextProvider>
@@ -459,15 +459,23 @@ function NegotionMismatchNotice() {
 
 const PostView: RenderItemType = (props) => {
   const channel = useChannelContext();
-  const { renderers } = usePostContentRenderersContext();
+  const { renderers } = useComponentsKitContext();
 
   const SpecificPostComponent = useMemo(() => {
-    const contentConfig = channel.contentConfiguration;
-    if (contentConfig != null) {
-      const rendererId = contentConfig.defaultPostContentRenderer;
-      if (rendererId != null && renderers[rendererId] != null) {
-        return renderers[rendererId];
+    // why do this iife?
+    // without it, TypeScript thinks the value from `renderers[]` may be null.
+    // sad!
+    const rendererFromContentConfig = (() => {
+      const contentConfig = channel.contentConfiguration;
+      if (
+        contentConfig != null &&
+        renderers[contentConfig.defaultPostContentRenderer] != null
+      ) {
+        return renderers[contentConfig.defaultPostContentRenderer];
       }
+    })();
+    if (rendererFromContentConfig != null) {
+      return rendererFromContentConfig;
     }
 
     // content config did not provide a renderer, fall back to default
@@ -492,9 +500,9 @@ const PostView: RenderItemType = (props) => {
 
 function DraftInputView(props: {
   draftInputContext: DraftInputContext;
-  type: string;
+  type: DraftInputId;
 }) {
-  const { inputs } = usePostContentRenderersContext();
+  const { inputs } = useComponentsKitContext();
   const InputComponent = inputs[props.type];
   if (InputComponent) {
     return <InputComponent draftInputContext={props.draftInputContext} />;
