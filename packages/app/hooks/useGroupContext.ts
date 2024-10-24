a667a870308d79a946c4d451099d4953a745fd67
David Lee <david.isaac.lee@gmail.com>
2024-10-24 12:35:56 -0700

    Pull useUpdateChannel into shared

--- packages/app/hooks/useGroupContext.ts
+++ packages/app/hooks/useGroupContext.ts
@@ -1,4 +1,4 @@
-import { sync, useCreateChannel } from '@tloncorp/shared';
+import { sync, useUpdateChannel } from '@tloncorp/shared';
 import * as db from '@tloncorp/shared/dist/db';
 import * as store from '@tloncorp/shared/dist/store';
 import { useCallback, useEffect, useMemo } from 'react';
@@ -108,25 +108,15 @@ export const useGroupContext = ({
     [group]
   );
 
+  const _updateChannel = useUpdateChannel();
   const updateChannel = useCallback(
     async (channel: db.Channel) => {
-      const navSection = groupNavSections.find((section) =>
-        section.channels.map((c) => c.channelId).includes(channel.id)
-      );
-
-      if (!navSection || !group) {
-        return;
+      if (group == null) {
+        throw new Error('Group is null');
       }
-
-      await store.updateChannel({
-        groupId: group.id,
-        channel,
-        sectionId: navSection.sectionId,
-        readers: channel.readerRoles?.map((r) => r.roleId) ?? [],
-        join: true,
-      });
+      return _updateChannel({ channel, group });
     },
-    [group, groupNavSections]
+    [group, _updateChannel]
   );
 
   const createNavSection = useCallback(
