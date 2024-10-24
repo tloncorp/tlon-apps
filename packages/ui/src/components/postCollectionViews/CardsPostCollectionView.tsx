a5036a736ab0fa2799fb8b27ffafed68ca934954
David Lee <david.isaac.lee@gmail.com>
2024-10-24 11:20:33 -0700

    Make cards collection view load more posts; add buttons

--- packages/ui/src/components/postCollectionViews/CardsPostCollectionView.tsx
+++ packages/ui/src/components/postCollectionViews/CardsPostCollectionView.tsx
@@ -1,7 +1,14 @@
-import { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from 'react';
 import { Text, View, useWindowDimensions } from 'tamagui';
 
 import { usePostCollectionContextUnsafelyUnwrapped } from '../../contexts/postCollection';
+import { Button } from '../Button';
 import { IPostCollectionView } from './shared';
 
 export const CardsPostCollection: IPostCollectionView = forwardRef(
@@ -33,27 +40,80 @@ export const CardsPostCollection: IPostCollectionView = forwardRef(
 
     const windowDimensions = useWindowDimensions();
 
+    useEffect(() => {
+      if (displayedIndex === 0 && ctx.hasOlderPosts) {
+        ctx.onScrollEndReached?.();
+      }
+      if (
+        ctx.posts &&
+        displayedIndex === ctx.posts.length - 1 &&
+        ctx.hasNewerPosts
+      ) {
+        ctx.onScrollStartReached?.();
+      }
+    }, [ctx, displayedIndex]);
+
+    const jog = useCallback(
+      (delta: number) => {
+        if (
+          displayedIndex != null &&
+          ctx.posts != null &&
+          ctx.posts.length > 0
+        ) {
+          const postsCount = ctx.posts.length;
+          setDisplayedIndex((prev) => {
+            if (prev == null) {
+              return 0;
+            }
+            let next = (prev + delta) % postsCount;
+            while (next < 0) {
+              next += postsCount;
+            }
+            return next;
+          });
+        } else {
+          setDisplayedIndex(null);
+        }
+      },
+      [ctx.posts, displayedIndex]
+    );
+
     return (
       <View
         onPress={(event) => {
-          if (displayedIndex != null && ctx.posts != null) {
-            const delta =
-              event.nativeEvent.locationX < windowDimensions.width * 0.5
-                ? -1
-                : 1;
-            setDisplayedIndex((displayedIndex + delta) % ctx.posts.length);
-          } else {
-            setDisplayedIndex(null);
-          }
+          jog(
+            event.nativeEvent.locationX < windowDimensions.width * 0.5 ? -1 : 1
+          );
         }}
         alignItems="stretch"
         justifyContent="center"
         flex={1}
       >
-        {displayedPost == null ? (
-          <Text>Loading...</Text>
-        ) : (
-          <ctx.PostView post={displayedPost} showAuthor />
+        <View flex={1} alignItems="stretch" justifyContent="center">
+          {displayedPost == null ? (
+            <Text>Loading...</Text>
+          ) : (
+            <ctx.PostView post={displayedPost} showAuthor />
+          )}
+        </View>
+        {ctx.posts == null ? null : (
+          <View
+            alignSelf="center"
+            padding="$l"
+            flexDirection="row"
+            alignItems="center"
+            gap="$l"
+          >
+            <Button onPress={() => jog(-1)}>
+              <Text>Previous</Text>
+            </Button>
+            <Text>
+              {(displayedIndex ?? 0) + 1} / {ctx.posts.length}
+            </Text>
+            <Button onPress={() => jog(1)}>
+              <Button.Text>Next</Button.Text>
+            </Button>
+          </View>
         )}
       </View>
     );
