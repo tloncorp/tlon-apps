4ddbf656c17cec62b1146cf936bb0c37490bfea5
Patrick O'Sullivan <patrick@osullivan.io>
2024-10-29 13:06:39 -0500

    Fix height/padding issues with BareChatInput on web, add new eslint rule to prevent use of getToken since it could break things when we're expecting to manipulate raw values

--- packages/ui/src/components/BigInput.native.tsx
+++ packages/ui/src/components/BigInput.native.tsx
@@ -4,7 +4,7 @@ import { Dimensions, KeyboardAvoidingView, Platform } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 // TODO: replace input with our own input component
-import { Input, View, YStack, getToken } from 'tamagui';
+import { Input, View, YStack, getTokenValue } from 'tamagui';
 
 import { ImageAttachment, useAttachmentContext } from '../contexts/attachment';
 import AttachmentSheet from './AttachmentSheet';
@@ -40,8 +40,8 @@ export function BigInput({
   }>(null);
   const { top } = useSafeAreaInsets();
   const { width } = Dimensions.get('screen');
-  const titleInputHeight = getToken('$4xl', 'size');
-  const imageButtonHeight = getToken('$4xl', 'size');
+  const titleInputHeight = getTokenValue('$4xl', 'size');
+  const imageButtonHeight = getTokenValue('$4xl', 'size');
   const keyboardVerticalOffset =
     Platform.OS === 'ios' ? top + titleInputHeight : top;
 
