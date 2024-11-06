3eebbe8b68263404aad3bfc52de03be293e49b94
David Lee <david.isaac.lee@gmail.com>
2024-11-06 09:38:49 -0800

    Add scrollToIndex method on Carousel ref

--- apps/tlon-mobile/src/fixtures/Carousel.fixture.tsx
+++ apps/tlon-mobile/src/fixtures/Carousel.fixture.tsx
@@ -1,7 +1,7 @@
-import { Carousel, IconButton } from '@tloncorp/ui';
-import { Close } from 'packages/ui/src/assets/icons';
-import { useCallback, useState } from 'react';
+import { Button, Carousel } from '@tloncorp/ui';
+import { useCallback, useRef, useState } from 'react';
 import { useSelect, useValue } from 'react-cosmos/client';
+import { Alert } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text, View } from 'tamagui';
@@ -27,10 +27,13 @@ export default function CarouselFixture() {
 
   const [visibleIndex, setVisibleIndex] = useState<number | null>(null);
 
+  const carouselRef = useRef<React.ElementRef<typeof Carousel>>(null);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View flex={1} {...(fullscreen ? null : { padding: 100 })}>
         <Carousel
+          ref={carouselRef}
           flex={1}
           scrollDirection={direction}
           onVisibleIndexChange={setVisibleIndex}
@@ -48,7 +51,6 @@ export default function CarouselFixture() {
                         edges={['top', 'right', 'left']}
                         style={{
                           padding: 40,
-                          // backgroundColor: 'rgba(0, 0, 0, 0.5)',
                           backgroundColor: colors[i % colors.length],
                         }}
                       >
@@ -60,7 +62,7 @@ export default function CarouselFixture() {
                         edges={['bottom', 'right', 'left']}
                         style={{
                           padding: 40,
-                          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                          backgroundColor: 'rgba(255, 255, 255, 0.5)',
                         }}
                       >
                         <Text>{uri}</Text>
@@ -76,8 +78,25 @@ export default function CarouselFixture() {
               </Carousel.Item>
             ))}
         </Carousel>
-        <View position="absolute" top={60} right={40}>
-          <Text>Showing index: {visibleIndex}</Text>
+        <View position="absolute" top={60} right={20}>
+          <Button
+            onPress={() => {
+              Alert.prompt('Go to index', undefined, (input: string) => {
+                try {
+                  const n = parseInt(input);
+                  if (n >= 0 && n < pageCount) {
+                    carouselRef.current?.scrollToIndex(n);
+                  } else {
+                    throw null;
+                  }
+                } catch {
+                  Alert.alert('Invalid index');
+                }
+              });
+            }}
+          >
+            <Text>Showing index: {visibleIndex}</Text>
+          </Button>
         </View>
       </View>
     </GestureHandlerRootView>
