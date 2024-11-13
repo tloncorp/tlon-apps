b58251904fb109bb511b96175f2bcabd95fcf173
~latter-bolden <brian@tlon.io>
2024-11-13 13:41:37 -0500

    use lure provider for fetching invite link metadata

--- packages/shared/src/logic/deeplinks.ts
+++ packages/shared/src/logic/deeplinks.ts
@@ -6,7 +6,7 @@ export async function getReferenceFromDeeplink(
   url: string,
   branchKey: string
 ): Promise<{ reference: ContentReference; path: string } | null> {
-  const linkMeta = await getBranchLinkMeta(url, branchKey);
+  const linkMeta = await getInviteLinkMeta(url, branchKey);
 
   if (linkMeta && typeof linkMeta === 'object') {
     if (isLureMeta(linkMeta) && linkMeta.invitedGroupId) {
@@ -28,7 +28,7 @@ export async function getMetadaFromInviteLink(
   url: string,
   branchKey: string
 ): Promise<DeepLinkMetadata | null> {
-  const linkMeta = await getBranchLinkMeta(url, branchKey);
+  const linkMeta = await getInviteLinkMeta(url, branchKey);
   if (linkMeta && typeof linkMeta === 'object') {
     if (isLureMeta(linkMeta)) {
       return linkMeta;
@@ -37,10 +37,96 @@ export async function getMetadaFromInviteLink(
   return null;
 }
 
+interface ProviderMetadataResponse {
+  fields: {
+    image?: string;
+    title?: string;
+    cover?: string;
+    description?: string;
+    group?: string;
+    inviter?: string;
+    inviterNickname?: string;
+    inviterAvatarImage?: string;
+  };
+}
+
+async function getInviteLinkMeta(
+  inviteLink: string,
+  branchDomain: string
+): Promise<DeepLinkMetadata | null> {
+  const token = extractTokenFromInviteLink(inviteLink, branchDomain);
+  if (!token) {
+    return null;
+  }
+
+  const providerResponse = await fetch(
+    `https://loshut-lonreg.tlon.network/lure/${token}/metadata`
+  );
+  if (!providerResponse.ok) {
+    return null;
+  }
+
+  // fetch invite link metadata from lure provider
+  const responseMeta: ProviderMetadataResponse = await providerResponse.json();
+  if (
+    !responseMeta.fields ||
+    !responseMeta.fields.group ||
+    !responseMeta.fields.inviter
+  ) {
+    return null;
+  }
+
+  const metadata: DeepLinkMetadata = {
+    inviterUserId: responseMeta.fields.inviter,
+    invitedGroupId: responseMeta.fields.group,
+    invitedGroupTitle: responseMeta.fields.title,
+    invitedGroupDescription: responseMeta.fields.description,
+    invitedGroupIconImageUrl: responseMeta.fields.image,
+    inviterNickname: responseMeta.fields.inviterNickname,
+    inviterAvatarImage: responseMeta.fields.inviterAvatarImage,
+  };
+
+  // some links might not have everything, try to extend with branch (fine if fails)
+  if (!metadata.inviterNickname) {
+    try {
+      const branchMeta = await getBranchLinkMeta(inviteLink, branchDomain);
+      if (branchMeta) {
+        if (branchMeta.inviterNickname && !metadata.inviterNickname) {
+          metadata.inviterNickname = branchMeta.inviterNickname;
+        }
+        if (branchMeta.inviterAvatarImage && !metadata.inviterAvatarImage) {
+          metadata.inviterAvatarImage = branchMeta.inviterAvatarImage;
+        }
+      }
+    } catch (e) {
+      console.error('Failed to fetch branch metadata. Ignoring', e);
+    }
+  }
+
+  return metadata;
+}
+
 export function createInviteLinkRegex(branchDomain: string) {
   return new RegExp(
     `^(https?://)?(${branchDomain}/|tlon\\.network/lure/)0v[^/]+$`
   );
+}
+
+export function extractTokenFromInviteLink(
+  url: string,
+  branchDomain: string
+): string | null {
+  if (!url) return null;
+  const INVITE_LINK_REGEX = createInviteLinkRegex(branchDomain);
+  const match = url.trim().match(INVITE_LINK_REGEX);
+
+  if (match) {
+    const parts = match[0].split('/');
+    const token = parts[parts.length - 1];
+    return token ?? null;
+  }
+
+  return null;
 }
 
 export function extractNormalizedInviteLink(
