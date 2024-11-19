986f65e0767408c089b271bd14d09e525982a699
David Lee <david.isaac.lee@gmail.com>
2024-11-19 10:11:01 -0800

    Show replies in DetailPostUsingContentConfiguration

--- packages/ui/src/components/DetailPostUsingContentConfiguration.tsx
+++ packages/ui/src/components/DetailPostUsingContentConfiguration.tsx
@@ -1,9 +1,16 @@
 import * as db from '@tloncorp/shared/db';
+import { useThreadPosts } from 'packages/shared/src';
+import { useMemo } from 'react';
+import { FlatList } from 'react-native';
 import { YStack } from 'tamagui';
 
 import { usePostCollectionContextUnsafelyUnwrapped } from '../contexts/postCollection';
 import { ForwardingProps } from '../utils/react';
 import { ChannelHeader } from './Channel/ChannelHeader';
+
+type ListItem =
+  | { type: 'op'; post: db.Post }
+  | { type: 'reply'; post: db.Post };
 
 export function DetailPostView({
   post,
@@ -17,6 +24,9 @@ export function DetailPostView({
   const { PostView, channel } = usePostCollectionContextUnsafelyUnwrapped();
   // use boolean coercion to also check if post.title is empty string
   const title = post.title ? post.title : 'Post';
+
+  const listData = useListData({ root: post });
+
   return (
     <YStack backgroundColor={'$background'} {...forwardedProps}>
       <ChannelHeader
@@ -29,7 +39,34 @@ export function DetailPostView({
         post={post}
         mode="default"
       />
-      <PostView post={post} />
+      <FlatList
+        data={listData}
+        renderItem={({ item }) => (
+          <PostView post={item.post} showReplies={item.type !== 'op'} />
+        )}
+        contentInsetAdjustmentBehavior="scrollableAxes"
+      />
     </YStack>
   );
+}
+
+function useListData({ root }: { root: db.Post }) {
+  const threadPostsQuery = useThreadPosts({
+    postId: root.id,
+    authorId: root.authorId,
+    channelId: root.channelId,
+  });
+
+  return useMemo(() => {
+    const out: ListItem[] = [{ type: 'op', post: root }];
+    if (threadPostsQuery.data != null && threadPostsQuery.data?.length > 0) {
+      // threadPostsQuery is in timestamp descending order
+      // iterate backwards to avoid a reverse()
+      for (let i = threadPostsQuery.data.length - 1; i >= 0; i--) {
+        const reply = threadPostsQuery.data[i];
+        out.push({ type: 'reply', post: reply });
+      }
+    }
+    return out;
+  }, [root, threadPostsQuery.data]);
 }
