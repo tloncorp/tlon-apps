65a50c0c359c9560e86c346d392b0001f7f99262
David Lee <david.isaac.lee@gmail.com>
2024-11-19 10:37:00 -0800

    Use Pressable for audio post tap

--- packages/ui/src/components/AudioPost.tsx
+++ packages/ui/src/components/AudioPost.tsx
@@ -5,6 +5,7 @@ import { RenderItemType } from '../contexts/componentsKits';
 import { ChatAuthorRow } from './AuthorRow';
 import { AudioPlayer, AudioPlayerHandle } from './Embed/AudioEmbed';
 import { convertContent } from './PostContent/contentUtils';
+import Pressable from './Pressable';
 
 export const AudioPost: RenderItemType = (props) => {
   const audioUrl = useMemo(() => {
@@ -29,12 +30,12 @@ export const AudioPost: RenderItemType = (props) => {
   }, []);
 
   return (
-    <View padding={20} onPress={togglePlayback}>
+    <Pressable padding={20} onPress={togglePlayback}>
       <ChatAuthorRow
         author={props.post.author}
         authorId={props.post.authorId}
       />
       {audioUrl && <AudioPlayer url={audioUrl} ref={playerRef} />}
-    </View>
+    </Pressable>
   );
 };
