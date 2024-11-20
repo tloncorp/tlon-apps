ee4dd8b5f97199143e7bea5115cc999575c4f4b9
David Lee <david.isaac.lee@gmail.com>
2024-11-20 11:10:48 -0800

    [wip] Add context menu to posts in detail view (no action functionality)

--- packages/ui/src/components/DetailPostUsingContentConfiguration.tsx
+++ packages/ui/src/components/DetailPostUsingContentConfiguration.tsx
@@ -1,12 +1,21 @@
+import { ChannelAction, useThreadPosts } from '@tloncorp/shared';
 import * as db from '@tloncorp/shared/db';
-import { useThreadPosts } from 'packages/shared/src';
-import { useMemo } from 'react';
-import { FlatList } from 'react-native';
+import {
+  ElementRef,
+  RefObject,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import { FlatList, View as RNView } from 'react-native';
 import { YStack } from 'tamagui';
 
 import { usePostCollectionContextUnsafelyUnwrapped } from '../contexts/postCollection';
 import { ForwardingProps } from '../utils/react';
 import { ChannelHeader } from './Channel/ChannelHeader';
+import { ChatMessageActions } from './ChatMessage/ChatMessageActions/Component';
+import { Modal } from './Modal';
 
 type ListItem =
   | { type: 'op'; post: db.Post }
@@ -27,26 +36,66 @@ export function DetailPostView({
 
   const listData = useListData({ root: post });
 
+  const postContextMenu = usePostContextMenu(
+    useMemo(
+      () => ({
+        performPostAction: (actionType, _post) => {
+          switch (actionType) {
+            case 'viewReactions': {
+              break;
+            }
+
+            case 'startThread': {
+              break;
+            }
+
+            case 'edit': {
+              break;
+            }
+          }
+        },
+      }),
+      []
+    )
+  );
+
   return (
-    <YStack backgroundColor={'$background'} {...forwardedProps}>
-      <ChannelHeader
-        channel={channel}
-        group={channel.group}
-        title={title}
-        goBack={navigateBack}
-        showSearchButton={false}
-        // showSpinner={isLoadingPosts}
-        post={post}
-        mode="default"
-      />
-      <FlatList
-        data={listData}
-        renderItem={({ item }) => (
-          <PostView post={item.post} showReplies={item.type !== 'op'} />
-        )}
-        contentInsetAdjustmentBehavior="scrollableAxes"
-      />
-    </YStack>
+    <>
+      <YStack backgroundColor={'$background'} {...forwardedProps}>
+        <ChannelHeader
+          channel={channel}
+          group={channel.group}
+          title={title}
+          goBack={navigateBack}
+          showSearchButton={false}
+          // showSpinner={isLoadingPosts}
+          post={post}
+          mode="default"
+        />
+        <FlatList
+          data={listData}
+          renderItem={({ item }) => (
+            <ContextGestureListener
+              presentationCandidate={{
+                post: item.post,
+                postActionIds: ['delete', 'edit', 'quote'],
+              }}
+              menuApi={postContextMenu}
+            >
+              {({ present }) => (
+                <PostView
+                  post={item.post}
+                  showReplies={item.type !== 'op'}
+                  onLongPress={present}
+                />
+              )}
+            </ContextGestureListener>
+          )}
+          contentInsetAdjustmentBehavior="scrollableAxes"
+        />
+      </YStack>
+      {postContextMenu.mount()}
+    </>
   );
 }
 
@@ -69,4 +118,96 @@ function useListData({ root }: { root: db.Post }) {
     }
     return out;
   }, [root, threadPostsQuery.data]);
+}
+
+interface PostContextMenuPresentation {
+  anchorRef: RefObject<ElementRef<typeof RNView>>;
+  postActionIds: ChannelAction.Id[];
+  post: db.Post;
+}
+
+// ensures that `Sub` is a narrowing of `T` - so if `T` changes, `Sub`
+// will be assured to remain a subtype
+type PickSubtype<T, Sub extends T> = Sub;
+
+type PostContextMenuActions = PickSubtype<
+  ChannelAction.Id,
+  'viewReactions' | 'startThread' | 'edit'
+>;
+function usePostContextMenu(opts: {
+  performPostAction: (
+    actionType: PostContextMenuActions,
+    post: db.Post
+  ) => void;
+}) {
+  const [presentation, setPresentation] =
+    useState<PostContextMenuPresentation | null>(null);
+
+  const performActionAndDismissCallback = useCallback(
+    (actionType: PostContextMenuActions) => {
+      return () => {
+        if (presentation == null) {
+          return;
+        }
+        opts.performPostAction(actionType, presentation.post);
+        setPresentation(null);
+      };
+    },
+    [opts, presentation]
+  );
+
+  const mount = useCallback(() => {
+    return (
+      <Modal
+        visible={presentation !== null}
+        onDismiss={() => setPresentation(null)}
+      >
+        {presentation !== null && (
+          <ChatMessageActions
+            post={presentation.post}
+            postActionIds={presentation.postActionIds}
+            postRef={presentation.anchorRef}
+            onDismiss={() => setPresentation(null)}
+            onReply={performActionAndDismissCallback('startThread')}
+            onEdit={performActionAndDismissCallback('edit')}
+            onViewReactions={performActionAndDismissCallback('viewReactions')}
+          />
+        )}
+      </Modal>
+    );
+  }, [performActionAndDismissCallback, presentation]);
+
+  return useMemo(
+    () => ({
+      present: setPresentation,
+      dismiss: () => setPresentation(null),
+      mount,
+    }),
+    [mount]
+  );
+}
+
+function ContextGestureListener({
+  children,
+  menuApi,
+  presentationCandidate,
+}: {
+  children: (api: { present: () => void }) => JSX.Element;
+  menuApi: {
+    present: (opts: PostContextMenuPresentation) => void;
+    dismiss: () => void;
+  };
+  presentationCandidate: Pick<
+    PostContextMenuPresentation,
+    'post' | 'postActionIds'
+  >;
+}) {
+  const contentContainer = useRef<ElementRef<typeof RNView>>(null);
+  const present = useCallback(() => {
+    menuApi.present({
+      ...presentationCandidate,
+      anchorRef: contentContainer,
+    });
+  }, [contentContainer, menuApi, presentationCandidate]);
+  return <RNView ref={contentContainer}>{children({ present })}</RNView>;
 }
