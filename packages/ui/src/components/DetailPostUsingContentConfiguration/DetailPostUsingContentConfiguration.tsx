f65a5638b3241ea5531260e95d8b9decfaedafad
David Lee <david.isaac.lee@gmail.com>
2024-11-20 11:15:46 -0800

    Pull context menu code into own file

--- packages/ui/src/components/DetailPostUsingContentConfiguration/DetailPostUsingContentConfiguration.tsx
+++ packages/ui/src/components/DetailPostUsingContentConfiguration/DetailPostUsingContentConfiguration.tsx
@@ -1,21 +1,13 @@
-import { ChannelAction, useThreadPosts } from '@tloncorp/shared';
+import { useThreadPosts } from '@tloncorp/shared';
 import * as db from '@tloncorp/shared/db';
-import {
-  ElementRef,
-  RefObject,
-  useCallback,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
-import { FlatList, View as RNView } from 'react-native';
+import { useMemo } from 'react';
+import { FlatList } from 'react-native';
 import { YStack } from 'tamagui';
 
 import { usePostCollectionContextUnsafelyUnwrapped } from '../../contexts/postCollection';
 import { ForwardingProps } from '../../utils/react';
 import { ChannelHeader } from '../Channel/ChannelHeader';
-import { ChatMessageActions } from '../ChatMessage/ChatMessageActions/Component';
-import { Modal } from '../Modal';
+import { ContextGestureListener, usePostContextMenu } from './contextmenu';
 
 type ListItem =
   | { type: 'op'; post: db.Post }
@@ -118,96 +110,4 @@ function useListData({ root }: { root: db.Post }) {
     }
     return out;
   }, [root, threadPostsQuery.data]);
-}
-
-interface PostContextMenuPresentation {
-  anchorRef: RefObject<ElementRef<typeof RNView>>;
-  postActionIds: ChannelAction.Id[];
-  post: db.Post;
-}
-
-// ensures that `Sub` is a narrowing of `T` - so if `T` changes, `Sub`
-// will be assured to remain a subtype
-type PickSubtype<T, Sub extends T> = Sub;
-
-type PostContextMenuActions = PickSubtype<
-  ChannelAction.Id,
-  'viewReactions' | 'startThread' | 'edit'
->;
-function usePostContextMenu(opts: {
-  performPostAction: (
-    actionType: PostContextMenuActions,
-    post: db.Post
-  ) => void;
-}) {
-  const [presentation, setPresentation] =
-    useState<PostContextMenuPresentation | null>(null);
-
-  const performActionAndDismissCallback = useCallback(
-    (actionType: PostContextMenuActions) => {
-      return () => {
-        if (presentation == null) {
-          return;
-        }
-        opts.performPostAction(actionType, presentation.post);
-        setPresentation(null);
-      };
-    },
-    [opts, presentation]
-  );
-
-  const mount = useCallback(() => {
-    return (
-      <Modal
-        visible={presentation !== null}
-        onDismiss={() => setPresentation(null)}
-      >
-        {presentation !== null && (
-          <ChatMessageActions
-            post={presentation.post}
-            postActionIds={presentation.postActionIds}
-            postRef={presentation.anchorRef}
-            onDismiss={() => setPresentation(null)}
-            onReply={performActionAndDismissCallback('startThread')}
-            onEdit={performActionAndDismissCallback('edit')}
-            onViewReactions={performActionAndDismissCallback('viewReactions')}
-          />
-        )}
-      </Modal>
-    );
-  }, [performActionAndDismissCallback, presentation]);
-
-  return useMemo(
-    () => ({
-      present: setPresentation,
-      dismiss: () => setPresentation(null),
-      mount,
-    }),
-    [mount]
-  );
-}
-
-function ContextGestureListener({
-  children,
-  menuApi,
-  presentationCandidate,
-}: {
-  children: (api: { present: () => void }) => JSX.Element;
-  menuApi: {
-    present: (opts: PostContextMenuPresentation) => void;
-    dismiss: () => void;
-  };
-  presentationCandidate: Pick<
-    PostContextMenuPresentation,
-    'post' | 'postActionIds'
-  >;
-}) {
-  const contentContainer = useRef<ElementRef<typeof RNView>>(null);
-  const present = useCallback(() => {
-    menuApi.present({
-      ...presentationCandidate,
-      anchorRef: contentContainer,
-    });
-  }, [contentContainer, menuApi, presentationCandidate]);
-  return <RNView ref={contentContainer}>{children({ present })}</RNView>;
 }
