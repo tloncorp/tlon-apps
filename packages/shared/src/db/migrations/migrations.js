9bccc6dd05b9947707b513acd855ef37052fce9f
~latter-bolden <brian@tlon.io>
2024-11-21 16:20:08 -0500

    fix reset migration script, update drizzle patch for new version

--- packages/shared/src/db/migrations/migrations.js
+++ packages/shared/src/db/migrations/migrations.js
@@ -1,7 +1,7 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_serious_penance.sql';
+import m0000 from './0000_aromatic_arachne.sql';
 
   export default {
     journal,
