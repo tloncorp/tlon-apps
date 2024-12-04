346a8668b2e7f1f8f1fdf86bb558120ec15e4cbb
David Lee <david.isaac.lee@gmail.com>
2024-12-04 12:02:38 -0800

    Wrap PostUsingContentConfigurationScreen with ChatOptionsProvider

--- packages/app/features/top/PostUsingContentConfigurationScreen.tsx
+++ packages/app/features/top/PostUsingContentConfigurationScreen.tsx
@@ -3,6 +3,7 @@ import { useChannel, usePostWithRelations } from '@tloncorp/shared';
 import { ChannelContentConfiguration } from '@tloncorp/shared';
 import {
   ChannelProvider,
+  ChatOptionsProvider,
   ComponentsKitContextProvider,
   ConnectedPostView,
   DetailPostUsingContentConfiguration,
@@ -34,43 +35,53 @@ export function PostUsingContentConfigurationScreen({
 
   // TODO: probably needs AttachmentProvider, NavigationProvider
   return (
-    <ComponentsKitContextProvider>
-      <ChannelProvider value={{ channel }}>
-        <PostCollectionContext.Provider
-          value={{
-            channel,
-            collectionConfiguration:
-              channel.contentConfiguration == null
-                ? undefined
-                : ChannelContentConfiguration.defaultPostCollectionRenderer(
-                    channel.contentConfiguration
-                  ).configuration,
-            editingPost: undefined,
-            goToImageViewer: noop,
-            goToPost: noop,
-            hasNewerPosts: false,
-            hasOlderPosts: false,
-            headerMode: 'default',
-            initialChannelUnread: undefined,
-            isLoadingPosts: false,
-            onPressDelete: noop,
-            onPressRetry: noop,
-            onScrollEndReached: noop,
-            onScrollStartReached: noop,
-            posts: undefined,
-            selectedPostId: undefined,
-            setEditingPost: noop,
-            LegacyPostView: PostView,
-            PostView: ConnectedPostView,
-          }}
-        >
-          <DetailPostUsingContentConfiguration
-            post={post}
-            navigateBack={() => navigation.goBack()}
-            flex={1}
-          />
-        </PostCollectionContext.Provider>
-      </ChannelProvider>
-    </ComponentsKitContextProvider>
+    <ChatOptionsProvider
+      onPressGroupMeta={noop}
+      onPressGroupMembers={noop}
+      onPressManageChannels={noop}
+      onPressGroupPrivacy={noop}
+      onPressChannelMembers={noop}
+      onPressChannelMeta={noop}
+      onPressRoles={noop}
+    >
+      <ComponentsKitContextProvider>
+        <ChannelProvider value={{ channel }}>
+          <PostCollectionContext.Provider
+            value={{
+              channel,
+              collectionConfiguration:
+                channel.contentConfiguration == null
+                  ? undefined
+                  : ChannelContentConfiguration.defaultPostCollectionRenderer(
+                      channel.contentConfiguration
+                    ).configuration,
+              editingPost: undefined,
+              goToImageViewer: noop,
+              goToPost: noop,
+              hasNewerPosts: false,
+              hasOlderPosts: false,
+              headerMode: 'default',
+              initialChannelUnread: undefined,
+              isLoadingPosts: false,
+              onPressDelete: noop,
+              onPressRetry: noop,
+              onScrollEndReached: noop,
+              onScrollStartReached: noop,
+              posts: undefined,
+              selectedPostId: undefined,
+              setEditingPost: noop,
+              LegacyPostView: PostView,
+              PostView: ConnectedPostView,
+            }}
+          >
+            <DetailPostUsingContentConfiguration
+              post={post}
+              navigateBack={() => navigation.goBack()}
+              flex={1}
+            />
+          </PostCollectionContext.Provider>
+        </ChannelProvider>
+      </ComponentsKitContextProvider>
+    </ChatOptionsProvider>
   );
 }
