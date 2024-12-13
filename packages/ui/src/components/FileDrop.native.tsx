a4e07cbe64d3c8d9afb9d9bd4a8790e7f90e94af
Dan Brewster <dnbrwstr@gmail.com>
2024-12-13 11:11:29 -0500

    fix type error

--- packages/ui/src/components/FileDrop.native.tsx
+++ packages/ui/src/components/FileDrop.native.tsx
@@ -2,11 +2,10 @@ import { ImagePickerAsset } from 'expo-image-picker';
 import { ComponentProps } from 'react';
 import { View } from 'tamagui';
 
-export function FileDrop({
-  onFilesDropped: _onFilesDropped,
-  ...props
-}: {
-  onFilesDropped: (files: ImagePickerAsset[]) => void;
-} & ComponentProps<typeof View>) {
+export function FileDrop(
+  props: {
+    onAssetsDropped: (files: ImagePickerAsset[]) => void;
+  } & ComponentProps<typeof View>
+) {
   return <View {...props} />;
 }
