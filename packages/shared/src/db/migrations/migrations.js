f0ef7bc2756a9b77ad81f5d70126274f3a5e96b1
Patrick O'Sullivan <patrick@osullivan.io>
2025-01-23 12:59:35 -0600

    add currentUserIsHost to channel model

--- packages/shared/src/db/migrations/migrations.js
+++ packages/shared/src/db/migrations/migrations.js
@@ -1,7 +1,7 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_safe_zarda.sql';
+import m0000 from './0000_unusual_christian_walker.sql';
 
   export default {
     journal,
