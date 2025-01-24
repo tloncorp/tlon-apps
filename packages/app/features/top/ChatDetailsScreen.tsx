e1dbe7f4d656b24f2cfb6899783bfcfe7d7e7671
Dan Brewster <dnbrwstr@gmail.com>
2025-01-24 13:17:34 -0500

    fix navigating between chat details screens

--- packages/app/features/top/ChatDetailsScreen.tsx
+++ packages/app/features/top/ChatDetailsScreen.tsx
@@ -56,6 +56,7 @@ export function ChatDetailsScreen(props: Props) {
 
   return (
     <ChatOptionsProvider
+      key={`${chatType}-${chatId}`}
       initialChat={{
         type: chatType,
         id: chatId,
