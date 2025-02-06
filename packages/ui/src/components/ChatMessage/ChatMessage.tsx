28f4b85b84109a88124db14b50f10002970d4c5c
Patrick O'Sullivan <patrick@osullivan.io>
2025-02-06 13:07:47 -0600

    desktop polish: make sure chat message actions stay open until clicking elsewhere

--- packages/ui/src/components/ChatMessage/ChatMessage.tsx
+++ packages/ui/src/components/ChatMessage/ChatMessage.tsx
@@ -57,7 +57,8 @@ const ChatMessage = ({
   hideOverflowMenu?: boolean;
 }) => {
   const [showRetrySheet, setShowRetrySheet] = useState(false);
-  const [showOverflowOnHover, setShowOverflowOnHover] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const channel = useChannelContext();
   const postActionIds = useMemo(
     () => ChannelAction.channelActionIdsFor({ channel }),
@@ -118,13 +119,13 @@ const ChatMessage = ({
 
   const handleHoverIn = useCallback(() => {
     if (isWeb) {
-      setShowOverflowOnHover(true);
+      setIsHovered(true);
     }
   }, []);
 
   const handleHoverOut = useCallback(() => {
     if (isWeb) {
-      setShowOverflowOnHover(false);
+      setIsHovered(false);
     }
   }, []);
 
@@ -237,12 +238,13 @@ const ChatMessage = ({
           onPressDelete={handleDeletePressed}
         />
       </YStack>
-      {!hideOverflowMenu && showOverflowOnHover && (
+      {!hideOverflowMenu && (isHovered || isPopoverOpen) && (
         <View position="absolute" top={0} right={12}>
           <ChatMessageActions
             post={post}
             postActionIds={postActionIds}
-            onDismiss={() => setShowOverflowOnHover(false)}
+            onDismiss={() => setIsPopoverOpen(false)}
+            onOpenChange={setIsPopoverOpen}
             onReply={handleRepliesPressed}
             onEdit={onPressEdit}
             onViewReactions={setViewReactionsPost}
