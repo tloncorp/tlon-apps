006908e25f7a4e22f7580b29e9f01b3e243e0611
Patrick O'Sullivan <patrick@osullivan.io>
2025-02-06 09:05:30 -0600

    desktop polish: clean up image viewer on web

--- packages/app/navigation/desktop/HomeNavigator.tsx
+++ packages/app/navigation/desktop/HomeNavigator.tsx
@@ -25,18 +25,29 @@ import { HomeDrawerParamList } from '../types';
 const HomeDrawer = createDrawerNavigator();
 
 export const HomeNavigator = () => {
+  const theme = useTheme();
+  const backgroundColor = getVariableValue(theme.background);
+  const borderColor = getVariableValue(theme.border);
+
   return (
     <HomeDrawer.Navigator
       drawerContent={DrawerContent}
       initialRouteName="ChatList"
-      screenOptions={{
-        drawerType: 'permanent',
-        headerShown: false,
-        drawerStyle: {
-          width: 340,
-          backgroundColor: getVariableValue(useTheme().background),
-          borderRightColor: getVariableValue(useTheme().border),
-        },
+      screenOptions={({ navigation }) => {
+        const state = navigation.getState();
+        const routes = state.routes[state.index].state?.routes;
+        const currentScreen = routes?.[routes.length - 1];
+        const isImageViewer = currentScreen?.name === 'ImageViewer';
+
+        return {
+          drawerType: 'permanent',
+          headerShown: false,
+          drawerStyle: {
+            width: isImageViewer ? 0 : 340,
+            backgroundColor,
+            borderRightColor: borderColor,
+          },
+        };
       }}
     >
       <HomeDrawer.Screen name="ChatList" component={MainStack} />
