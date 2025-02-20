adc3367befed326154364386a1593fc0a3e2b953
David Lee <david.isaac.lee@gmail.com>
2025-02-19 20:16:02 -0800

    [wip] move pure modules to tmp/ dir - utils

--- packages/app/ui/utils/index.ts
+++ packages/app/ui/utils/index.ts
@@ -6,4 +6,4 @@ export * from './animation';
 // WIP: Temporary export to avoid breaking imports.
 // The `indexPure` module contains modules that do not rely on code that should
 // be in the `app` subpackage.
-export * from './indexPure';
+export * from '../tmp/utils/indexPure';
