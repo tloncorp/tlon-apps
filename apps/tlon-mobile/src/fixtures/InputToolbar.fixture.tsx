8667d2598072d5298331a368b7e81815989335f9
David Lee <david.isaac.lee@gmail.com>
2025-02-19 20:06:19 -0800

    Spot-fix fixture importing from specific file

--- apps/tlon-mobile/src/fixtures/InputToolbar.fixture.tsx
+++ apps/tlon-mobile/src/fixtures/InputToolbar.fixture.tsx
@@ -1,7 +1,7 @@
 import { useEditorBridge } from '@10play/tentap-editor';
 import { FixtureWrapper } from '@tloncorp/app/fixtures/FixtureWrapper';
-import { InputToolbar } from '@tloncorp/ui/components/MessageInput/InputToolbar.native';
-import { TlonEditorBridge } from '@tloncorp/ui/components/MessageInput/toolbarActions.native';
+import { InputToolbar } from '@tloncorp/app/ui/components/MessageInput/InputToolbar.native';
+import { TlonEditorBridge } from '@tloncorp/app/ui/components/MessageInput/toolbarActions.native';
 
 const InputToolbarFixture = () => {
   const editor = useEditorBridge() as TlonEditorBridge;
