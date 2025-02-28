2dab6c6b5bc82772401720fc2edd4caabd901fd7
David Lee <david.isaac.lee@gmail.com>
2025-02-28 10:53:13 -0800

    Put PostScreenView fixture in an object

--- packages/app/fixtures/PostScreen.fixture.tsx
+++ packages/app/fixtures/PostScreen.fixture.tsx
@@ -9,39 +9,41 @@ import {
 
 const posts = createFakePosts(10);
 
-export default (
-  <AppDataContextProvider
-    contacts={initialContacts}
-    calmSettings={{
-      disableAvatars: false,
-      disableNicknames: false,
-      disableRemoteContent: false,
-    }}
-  >
-    <FixtureWrapper fillWidth fillHeight>
-      <PostScreenView
-        handleGoToUserProfile={() => {}}
-        isLoadingPosts={false}
-        editPost={async () => {}}
-        onPressRetry={async () => {}}
-        onPressDelete={() => {}}
-        editingPost={undefined}
-        negotiationMatch={true}
-        setEditingPost={() => {}}
-        parentPost={null}
-        channel={tlonLocalBulletinBoard}
-        posts={posts}
-        sendReply={async () => {}}
-        markRead={() => {}}
-        groupMembers={group.members ?? []}
-        getDraft={async () => ({})}
-        storeDraft={async () => {}}
-        clearDraft={async () => {}}
-        headerMode="default"
-        onPressRef={() => {}}
-        onGroupAction={() => {}}
-        goToDm={() => {}}
-      />
-    </FixtureWrapper>
-  </AppDataContextProvider>
-);
+export default {
+  PostScreenView: (
+    <AppDataContextProvider
+      contacts={initialContacts}
+      calmSettings={{
+        disableAvatars: false,
+        disableNicknames: false,
+        disableRemoteContent: false,
+      }}
+    >
+      <FixtureWrapper fillWidth fillHeight>
+        <PostScreenView
+          handleGoToUserProfile={() => {}}
+          isLoadingPosts={false}
+          editPost={async () => {}}
+          onPressRetry={async () => {}}
+          onPressDelete={() => {}}
+          editingPost={undefined}
+          negotiationMatch={true}
+          setEditingPost={() => {}}
+          parentPost={null}
+          channel={tlonLocalBulletinBoard}
+          posts={posts}
+          sendReply={async () => {}}
+          markRead={() => {}}
+          groupMembers={group.members ?? []}
+          getDraft={async () => ({})}
+          storeDraft={async () => {}}
+          clearDraft={async () => {}}
+          headerMode="default"
+          onPressRef={() => {}}
+          onGroupAction={() => {}}
+          goToDm={() => {}}
+        />
+      </FixtureWrapper>
+    </AppDataContextProvider>
+  ),
+};
