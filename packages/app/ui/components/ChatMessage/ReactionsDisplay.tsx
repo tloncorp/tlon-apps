f38925a9af5e207b1af16698af36c010293f50f9
Patrick O'Sullivan <patrick@osullivan.io>
2025-03-06 09:51:16 -0600

    desktop: show contact nicknames in emoji hover tooltip

--- packages/app/ui/components/ChatMessage/ReactionsDisplay.tsx
+++ packages/app/ui/components/ChatMessage/ReactionsDisplay.tsx
@@ -8,7 +8,7 @@ import { Tooltip, XStack } from 'tamagui';
 
 import { useCurrentUserId } from '../../contexts/appDataContext';
 import { triggerHaptic } from '../../utils';
-import { useReactionDetails } from '../../utils/postUtils';
+import { ReactionListItem, useReactionDetails } from '../../utils/postUtils';
 
 export function ReactionsDisplay({
   post,
@@ -39,6 +39,20 @@ export function ReactionsDisplay({
         : store.addPostReaction(post, value, currentUserId);
     },
     [currentUserId, post, reactionDetails.self.didReact]
+  );
+
+  const firstThreeReactionUsers = useCallback(
+    (reaction: ReactionListItem) =>
+      reaction.users
+        ? reaction.users
+            .slice(0, 3)
+            .map((user) => user.name)
+            .join(', ') +
+          (reaction.users.length > 3
+            ? ` +${reaction.users.length - 3} more`
+            : '')
+        : '',
+    []
   );
 
   if (reactionDetails.list.length === 0) {
@@ -104,14 +118,7 @@ export function ReactionsDisplay({
                 backgroundColor="$secondaryBackground"
                 borderRadius="$s"
               >
-                <Text size="$label/m">
-                  {reaction.users
-                    ? reaction.users.slice(0, 3).join(', ') +
-                      (reaction.users.length > 3
-                        ? ` +${reaction.users.length - 3} more`
-                        : '')
-                    : ''}
-                </Text>
+                <Text size="$label/m">{firstThreeReactionUsers(reaction)}</Text>
               </Tooltip.Content>
             </Tooltip>
           </Pressable>
