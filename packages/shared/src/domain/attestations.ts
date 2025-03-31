d0187e699afdb4966436ba1dcbbc5d3f6c5c9b85
~latter-bolden <brian@tlon.io>
2025-03-31 10:30:59 -0400

    normalize phone numbers when sending to lanyard, make them pretty when reading from lanyard

--- packages/shared/src/domain/attestations.ts
+++ packages/shared/src/domain/attestations.ts
@@ -1,3 +1,5 @@
+import * as LibPhone from 'libphonenumber-js';
+
 export function parseForTwitterPostId(text: string): string | null {
   // If input is already just a numeric ID
   if (/^\d+$/.test(text)) {
@@ -31,4 +33,17 @@ export function twitterHandleDisplay(text: string): string {
   }
 
   return `@${text}`;
+}
+
+export function displayablePhoneNumber(phoneNumber: string): string {
+  if (!phoneNumber) {
+    console.error('Cannot make displayable phone number, none passed in');
+  }
+
+  const parsed = LibPhone.parsePhoneNumberFromString(phoneNumber, 'US');
+  if (!parsed) {
+    return phoneNumber;
+  }
+
+  return parsed.format('INTERNATIONAL');
 }
