8cc810141d4b3d39060ef3e88b488fcfd9e24fed
bᵣᵢₐₙ <90741358+latter-bolden@users.noreply.github.com>
2025-04-01 10:55:20 -0400

    Update packages/shared/src/api/urbit.ts
    Co-authored-by: Hunter Miller <hunter@hmiller.dev>

--- packages/shared/src/api/urbit.ts
+++ packages/shared/src/api/urbit.ts
@@ -14,7 +14,7 @@ import {
 import { desig, preSig } from '../urbit';
 import { getLandscapeAuthCookie } from './landscapeApi';
 
-const logger = createDevLogger('urbit', true);
+const logger = createDevLogger('urbit', false);
 
 interface Config
   extends Pick<
