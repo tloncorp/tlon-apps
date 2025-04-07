9831d290728ec56dd2f2c9c51b612a00d94a4fec
Patrick O'Sullivan <patrick@osullivan.io>
2025-04-07 10:16:11 -0500

    Memoize the ChatList + only pull out the  method we need from useChatOptions

--- packages/app/features/chat-list/ChatList.tsx
+++ packages/app/features/chat-list/ChatList.tsx
@@ -1,10 +1,12 @@
 import { FlashList, ListRenderItem } from '@shopify/flash-list';
 import * as db from '@tloncorp/shared/db';
+import { isEqual } from 'lodash';
 import React, { useCallback, useMemo, useRef } from 'react';
 import { LayoutChangeEvent } from 'react-native';
 import { getTokenValue } from 'tamagui';
 
 import { SectionedChatData } from '../../hooks/useFilteredChats';
+import { useRenderCount } from '../../hooks/useRenderCount';
 import {
   ChatListItem,
   InteractableChatListItem,
@@ -33,14 +35,14 @@ export const ChatList = React.memo(function ChatListComponent({
     [data]
   );
 
-  const chatOptions = useChatOptions();
+  const { open } = useChatOptions();
   const handleLongPress = useCallback(
     (item: db.Chat) => {
       if (!item.isPending) {
-        chatOptions.open(item.id, item.type);
+        open(item.id, item.type);
       }
     },
-    [chatOptions]
+    [open]
   );
 
   // removed the use of useStyle here because it was causing FlashList to
@@ -117,6 +119,8 @@ export const ChatList = React.memo(function ChatListComponent({
     [handleHeaderLayout, onPressItem, handleLongPress, handleItemLayout]
   );
 
+  useRenderCount('ChatList');
+
   return (
     <FlashList
       data={listItems}
@@ -128,7 +132,7 @@ export const ChatList = React.memo(function ChatListComponent({
       overrideItemLayout={handleOverrideLayout}
     />
   );
-});
+}, isEqual);
 
 export function getItemType(item: ChatListItemData) {
   return isSectionHeader(item) ? 'sectionHeader' : item.type;
