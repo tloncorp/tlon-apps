0cb9bb275598d7d6f7757b0d0e140c464a3d0849
David Lee <david.isaac.lee@gmail.com>
2025-04-21 13:00:38 -0700

    Re-add PushNotificationManager

---
+++ apps/tlon-mobile/ios/Shared/Notifications/PushNotificationManager.swift
@@ -0,0 +1,43 @@
+//
+//  PushNotificationManager.swift
+//  Pocket
+//
+//  Created by Alec Ananian on 1/29/23.
+//
+
+import Foundation
+import Intents
+import NotificationCenter
+
+@objc final class PushNotificationManager: NSObject {
+    enum ParseNotificationResult {
+        case activityEventJson(Any?)
+        case dismiss(uid: String)
+        case invalid
+        case failedFetchContents(Error)
+    }
+
+    static func parseNotificationUserInfo(_ userInfo: [AnyHashable: Any]) async -> ParseNotificationResult {
+        guard let action = userInfo["action"] as? String,
+              let uid = userInfo["uid"] as? String
+        else {
+            return .invalid
+        }
+
+        switch action {
+        case "notify":
+            do {
+                let aerData = try await PocketAPI.shared.fetchRawPushNotificationContents(uid)
+                return .activityEventJson(try JSONSerialization.jsonObject(with: aerData))
+            } catch {
+                return .failedFetchContents(error)
+            }
+
+        case "dismiss":
+            return .dismiss(uid: uid)
+
+        default:
+            return .invalid
+        }
+    }
+}
