f6134f86194b412624f74d7659eb28b3f6bd2e5e
Patrick O'Sullivan <patrick@osullivan.io>
2025-05-22 13:40:15 -0500

    use emoji-regex package rather than rolling our own emoji-regex

--- packages/ui/src/components/Emoji/SizableEmoji.tsx
+++ packages/ui/src/components/Emoji/SizableEmoji.tsx
@@ -3,6 +3,7 @@ import { FontSizeTokens, SizableTextProps, getFontSize } from 'tamagui';
 import { SizableText } from 'tamagui';
 
 import { getNativeEmoji } from './data';
+import { isEmoji } from './utils';
 
 // unclear what this should be (or how it should be calculated), but seems
 // to work?
@@ -19,16 +20,7 @@ export function SizableEmoji(
   const finalEmoji = useMemo(() => {
     const emoji = getNativeEmoji(emojiInput);
 
-    // Check if input is already a Unicode emoji:
-    // 1. Matches Unicode emoji pattern (including presentation style)
-    // 2. Is not found in our emoji mapping
-    // 3. Does not include control characters or unwanted symbols
-    const isEmojiPattern = /^\p{Emoji}+$/u.test(emojiInput);
-    const hasEmojiPresentation = /\p{Emoji_Presentation}/u.test(emojiInput);
-    const isNotControlChar = !/[\p{Cc}\p{Cf}\p{Zl}\p{Zp}]/u.test(emojiInput);
-
-    const isDirectEmoji =
-      isEmojiPattern && hasEmojiPresentation && isNotControlChar && !emoji;
+    const isDirectEmoji = isEmoji(emojiInput) && !emoji;
 
     return isDirectEmoji ? emojiInput : emoji;
   }, [emojiInput]);
