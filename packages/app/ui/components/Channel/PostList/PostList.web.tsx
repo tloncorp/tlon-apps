46e5afc5e7923f7f6b1f8f79d6c8d5826b50b29c
David Lee <david.isaac.lee@gmail.com>
2025-06-16 16:09:36 -0700

    Fix PostList content not filling container width

--- packages/app/ui/components/Channel/PostList/PostList.web.tsx
+++ packages/app/ui/components/Channel/PostList/PostList.web.tsx
@@ -143,6 +143,8 @@ const PostListSingleColumn: PostListComponent = React.forwardRef(
             style={{
               display: 'flex',
               minHeight: '100%',
+              flexDirection: 'column',
+              justifyContent: 'stretch',
             }}
           >
             <View
