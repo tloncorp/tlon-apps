7103887c219e6b1d50513d9c387a5f4e2fcb6619
Dan Brewster <dnbrwstr@gmail.com>
2025-07-07 16:45:09 -0400

    fix scrolling in create group sheet (#4907)

--- packages/app/ui/components/ContactBook.tsx
+++ packages/app/ui/components/ContactBook.tsx
@@ -122,17 +122,8 @@ export function ContactBook({
     [selected, immutableSet, multiSelect, handleSelect]
   );
 
-  const scrollPosition = useRef(0);
-  const handleScroll = useCallback(
-    (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-      scrollPosition.current = event.nativeEvent.contentOffset.y;
-    },
-    []
-  );
   const onTouchStart = useCallback(() => {
-    if (scrollPosition.current > 0) {
-      onScrollChange?.(true);
-    }
+    onScrollChange?.(true);
   }, [onScrollChange]);
 
   const onTouchEnd = useCallback(
@@ -202,7 +193,6 @@ export function ContactBook({
           <BlockSectionList
             ListHeaderComponent={!showSearchResults ? quickActions : null}
             sections={sections}
-            onScroll={handleScroll}
             onTouchStart={onTouchStart}
             onTouchEnd={onTouchEnd}
             renderItem={renderItem}
