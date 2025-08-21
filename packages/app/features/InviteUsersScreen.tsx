b2670a6a5f56e6e1ba06d5a4ab08967d1c5cbb5c
Patrick O'Sullivan <patrick@osullivan.io>
2025-08-21 09:26:19 -0500

    move invite users logic out into shared hook

--- packages/app/features/InviteUsersScreen.tsx
+++ packages/app/features/InviteUsersScreen.tsx
@@ -1,10 +1,10 @@
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
-import { createDevLogger } from '@tloncorp/shared';
 import { Button, LoadingSpinner } from '@tloncorp/ui';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { View, YStack, getTokenValue } from 'tamagui';
 
+import { useInviteGroupMembers } from '../hooks/useInviteUsers';
 import { RootStackParamList } from '../navigation/types';
 import {
   ContactBook,
@@ -15,8 +15,6 @@ import {
 } from '../ui';
 import { InviteFriendsToTlonButton } from '../ui/components/InviteFriendsToTlonButton';
 
-const logger = createDevLogger('InviteUsersScreen', false);
-
 type InviteUsersScreenRouteProp = RouteProp<RootStackParamList, 'InviteUsers'>;
 
 export function InviteUsersScreen() {
@@ -26,41 +24,13 @@ export function InviteUsersScreen() {
   const { bottom } = useSafeAreaInsets();
   const store = useStore();
   const { data: group } = store.useGroup({ id: groupId });
-  const [loading, setLoading] = useState(false);
-  const [invitees, setInvitees] = useState<string[]>([]);
-
-  const handleInviteGroupMembers = useCallback(async () => {
-    if (!group) return;
-
-    setLoading(true);
-    try {
-      await store.inviteGroupMembers({
-        groupId: group.id,
-        contactIds: invitees,
-      });
-      setLoading(false);
-      navigation.goBack();
-    } catch (error) {
-      logger.trackError('Error inviting group members', {
-        errorMessage: error.message,
-        errorStack: error.stack,
-      });
-    } finally {
-      setLoading(false);
-    }
-  }, [invitees, group, navigation, store]);
-
-  const buttonText = useMemo(() => {
-    if (invitees.length === 0) {
-      return `Select people to invite`;
-    }
-
-    return `Invite ${invitees.length} and continue`;
-  }, [invitees]);
 
   const handleGoBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
+
+  const { loading, invitees, setInvitees, handleInvite, buttonText } =
+    useInviteGroupMembers(groupId, handleGoBack);
 
   if (!group) {
     return (
@@ -94,7 +64,7 @@ export function InviteUsersScreen() {
           <PaddedBlock paddingBottom={bottom}>
             <Button
               hero
-              onPress={handleInviteGroupMembers}
+              onPress={handleInvite}
               disabled={invitees.length === 0 || loading}
               gap="$xl"
             >
