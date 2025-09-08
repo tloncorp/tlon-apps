d7e61ab9e6b9cdf221512757ef0dc52c05c32b4e
Hunter Miller <hunter@hmiller.dev>
2025-09-08 13:41:38 -0500

    use-group-context: removing unnecessary log

--- packages/app/hooks/useGroupContext.ts
+++ packages/app/hooks/useGroupContext.ts
@@ -24,7 +24,6 @@ export const useGroupContext = ({
   }, [groupId]);
 
   const group = groupQuery.data ?? null;
-  console.log('Group context', { groupId, group, isFocused });
 
   const currentUserIsAdmin = useMemo(() => {
     return group?.members.some(
