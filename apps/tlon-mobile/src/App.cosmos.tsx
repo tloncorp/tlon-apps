2e6c58a82591e9980a7f18738383b0ecab236332
David Lee <david.isaac.lee@gmail.com>
2025-09-11 09:27:32 -0700

    Add defense against sticky splash on Cosmos

--- apps/tlon-mobile/src/App.cosmos.tsx
+++ apps/tlon-mobile/src/App.cosmos.tsx
@@ -1,9 +1,17 @@
+import * as SplashScreen from 'expo-splash-screen';
 import React, { Component } from 'react';
 import { NativeFixtureLoader } from 'react-cosmos-native';
 
 import { moduleWrappers, rendererConfig } from '../../../cosmos.imports';
 
 export default class CosmosApp extends Component {
+  componentDidMount(): void {
+    // Splash screen sometimes stick - no idea why, since the
+    // `preventAutoHideAsync` call is happening in App.main.tsx, but throwing
+    // this hear to avoid getting stuck.
+    SplashScreen.hideAsync();
+  }
+
   render() {
     return (
       <NativeFixtureLoader
