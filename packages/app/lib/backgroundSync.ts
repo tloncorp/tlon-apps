47359dcdaba68c814d5c1100900bb63ed7fe727b
Dan Brewster <dnbrwstr@gmail.com>
2025-09-15 16:03:45 -0400

    ensure db is available when running background sync

--- packages/app/lib/backgroundSync.ts
+++ packages/app/lib/backgroundSync.ts
@@ -6,10 +6,12 @@ import * as TaskManager from 'expo-task-manager';
 import { v4 as uuidv4 } from 'uuid';
 
 import { configureUrbitClient } from '../hooks/useConfigureUrbitClient';
+import { setupDb } from './nativeDb';
 
 const logger = createDevLogger('backgroundSync', true);
 
 async function performSync() {
+  await setupDb();
   const taskExecutionId = uuidv4();
   logger.trackEvent('Initiating background sync', { taskExecutionId });
   const timings: Record<string, number> = {
