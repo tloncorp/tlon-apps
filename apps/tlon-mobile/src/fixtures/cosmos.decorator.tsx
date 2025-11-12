51293e9610b1680d34eb7ef7c537ffc4c92a737f
~latter-bolden <brian@tlon.io>
2025-11-12 16:50:48 -0600

    fix store.useGroup within fixtures, prevent splash lock

--- apps/tlon-mobile/src/fixtures/cosmos.decorator.tsx
+++ apps/tlon-mobile/src/fixtures/cosmos.decorator.tsx
@@ -1,12 +1,41 @@
+import { group } from '@tloncorp/app/fixtures/fakeData';
 import { StoreProvider, TamaguiProvider, config } from '@tloncorp/app/ui';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+function spyOn<T extends object, MethodName extends keyof T>(
+  base: T,
+  method: MethodName,
+  fn: T[MethodName]
+) {
+  return new Proxy(base, {
+    get(target, prop) {
+      if (prop === method) {
+        return fn;
+      }
+      return target[prop as keyof T];
+    },
+  });
+}
+
 // eslint-disable-next-line
-export default ({ children }: { children: React.ReactNode }) => (
-  <TamaguiProvider defaultTheme={'light'} config={config}>
-    <StoreProvider stub>
-      <SafeAreaProvider>{children}</SafeAreaProvider>
-    </StoreProvider>
-  </TamaguiProvider>
-);
+export default ({ children }: { children: React.ReactNode }) => {
+  const store = useMemo(() => {
+    const mockUseGroup = () => ({
+      data: group,
+      isLoading: false,
+      error: null,
+    });
+
+    // @ts-expect-error - fixture mock
+    return spyOn(baseStore, 'useGroup', mockUseGroup);
+  }, []);
+
+  return (
+    <TamaguiProvider defaultTheme={'light'} config={config}>
+      <StoreProvider stub={store}>
+        <SafeAreaProvider>{children}</SafeAreaProvider>
+      </StoreProvider>
+    </TamaguiProvider>
+  );
+};
