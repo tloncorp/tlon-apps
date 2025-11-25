09b7f053443e322f5e92aa9ed2ccc58c4f0ad32c
Dan Brewster <dnbrwstr@gmail.com>
2025-11-25 12:35:48 -0500

    remove legacy tasks

--- apps/tlon-mobile/src/App.main.tsx
+++ apps/tlon-mobile/src/App.main.tsx
@@ -14,8 +14,7 @@ import ErrorBoundary from '@tloncorp/app/ErrorBoundary';
 import { BranchProvider } from '@tloncorp/app/contexts/branch';
 import { useShip } from '@tloncorp/app/contexts/ship';
 import { useIsDarkMode } from '@tloncorp/app/hooks/useIsDarkMode';
-// DISABLED FOR DEBUG: Background sync disabled to isolate freeze issue
-// import { registerBackgroundSyncTask } from '@tloncorp/app/lib/backgroundSync';
+import { removeLegacyTasks } from '@tloncorp/app/lib/backgroundSync';
 import { useMigrations } from '@tloncorp/app/lib/nativeDb';
 import { splashScreenProgress } from '@tloncorp/app/lib/splashscreen';
 import { BaseProviderStack } from '@tloncorp/app/provider/BaseProviderStack';
@@ -85,6 +84,8 @@ const useSplashHider = () => {
 
 // DISABLED FOR DEBUG: Background sync disabled to isolate freeze issue
 // registerBackgroundSyncTask();
+// ADDED: remove legacy tasks
+removeLegacyTasks();
 
 // Android notification tap handler passes initial params here
 const App = () => {
