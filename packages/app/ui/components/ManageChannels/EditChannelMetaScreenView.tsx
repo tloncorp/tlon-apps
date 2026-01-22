b8095245465d69d23cb5ca3c15cae0ce0fa8ef28
Dan Brewster <dnbrwstr@gmail.com>
2026-01-22 14:50:20 -0500

    deduplicate between edit screens

--- packages/app/ui/components/ManageChannels/EditChannelMetaScreenView.tsx
+++ packages/app/ui/components/ManageChannels/EditChannelMetaScreenView.tsx
@@ -1,9 +1,7 @@
 import * as db from '@tloncorp/shared/db';
-import { KeyboardAvoidingView, useIsWindowNarrow } from '@tloncorp/ui';
+import { KeyboardAvoidingView } from '@tloncorp/ui';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { ScrollView, View } from 'tamagui';
 
 import {
   ControlledTextField,
@@ -11,6 +9,7 @@ import {
   FormFrame,
 } from '../Form';
 import { ScreenHeader } from '../ScreenHeader';
+import { ChannelEditFormLayout } from './ChannelEditFormLayout';
 
 interface ChannelMetaFormSchema {
   title: string;
@@ -73,19 +72,14 @@ export function EditChannelMetaScreenView({
     [handleSubmit, handleSave]
   );
 
-  const insets = useSafeAreaInsets();
-  const isWindowNarrow = useIsWindowNarrow();
-
   return (
-    <View flex={1} backgroundColor="$secondaryBackground">
-      <ScreenHeader
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <ChannelEditFormLayout
         title="Edit channel info"
-        subtitle={`${group?.title}: ${channel?.title}`}
-        showSubtitle={!!channel?.title && !!group?.title}
-        backgroundColor="$secondaryBackground"
-        backAction={goBack}
+        channel={channel}
+        group={group}
+        goBack={goBack}
         isLoading={isLoading}
-        useHorizontalTitleLayout={!isWindowNarrow}
         rightControls={
           <ScreenHeader.TextButton
             onPress={runSubmit}
@@ -96,51 +90,42 @@ export function EditChannelMetaScreenView({
             Save
           </ScreenHeader.TextButton>
         }
-      />
-      <KeyboardAvoidingView style={{ flex: 1 }}>
-        <ScrollView
-          keyboardDismissMode="on-drag"
-          contentContainerStyle={{
-            minHeight: '100%',
-            paddingBottom: insets.bottom,
-          }}
-        >
-          <FormFrame paddingBottom="$2xl" flex={1} backgroundType="secondary">
-            <ControlledTextField
-              name="title"
-              label="Name"
-              control={control}
-              inputProps={{
-                placeholder: 'Channel name',
-                testID: 'ChannelTitleInput',
-              }}
-              rules={{
-                maxLength: {
-                  value: 30,
-                  message: 'Your channel name is limited to 30 characters',
-                },
-              }}
-            />
-            <ControlledTextareaField
-              name="description"
-              label="Description"
-              control={control}
-              inputProps={{
-                placeholder: 'About this channel',
-                numberOfLines: 5,
-                testID: 'ChannelDescriptionInput',
-                multiline: true,
-              }}
-              rules={{
-                maxLength: {
-                  value: 300,
-                  message: 'Description is limited to 300 characters',
-                },
-              }}
-            />
-          </FormFrame>
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </View>
+      >
+        <FormFrame paddingBottom="$2xl" flex={1} backgroundType="secondary">
+          <ControlledTextField
+            name="title"
+            label="Name"
+            control={control}
+            inputProps={{
+              placeholder: 'Channel name',
+              testID: 'ChannelTitleInput',
+            }}
+            rules={{
+              maxLength: {
+                value: 30,
+                message: 'Your channel name is limited to 30 characters',
+              },
+            }}
+          />
+          <ControlledTextareaField
+            name="description"
+            label="Description"
+            control={control}
+            inputProps={{
+              placeholder: 'About this channel',
+              numberOfLines: 5,
+              testID: 'ChannelDescriptionInput',
+              multiline: true,
+            }}
+            rules={{
+              maxLength: {
+                value: 300,
+                message: 'Description is limited to 300 characters',
+              },
+            }}
+          />
+        </FormFrame>
+      </ChannelEditFormLayout>
+    </KeyboardAvoidingView>
   );
 }
