d055be6961e9b8cdf5ea0128c1e6656c6a425754
David <david.isaac.lee@gmail.com>
2026-02-03 21:04:49 -0800

    Add button to toggle sheet in fixture

--- packages/app/fixtures/ActionSheet/AttachmentSheet.fixture.tsx
+++ packages/app/fixtures/ActionSheet/AttachmentSheet.fixture.tsx
@@ -1,8 +1,25 @@
+import { Button, Text } from '@tloncorp/ui';
+import { useState } from 'react';
+
 import AttachmentSheet from '../../ui/components/AttachmentSheet';
 import { FixtureWrapper } from '../FixtureWrapper';
 
-export default (
-  <FixtureWrapper>
-    <AttachmentSheet onOpenChange={() => {}} isOpen={true} mediaType="all" />
-  </FixtureWrapper>
-);
+export default function AttachmentSheetFixture() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <FixtureWrapper>
+      <Button
+        label={isOpen ? '(Sheet is open)' : 'Open Sheet'}
+        onPress={() => {
+          setIsOpen((x) => !x);
+        }}
+      />
+      <AttachmentSheet
+        onOpenChange={setIsOpen}
+        isOpen={isOpen}
+        mediaType="all"
+      />
+    </FixtureWrapper>
+  );
+}
