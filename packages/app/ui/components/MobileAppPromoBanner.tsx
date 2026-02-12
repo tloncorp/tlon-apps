366eeee1e40272732f1d8fa38d7f2bfc85684474
~latter-bolden <brian@tlon.io>
2026-02-11 21:57:39 -0600

    fix relative image url in MobileAppPromoBanner

--- packages/app/ui/components/MobileAppPromoBanner.tsx
+++ packages/app/ui/components/MobileAppPromoBanner.tsx
@@ -79,7 +79,7 @@ export function MobileAppPromoBanner() {
           <Image
             style={{ width: '100%', height: 150 }}
             contentFit={'cover'}
-            source={'./mobile-banner.png'}
+            source={`/apps/groups/mobile-banner.png`}
           />
         </YStack>
         <Pressable
