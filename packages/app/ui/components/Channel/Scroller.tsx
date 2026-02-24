1ca2acac482310cfdbf84a8bc1bc253eb1b041a0
James Acklin <james.acklin@gmail.com>
2026-02-24 12:24:58 -0500

    channel: add vertical padding for comfy-list and grid layouts

--- packages/app/ui/components/Channel/Scroller.tsx
+++ packages/app/ui/components/Channel/Scroller.tsx
@@ -324,10 +324,21 @@ const Scroller = forwardRef(
     );
 
     const insets = useSafeAreaInsets();
+    const rootVerticalPadding = getTokens().space.l.val;
 
     const contentContainerStyle = useStyle(
       useMemo(() => {
         if (!posts?.length) {
+          if (
+            collectionLayoutType === 'comfy-list-top-to-bottom' ||
+            collectionLayoutType === 'grid'
+          ) {
+            return {
+              flex: 1,
+              paddingTop: rootVerticalPadding,
+              paddingBottom: insets.bottom + rootVerticalPadding,
+            };
+          }
           return { flex: 1 };
         }
 
@@ -342,7 +353,8 @@ const Scroller = forwardRef(
             return {
               paddingHorizontal: '$m',
               gap: '$l',
-              paddingBottom: insets.bottom,
+              paddingTop: rootVerticalPadding,
+              paddingBottom: insets.bottom + rootVerticalPadding,
             };
           }
 
@@ -350,11 +362,12 @@ const Scroller = forwardRef(
             return {
               paddingHorizontal: '$m',
               gap: '$l',
-              paddingBottom: insets.bottom,
+              paddingTop: rootVerticalPadding,
+              paddingBottom: insets.bottom + rootVerticalPadding,
             };
           }
         }
-      }, [insets, posts?.length, collectionLayoutType])
+      }, [insets.bottom, posts?.length, collectionLayoutType, rootVerticalPadding])
     ) as StyleProp<ViewStyle>;
 
     const columnWrapperStyle = useStyle(
