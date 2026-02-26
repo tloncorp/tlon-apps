61fe5a19b30dff6efe88a7ac2c643f1fdebb356c
reid <yapishu@users.noreply.github.com>
2026-02-26 11:37:09 -0600

    remove old wasm

--- core/scripts/build-wasm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CORE_DIR="$(dirname "$SCRIPT_DIR")"
-
-cd "$CORE_DIR"
-wasm-pack build --release --target web --out-dir ../packages/shared/src/signal/pkg
