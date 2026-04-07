514aec97a40817c31eed48959b350fc766483dac
David <david.isaac.lee@gmail.com>
2026-04-07 14:32:58 -0700

    Import directly from @tloncorp/ui instead of proxying through app/ui

--- packages/app/navigation/utils.ts
+++ packages/app/navigation/utils.ts
@@ -8,6 +8,7 @@ import { createDevLogger } from '@tloncorp/shared';
 import * as db from '@tloncorp/shared/db';
 import * as logic from '@tloncorp/shared/logic';
 import * as store from '@tloncorp/shared/store';
+import { useGlobalSearch, useIsWindowNarrow } from '@tloncorp/ui';
 import { useCallback, useMemo } from 'react';
 
 import { useGlobalSearch, useIsWindowNarrow } from '../ui';
