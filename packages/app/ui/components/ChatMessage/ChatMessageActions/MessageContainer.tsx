43f75b57b7c4c5fe9851cd56fa4ec79b7397bd06
David <david.isaac.lee@gmail.com>
2026-04-14 11:37:03 -0700

    Hide post content in actions preview when user has hidden post

--- packages/app/ui/components/ChatMessage/ChatMessageActions/MessageContainer.tsx
+++ packages/app/ui/components/ChatMessage/ChatMessageActions/MessageContainer.tsx
@@ -2,12 +2,12 @@ import { getSize } from '@tamagui/get-token';
 import * as db from '@tloncorp/shared/db';
 import { useIsWindowNarrow } from '@tloncorp/ui';
 import { Dimensions } from 'react-native';
-import { ScrollView, View } from 'tamagui';
+import { View } from 'tamagui';
 
 import AuthorRow from '../../AuthorRow';
 import { NotebookPostContent } from '../../NotebookPost/NotebookPostContent';
 import { NotebookPostFrame } from '../../NotebookPost/shared';
-import { MaskedChatMessage } from '../../PostModerationSwitch';
+import { PostModerationSwitch } from '../../PostModerationSwitch';
 import { StaticChatMessage } from '../StaticChatMessage';
 
 const MAX_MESSAGE_TO_SCREEN_RATIO = 0.3;
@@ -19,42 +19,56 @@ export function MessageContainer({ post }: { post: db.Post }) {
   const isWindowNarrow = useIsWindowNarrow();
   const width = isWindowNarrow ? screenWidth - getSize('$xl').val * 2 : 400;
 
-  if (post.type === 'note') {
-    return (
-      <ScrollView
-        maxHeight={screenHeight * MAX_MESSAGE_TO_SCREEN_RATIO_NOTE}
-        overflow="hidden"
-        backgroundColor="$background"
-        padding="$l"
-        borderRadius="$l"
-        width={width}
-      >
-        <NotebookPostFrame>
-          <NotebookPostContent post={post} showAuthor={false} />
-        </NotebookPostFrame>
-      </ScrollView>
-    );
-  }
-
   return (
     <View
-      maxHeight={screenHeight * MAX_MESSAGE_TO_SCREEN_RATIO}
-      maxWidth={width}
+      {...(post.type === 'note'
+        ? {
+            maxHeight: screenHeight * MAX_MESSAGE_TO_SCREEN_RATIO_NOTE,
+            width,
+            backgroundColor: '$background',
+          }
+        : {
+            maxHeight: screenHeight * MAX_MESSAGE_TO_SCREEN_RATIO,
+            maxWidth: width,
+            backgroundColor: isWindowNarrow
+              ? '$background'
+              : '$secondaryBackground',
+          })}
       overflow="hidden"
-      backgroundColor={isWindowNarrow ? '$background' : '$secondaryBackground'}
       padding="$l"
       borderRadius="$l"
     >
-      <AuthorRow
-        author={post.author}
-        authorId={post.authorId}
-        sent={post.sentAt ?? 0}
-        type={post.type}
-        // roles={roles}
-      />
-      <MaskedChatMessage post={post}>
-        <StaticChatMessage post={post} hideSentAtTimestamp />
-      </MaskedChatMessage>
+      <PostModerationSwitch post={post}>
+        {(m) => {
+          switch (m.type) {
+            case 'deleted':
+              return m.deleted;
+            case 'blocked':
+              return m.blocked;
+            case 'hidden':
+              return m.hidden;
+            case 'post': {
+              post = m.post;
+              return post.type === 'note' ? (
+                <NotebookPostFrame>
+                  <NotebookPostContent post={post} showAuthor={false} />
+                </NotebookPostFrame>
+              ) : (
+                <>
+                  <AuthorRow
+                    author={post.author}
+                    authorId={post.authorId}
+                    sent={post.sentAt ?? 0}
+                    type={post.type}
+                    // roles={roles}
+                  />
+                  <StaticChatMessage post={post} hideSentAtTimestamp />
+                </>
+              );
+            }
+          }
+        }}
+      </PostModerationSwitch>
     </View>
   );
 }
