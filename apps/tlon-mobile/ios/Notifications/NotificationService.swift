b632bcedd3ada0458b313ed0b36ef3d9b8b9f13d
~latter-bolden <brian@tlon.io>
2026-04-29 13:40:56 -0500

    mobile: update client notif handling to account for generic messages

--- apps/tlon-mobile/ios/Notifications/NotificationService.swift
+++ apps/tlon-mobile/ios/Notifications/NotificationService.swift
@@ -132,6 +132,18 @@ class NotificationService: UNNotificationServiceExtension {
         return preview
     }
 
+    private func applyGenericMessage(_ message: String, uid: String, notification: UNMutableNotificationContent) -> UNNotificationContent {
+        if notification.title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            notification.title = "Tlon"
+        }
+        notification.body = message
+        notification.sound = UNNotificationSound.default
+        notification.interruptionLevel = .active
+        notification.userInfo["uid"] = uid
+        notification.userInfo["genericMessage"] = message
+        return notification
+    }
+
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
@@ -171,6 +183,18 @@ class NotificationService: UNNotificationServiceExtension {
               contentHandler(notifContent)
               return
 
+          case .message(let uid, let message):
+              let notifContent = bestAttemptContent ?? UNMutableNotificationContent()
+              let notification = notifContent.mutableCopy() as! UNMutableNotificationContent
+              let renderedNotification = applyGenericMessage(
+                message,
+                uid: uid,
+                notification: notification
+              )
+              await NotificationLogger.logDelivery(properties: ["uid": .string(uid), "message": .string("Generic notification delivered successfully")])
+              contentHandler(renderedNotification)
+              return
+
           case .dismiss:
               // Should not be hit, but set up fallback notification just in case.
               // We can't prevent this alert from being shown, so at least make it truthful.
@@ -202,10 +226,6 @@ class NotificationService: UNNotificationServiceExtension {
               }
 
               await NotificationLogger.logDelivery(properties: ["message": .string("Fallback notification delivered successfully")])
-              contentHandler(bestAttemptContent!)
-              return
-
-            case .dismiss:
               contentHandler(bestAttemptContent!)
               return
             }
