97704b6fe422733efb4fc677101e3e80bc52a3c0
Dan Brewster <dnbrwstr@gmail.com>
2026-06-09 15:23:33 -0400

    regenerate notes migration with reset-migrations util

--- packages/shared/src/db/migrations/migrations.js
+++ packages/shared/src/db/migrations/migrations.js
@@ -1,13 +1,12 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_real_rhino.sql';
-import m0001 from './0001_native_notes.sql';
+import m0000 from './0000_dusty_toxin.sql';
 
-export default {
-  journal,
-  migrations: {
-    m0000,
-    m0001,
+  export default {
+    journal,
+    migrations: {
+      m0000
+    }
   }
-}
+  
