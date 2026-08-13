3aa7482d901642b0a02da5eb9914fecfc490811b
Dan Brewster <dnbrwstr@gmail.com>
2026-08-13 15:27:09 -0400

    polish agent onboarding handoff

--- packages/app/features/top/ChannelScreen.tsx
+++ packages/app/features/top/ChannelScreen.tsx
@@ -42,6 +42,7 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Channel'>;
 export default function ChannelScreen(props: Props) {
   const {
     channelId,
+    disableTransition,
     selectedPostId,
     startDraft,
     groupId: routeGroupId,
@@ -51,6 +52,15 @@ export default function ChannelScreen(props: Props) {
     startDraft: false,
     groupId: undefined,
   };
+
+  useEffect(() => {
+    if (!disableTransition) return;
+
+    const frame = requestAnimationFrame(() => {
+      props.navigation.setParams({ disableTransition: undefined });
+    });
+    return () => cancelAnimationFrame(frame);
+  }, [disableTransition, props.navigation]);
   const [currentChannelId, setCurrentChannelId] = React.useState(channelId);
 
   useEffect(() => {
