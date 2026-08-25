58bc812421a01690c1942c6423410ef3dfe27860
Dan Brewster <dnbrwstr@gmail.com>
2026-08-24 21:26:11 -0400

    remove duplicate onboarding context props

--- packages/app/ui/components/PostContent/ContentRenderer.tsx
+++ packages/app/ui/components/PostContent/ContentRenderer.tsx
@@ -71,9 +71,6 @@ function ContentRenderer({
   provisionedAgentTopics,
   consumedA2UIMessageText,
   getConsumedA2UISelection,
-  configuredAgentProviderIds,
-  provisionedAgentTopics,
-  consumedA2UIMessageText,
   isNotice,
   searchQuery,
   ...rest
@@ -96,9 +93,6 @@ function ContentRenderer({
       provisionedAgentTopics={provisionedAgentTopics}
       consumedA2UIMessageText={consumedA2UIMessageText}
       getConsumedA2UISelection={getConsumedA2UISelection}
-      configuredAgentProviderIds={configuredAgentProviderIds}
-      provisionedAgentTopics={provisionedAgentTopics}
-      consumedA2UIMessageText={consumedA2UIMessageText}
       isNotice={isNotice}
       searchQuery={searchQuery}
     >
