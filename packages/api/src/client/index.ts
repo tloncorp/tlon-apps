037e27f8d870a017b61e65b013d9b61ff3750b81
Dan Brewster <dnbrwstr@gmail.com>
2026-08-28 01:53:31 -0400

    add recurring task management fixtures

--- packages/api/src/client/index.ts
+++ packages/api/src/client/index.ts
@@ -121,4 +121,5 @@ export * from './changesApi';
 export * from './computingStatus';
 export * from './presenceApi';
 export * from './stewardGatewayApi';
+export * from './stewardAutomationApi';
 export * from './lensApi';
