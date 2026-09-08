9fb7c70b4d257151edc39b1b937970c3079cf0db
Vincent de Lafontaine <vincent@appandflow.com>
2026-09-08 12:21:28 -0400

    fix(android): use Expo's virtual Metro entry point

--- apps/tlon-mobile/android/app/src/main/java/io/tlon/landscape/MainApplication.kt
+++ apps/tlon-mobile/android/app/src/main/java/io/tlon/landscape/MainApplication.kt
@@ -26,7 +26,7 @@ class MainApplication : Application(), ReactApplication {
       packageList = PackageList(this).packages.apply {
         add(TalkPackage())
       },
-      jsMainModulePath = "index"
+      jsMainModulePath = ".expo/.virtual-metro-entry"
     )
   }
 
