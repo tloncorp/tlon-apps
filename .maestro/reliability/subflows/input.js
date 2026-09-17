c0c82260d1cb8e1ad263301675475bcc0660ddb7
Dan Brewster <dnbrwstr@gmail.com>
2026-09-17 07:39:31 -0400

    split notebook title coverage by platform

--- .maestro/reliability/subflows/input.js
+++ .maestro/reliability/subflows/input.js
@@ -1,9 +1,1 @@
-var forceIncremental =
-  typeof INPUT_FORCE_INCREMENTAL !== 'undefined' &&
-  (INPUT_FORCE_INCREMENTAL === true || INPUT_FORCE_INCREMENTAL === 'true');
-
-output.fixtureInput = {
-  index: 0,
-  pattern: '^' + INPUT_TEXT + '\\n?$',
-  forceIncremental: forceIncremental,
-};
+output.fixtureInput = { index: 0, pattern: '^' + INPUT_TEXT + '\\n?$' };
