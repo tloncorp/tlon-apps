b1e4e9026aa8f0950d13e211bda94483dc762693
Dan Brewster <dnbrwstr@gmail.com>
2026-09-17 07:24:30 -0400

    acknowledge notebook title input on iOS

--- .maestro/reliability/subflows/input.js
+++ .maestro/reliability/subflows/input.js
@@ -1,1 +1,9 @@
-output.fixtureInput = { index: 0, pattern: '^' + INPUT_TEXT + '\\n?$' };
+var forceIncremental =
+  typeof INPUT_FORCE_INCREMENTAL !== 'undefined' &&
+  (INPUT_FORCE_INCREMENTAL === true || INPUT_FORCE_INCREMENTAL === 'true');
+
+output.fixtureInput = {
+  index: 0,
+  pattern: '^' + INPUT_TEXT + '\\n?$',
+  forceIncremental: forceIncremental,
+};
